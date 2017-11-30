a3f9ed5f4411357af84d3983f3e48917bda519a0
ramboze <samuel.brooks@gmail.com>
2017-12-01 10:09:13 +1100

    added file header

---
+++ tokensale/Tokensale.sol
@@ -0,0 +1,61 @@
+/*
+-----------------------------------------------------------------
+FILE INFORMATION
+-----------------------------------------------------------------
+file:       Tokensale.sol
+version:    0.1
+author:     Block8 Technologies, in partnership with Havven
+
+            Anton Jurisevic
+            Samuel Brooks
+
+date:       2017-11-31
+
+checked:    -
+approved:   -
+
+-----------------------------------------------------------------
+MODULE DESCRIPTION
+-----------------------------------------------------------------
+Token sale contract for the Havven stablecoin system.
+
+
+-----------------------------------------------------------------
+LICENCE INFORMATION
+-----------------------------------------------------------------
+
+Copyright (c) 2017 Havven.io
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+    
+-----------------------------------------------------------------
+RELEASE NOTES
+-----------------------------------------------------------------
+- Initial scaffolding of tokensale contract. Interfaces to other
+contracts holding core Havven (alpha version) functionality.
+
+
+-----------------------------------------------------------------
+Block8 Technologies are accellerating blockchain technology
+through incubating meaningful, next-generation businesses.
+Find out more at block8.io
+-----------------------------------------------------------------
+
+*/
+
