f2a7ed915d27324c66f5d05433a9f26eb8210ae7
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2017-12-08 15:33:53 +1100

    Add oracle scaffold.

---
+++ priceoracle/oracle.py
@@ -0,0 +1,61 @@
+"""
+-----------------------------------------------------------------
+FILE INFORMATION
+-----------------------------------------------------------------
+file:       oracle.py
+version:    0.1
+author:     Block8 Technologies, in partnership with Havven
+
+            Anton Jurisevic
+
+date:       2017-12-08
+
+checked:    -
+approved:   -
+
+-----------------------------------------------------------------
+MODULE DESCRIPTION
+-----------------------------------------------------------------
+An oracle that monitors the ETH/USD price on multiple exchanges,
+periodically pushing the median price to a target smart contract.
+
+
+-----------------------------------------------------------------
+LICENCE INFORMATION
+-----------------------------------------------------------------
+
+Copyright (c) 2017 Havven.io
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+    
+-----------------------------------------------------------------
+RELEASE NOTES
+-----------------------------------------------------------------
+- Initial scaffolding of tokensale contract. Interfaces to other
+contracts holding core Havven (alpha version) functionality.
+
+
+-----------------------------------------------------------------
+Block8 Technologies are accelerating blockchain technology
+through incubating meaningful, next-generation businesses.
+Find out more at block8.io
+-----------------------------------------------------------------
+
+"""
+
