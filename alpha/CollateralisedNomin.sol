1ad4aab9112c1d0f72e6d186a7d65d64af8f3eda
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2017-12-18 11:03:51 +1100

    Fix small bug, update function access modifier.

--- alpha/CollateralisedNomin.sol
+++ alpha/CollateralisedNomin.sol
@@ -609,13 +609,14 @@ contract CollateralisedNomin is ERC20Token {
         onlyOwner
     {
         stalePeriod = period;
-        StalePeriodUpdate(price);
+        StalePeriodUpdate(period);
     }
 
     /* True iff the current block timestamp is later than the time
      * the price was last updated, plus the stale period. */
     function priceIsStale()
         public
+        view
         returns (bool)
     {
         return lastPriceUpdate + stalePeriod < now;
