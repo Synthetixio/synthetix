3b6c1f702078e6625b5f8869bcbcec31ac1514b2
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-03 14:15:26 +1100

    Update licence headers.

--- contracts/ERC20FeeToken.sol
+++ contracts/ERC20FeeToken.sol
@@ -1,3 +1,60 @@
+/*
+-----------------------------------------------------------------
+FILE INFORMATION
+-----------------------------------------------------------------
+file:       ERC20FeeToken.sol
+version:    0.1
+author:     Block8 Technologies, in partnership with Havven
+
+            Anton Jurisevic
+
+date:       2018-1-3
+
+checked:    -
+approved:   -
+
+-----------------------------------------------------------------
+MODULE DESCRIPTION
+-----------------------------------------------------------------
+
+An ERC20-compliant token which also has a configurable fee rate
+that charged on its transfers.
+
+-----------------------------------------------------------------
+LICENCE INFORMATION
+-----------------------------------------------------------------
+
+Copyright (c) 2017 Havven.io
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+    
+-----------------------------------------------------------------
+RELEASE NOTES
+-----------------------------------------------------------------
+
+-----------------------------------------------------------------
+Block8 Technologies is accelerating blockchain technology
+by incubating meaningful next-generation businesses.
+Find out more at https://www.block8.io/
+-----------------------------------------------------------------
+*/
+
 pragma solidity ^0.4.19;
 
 import "SafeFixedMath.sol";
