1a76f0e163ce41e758cc33a323299d6d21cc98a9
DominicRomanowski <romanowski.dominic@gmail.com>
2018-01-10 14:30:23 +1100

    added price history, SMA, volume weighted median, error proof run, price shock updates, timers for various things

--- priceoracle/markets.py
+++ priceoracle/markets.py
@@ -60,50 +60,74 @@ Find out more at block8.io
 
 
 import requests
+import requests.exceptions
 import json
 import asyncio
 
+
 class PriceFeed:
-    def __init__(self, name, query_string, access_func):
+    def __init__(self, name, query_string, price_func, vol_func=None):
         self.name = name
         self.query_string = query_string
-        self.access_func = access_func
+        self.price_func = price_func
+        self.vol_func = vol_func
 
     def query(self):
         return requests.get(self.query_string).text
     
-    def price(self):
-        return float(self.access_func(json.loads(self.query())))
+    def price(self, get_volume=False):
+        try:
+            result = json.loads(self.query())
+            if get_volume and self.vol_func:
+                return float(self.price_func(result)), float(self.vol_func(result))
+            else:
+                return float(self.price_func(result)), 1
+        except requests.exceptions.Timeout:
+            print("Timeout for:", self.name)
+            return None
+        except Exception as e:
+            print("Error", e, "for:", self.name)
+            return None
 
 
 FEEDS = [PriceFeed("Kraken",
                    "https://api.kraken.com/0/public/Ticker?pair=ETHUSD",
-                   lambda j: j["result"]["XETHZUSD"]["c"][0]),
+                   lambda j: j["result"]["XETHZUSD"]["c"][0],
+                   lambda j: j["result"]["XETHZUSD"]["v"][1]),
          PriceFeed("HitBTC",
                    "https://api.hitbtc.com/api/2/public/ticker/ETHUSD",
-                   lambda j: j["last"]),
+                   lambda j: j["last"],
+                   lambda j: j["volume"]),
          PriceFeed("Bitstamp",
                    "https://www.bitstamp.net/api/v2/ticker/ethusd/",
-                   lambda j: j["last"]),
+                   lambda j: j["last"],
+                   lambda j: j["volume"]),
          PriceFeed("Gemini",
                    "https://api.gemini.com/v1/pubticker/ethusd",
-                   lambda j: j["last"]),
+                   lambda j: j["last"],
+                   lambda j: j["volume"]["ETH"]),
          PriceFeed("Bitfinex",
                    "https://api.bitfinex.com/v2/ticker/tETHUSD",
-                   lambda j: j[6]),
+                   lambda j: j[6],
+                   lambda j: j[7]),
          PriceFeed("GDAX",
                    "https://api.gdax.com/products/ETH-USD/ticker",
-                   lambda j: j["price"]),
+                   lambda j: j["price"],
+                   lambda j: j['volume']),
          PriceFeed("Coinbase",
                    "https://api.coinbase.com/v2/prices/ETH-USD/spot",
-                   lambda j: j["data"]["amount"]),
+                   lambda j: j["data"]["amount"],
+                   None),
          PriceFeed("CEX",
                    "https://cex.io/api/ticker/ETH/USD",
-                   lambda j: j["last"]),
+                   lambda j: j["last"],
+                   lambda j: j['volume']),
          PriceFeed("WEX",
                    "https://wex.nz/api/3/ticker/eth_usd",
-                   lambda j: j["eth_usd"]["last"]),
+                   lambda j: j["eth_usd"]["last"],
+                   lambda j: j["eth_usd"]["vol_cur"]),
          PriceFeed("Quoine",
                    "https://api.quoine.com/products/27",
-                   lambda j: j["last_traded_price"])]
+                   lambda j: j["last_traded_price"],
+                   lambda j: j["volume_24h"])]
 
--- priceoracle/oracle.py
+++ priceoracle/oracle.py
@@ -61,6 +61,37 @@ Find out more at block8.io
 
 from concurrent.futures import ThreadPoolExecutor
 from markets import FEEDS, PriceFeed
+import time
+
+
+PRICE_REFRESH_TIME = 5
+'Refresh the price every x seconds'
+
+PRICE_DISPLAY_TIME = 80
+'Display the price every x seconds'
+
+VOLUME_WEIGHTED = True
+'Weigh price results based on volume'
+
+FORCE_PRICE_UPDATE_SHOCK = .01
+'''
+How much % of a shock needs to happen since the last displayed price
+to force the next price to be shown immediately
+'''
+
+BLOCK_TIME = 20
+'''
+The minimum time between price displays during periods of large shocks
+This should be close to ether block time, in the future, this can be faster
+as long as previous updates can be changed before they go to the blockchain
+'''
+
+PRICE_REFRESH_TIMEOUT = 2
+'How long should each individual price feed wait for a response'
+
+SIMPLE_MOVING_AVERAGE_STEPS = 5
+'Price is a simple moving average that takes x steps into consideration'
+
 
 class PriceList:
     def __init__(self):
@@ -68,34 +99,89 @@ class PriceList:
         if len(self.prices) == 0:
             raise ValueError("FATAL: Price feed list is empty!")
         self.last_median = None
+        self.last_update_time = 0
+        self.last_displayed_price = 0
+        self.last_display_time = 0
+        self.historical_prices = []
 
-    def update_price(self, feed):
+    def update_single_price(self, feed):
         try:
-            self.prices[feed.name] = feed.price()
-            print(f"Updated {feed.name} to {self.prices[feed.name]}.")
+            self.prices[feed.name] = feed.price(VOLUME_WEIGHTED)
+            # print(f"Updated {feed.name} to {self.prices[feed.name]}.")
             return self.prices[feed.name]
         except:
             self.prices[feed.name] = None
             return None
 
-    def median_price(self):
-        print(self.prices)
-        s_prices = sorted([v for v in self.prices.values() if v is not None])
-        p_len = len(s_prices)
-        
-        if p_len != 0:
-            # Update the current median price if any prices were reported.
-            mid = p_len // 2
-            if p_len % 2:
-                self.last_median = s_prices[mid]
-            else:
-                self.last_median = (s_prices[mid] + s_prices[mid - 1]) / 2
+    def update_prices(self):
+        with ThreadPoolExecutor() as executor:
+            executor.map(self.update_single_price, FEEDS, timeout=PRICE_REFRESH_TIMEOUT)
 
+        self.historical_prices.append(self.median_price())
+        if len(self.historical_prices) > SIMPLE_MOVING_AVERAGE_STEPS:
+            self.historical_prices.pop(0)
+
+    def run(self):
+        while True:
+            start_time = time.time()
+            self.update_prices()
+
+            # if no prices, try again TODO: count failures, do something when it fails too much?
+            if len(self.historical_prices) < 1:
+                continue
+
+            price = sum(self.historical_prices) / len(self.historical_prices)
+            print(f"price history now: {self.historical_prices}")
+            # if its time to present a new price
+            if time.time() - self.last_display_time >= PRICE_DISPLAY_TIME:
+                self.send_price(price)
+            elif abs((self.last_displayed_price-price)/self.last_displayed_price) > FORCE_PRICE_UPDATE_SHOCK:
+                print(f"PRICE SHOCK! {(self.last_displayed_price-price)/self.last_displayed_price}%")
+                self.send_price(price)
+
+            # Wait for PRICE_REFRESH_TIME
+            price_update_duration = time.time() - start_time
+            if price_update_duration < PRICE_REFRESH_TIME:
+                time.sleep(PRICE_REFRESH_TIME - price_update_duration)
+
+    def send_price(self, value):
+        if time.time() - self.last_displayed_price > BLOCK_TIME:
+            print(f"Send to smart contract: {value}")
+            self.last_display_time = time.time()
+            self.last_displayed_price = value
+
+    def median_price(self):
+        """
+        Get the volume weighted median price
+        if VOLUME_WEIGHTED is false, all volumes will be 1,
+        if volume is missing, volume will also be 1.
+        """
+
+        s_prices = sorted([v for v in self.prices.values() if v is not None])
+        total_weight = sum([i[1] for i in s_prices])
+        current_weight = 0
+        for n, i in enumerate(s_prices):
+            current_weight += i[1]
+            if current_weight > total_weight/2:
+                self.last_median = i[0]
+                break
+            elif current_weight == total_weight/2:
+                # should be safe to assume n+1 exists in this case, as weights should be at least 1
+                # i.e. one element exists, its weight would be 1 > 0.5(total/2)
+                self.last_median = (i[0] + s_prices[n+1][0])/2
+                break
         return self.last_median
 
-plist = PriceList()
-with ThreadPoolExecutor() as executor:
-    futures = executor.map(plist.update_price, FEEDS, timeout=10)
-    print(list(futures))
 
-print(plist.median_price())
+if __name__ == '__main__':
+    plist = PriceList()
+    while True:
+        # NEVER DIE
+        try:
+            plist.run()
+        except KeyboardInterrupt:
+            break
+        except Exception:
+            continue
+    pass
+
