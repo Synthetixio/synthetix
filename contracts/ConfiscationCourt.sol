8112fbf1ecac45b043923dfacc79bcd10eabf10f
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-15 01:41:43 +1100

    Confiscation court improvement and reorganisation.

--- contracts/ConfiscationCourt.sol
+++ contracts/ConfiscationCourt.sol
@@ -30,12 +30,14 @@
  * The weight of their vote will be proportional with their locked balance.
  *
  * Hence an action to confiscate the balance of a given address composes
- * a state machine that proceeds in the following way:
- * 
+ * a state machine built of the following states:
+ *
+ *
  * Waiting:
  *   - A user with standing brings a vote:
- *     If the target address is not frozen,
- *     initialise vote tallies to 0 and transition to the Voting state.
+ *     If the target address is not frozen;
+ *     initialise vote tallies to 0;
+ *     transition to the Voting state.
  *
  * Voting:
  *   - The foundation vetoes the in-progress vote:
@@ -45,11 +47,11 @@
  *     transition to the Confirmation state.
  *
  *   - An account votes (for or against the motion):
- *     the account is locked, its balance is added to the appropriate tally,
+ *     the account is locked, its balance is added to the appropriate tally;
  *     remain in the Voting state.
  * 
  *   - An account cancels its previous vote: 
- *     the account is unlocked, its balance is deducted from the appropriate tally (if any), 
+ *     the account is unlocked, its balance is deducted from the appropriate tally (if any);
  *     remain in the Voting state.
  *
  * Confirmation:
@@ -57,13 +59,18 @@
  *     transition to the Waiting state.
  *
  *   - The foundation approves confiscation of the target account:
- *     freeze the target account and transfer its balance to the nomin fee pool,
+ *     freeze the target account, transfer its balance to the nomin fee pool;
  *     transition to the Waiting state.
  *
  *   - The confirmation period elapses:
  *     transition to the Waiting state.
  *
- * For the sake of correctness, this procedure is designed to be relatively simple.
+ *
+ * User votes are not automatically cancelled upon the conclusion of a vote.
+ * Therefore, after a vote comes to a conclusion, if a user wishes to free
+ * their havven balance, they must manually cancel their vote in order to do so.
+ * 
+ * This procedure is designed to be relatively simple.
  * There are some things that can be added to enhance the functionality
  * at the expense of simplicity and efficiency:
  * 
@@ -75,39 +82,40 @@
  * 
  * We might consider updating the contract with any of these features at a later date if necessary.
  */
- import "Owned.sol";
 
+import "Owned.sol";
 
 contract ConfiscationCourt is Owned {
 
-    /* ========== CONTRACT MEMBERS ========== */
+    /* ========== STATE VARIABLES ========== */
 
 	// The minimum havven balance required to be considered to have standing
     // to begin confiscation proceedings.
     uint public minStandingBalance = 100 * UNIT;
 
-    // The voting period lasts for this length of time.
+    // The voting period lasts for this duration,
+    // and if set, must fall within the given bounds.
     uint public votingPeriod = 1 weeks;
-    // The voting duration must fall within the following range.
     uint public constant minVotingPeriod = 3 days;
     uint public constant maxVotingPeriod = 1 months;
 
-    // Period during which the foundation may confirm or veto a vote that has concluded.
+    // Duration of the period during which the foundation may confirm
+    // or veto a vote that has concluded.
+    // If set, the confirmation duration must fall within the given bounds.
     uint public confirmationPeriod = 1 weeks;
-    // Confirmation duration must fall within the following range.
     uint public constant minConfirmationPeriod = 1 days;
     uint public constant maxConfirmationPeriod = 2 weeks;
 
     // No fewer than this fraction of havvens must participate in the vote
-    // in order for it to have standing.
-    uint public requiredParticipation = 3 * UNIT / 10;
+    // in order for a quorum to be reached.
     // The participation fraction required may be set no lower than 20%.
+    uint public requiredParticipation = 3 * UNIT / 10;
     uint public constant minRequiredParticipation = 2 * UNIT / 10;
 
     // At least this fraction of participating votes must be in favour of
     // confiscation for the proposal to pass.
-    uint public requiredMajority = (2 * UNIT) / 3;
     // The required majority may be no lower than 50%.
+    uint public requiredMajority = (2 * UNIT) / 3;
     uint public constant minRequiredMajority = UNIT / 2;
 
     // The timestamp at which a vote began. This is used to determine
@@ -116,13 +124,13 @@ contract ConfiscationCourt is Owned {
     // A vote runs from its start time t until (t + votingPeriod),
     // and then the confirmation period terminates no later than
     // (t + votingPeriod + confirmationPeriod).
-    mapping(address => uint) voteStartTime;
+    mapping(address => uint) public voteStartTime;
 
     // The tallies for and against confiscation of a given balance.
     // These are set to zero at the start of a vote, and also on conclusion,
     // just to keep the blockchain clean.
-    mapping(address => uint) votesFor;
-    mapping(address => uint) votesAgainst;
+    mapping(address => uint) public votesFor;
+    mapping(address => uint) public votesAgainst;
 
     // The addresses of the havven and nomin contracts.
     address public havven;
@@ -185,9 +193,12 @@ contract ConfiscationCourt is Owned {
     }
 
 
-    /* ========== QUERY FUNCTIONS ========== */
+    /* ========== VIEW FUNCTIONS ========== */
 
-    function voteIsOpen(address target)
+    /* There is an action in progress on the specified
+     * account, and votes are being accepted in that action.
+     */
+    function voting(address target)
         public
         view
     {
@@ -196,7 +207,10 @@ contract ConfiscationCourt is Owned {
         return now < voteStartTime[target] + votingPeriod;
     }
 
-    function inConfirmationPeriod(address target)
+    /* A vote on the target account has concluded, but the action
+     * has not yet been approved, vetoed, or closed.
+     */
+    function confirming(address target)
     	public
     	view
     {
@@ -205,14 +219,17 @@ contract ConfiscationCourt is Owned {
                now < startTime + votingPeriod + confirmationPeriod;
     }
 
-    function voteHasTerminated(address target)
+    /* A vote has either not begun, or it has completely terminated. */
+    function waiting(address target)
     	public
     	view
     {
     	return voteStartTime[target] + votingPeriod + confirmationPeriod <= now;
     }
 
-    /* If the vote was to terminate at this instant, would it pass? */
+    /* If the vote was to terminate at this instant, it would pass.
+     * That is: there was sufficient participation and a sizeable enough majority.
+     */
     function votePasses(address target) 
     	public
     	view
@@ -235,53 +252,80 @@ contract ConfiscationCourt is Owned {
     }
     
 
-    /* ========== MUTATORS ========== */
+    /* ========== MUTATIVE FUNCTIONS ========== */
 
     /* Begin a vote to confiscate the funds in a given nomin account.
-     * Only people with sufficient havven balances may elect to start such a vote.
+     * Only the foundation, or accounts with sufficient havven balances
+     * may elect to start such a vote.
      */
-    function mootConfiscation(address target)
+    function beginConfiscationAction(address target)
         public
     {
-        // A confiscation action must be mooted by someone with standing;
-        // That is, they must have a sufficient havven balance or be
-        // the contract's owner.
+        // A confiscation action must be mooted by someone with standing.
         require((havven.balanceOf(msg.sender) > minStandingBalance) || 
                 msg.sender == owner);
+
+        // There must be no confiscation vote already running for this account.
+        require(!voting(target));
+
         // Disallow votes on accounts that have previously been frozen.
         require(!nomin.frozenAccounts[target]);
-
-        // There must be no confiscation vote already running.
-        require(!voteIsOpen(target));
 
         voteStartTime[target] = now;
         votesFor[target] = 0;
         votesAgainst[target] = 0;
     }
 
-    function voteForConfiscation(address target)
+    /* The sender casts a vote in favour of confiscation of the
+     * target account's nomin balance. */
+    function voteFor(address target)
         public
     {
-        require(voteIsOpen(target));
+    	// There must be an active vote for this target running.
+    	// Vote totals must only change during the voting phase.
+        require(voting(target));
+
+        // This user can't already have voted in anything.
         require(!havven.hasVoted(msg.sender));
-        havven.setVotedFor(msg.sender);
+
+        // The user should not have voted previously without cancelling
+        // that vote; the check inside havven.setVotedFor() ensures this.
+        havven.setVotedFor(msg.sender, target);
         votesFor[msg.sender] += havven.balanceOf(msg.sender);
     }
 
-    function voteAgainstConfiscation(address target)
+    /* The sender casts a vote against confiscation of the
+     * target account's nomin balance. */
+    function voteAgainst(address target)
         public
     {
-        require(voteIsOpen(target));
+    	// There must be an active vote for this target running.
+    	// Vote totals must only change during the voting phase.
+        require(voting(target));
+
+        // This user can't already have voted in anything.
         require(!havven.hasVoted(msg.sender));
-        havven.setVotedAgainst(msg.sender);
+
+        // The user should not have voted previously without cancelling
+        // that vote; the check inside havven.setVotedAgainst() ensures this.
+        havven.setVotedAgainst(msg.sender, target);
         votesAgainst[msg.sender] += havven.balanceOf(msg.sender);
     }
 
+    /* Cancel an existing vote by the sender on an action
+     * to confiscate the target balance. */
     function cancelVote(address target) 
         public
     {
-    	require(!inConfirmationPeriod(target));
-        if (voteIsOpen(target)) {
+    	// An account may cancel its vote either before the confirmation phase
+    	// when the vote is still open, or after the confirmation phase,
+   		// when the vote has concluded.
+   		// But the totals must not change during the confirmation phase itself.
+    	require(!confirming(target));
+
+    	// If we are not voting, there is no reason to update the vote totals.
+        if (voting(target)) {
+    		// This call to getVote() must come before the later call to cancelVote(), obviously.
 	        int vote = havven.getVote(msg.sender);
 	        if (vote == 1) {
 	        	votesFor[msg.sender] -= havven.balanceOf(msg.sender);
@@ -290,17 +334,22 @@ contract ConfiscationCourt is Owned {
 	        	votesAgainst[msg.sender] -= havven.balanceOf(msg.sender);
 	        }
     	}
-        havven.cancelVote(msg.sender);
+
+    	// If the user is trying to cancel a vote for a different target
+    	// than the one they have previously voted for, an exception is thrown
+    	// inside havven.cancelVote, and the state is rolled back.
+        havven.cancelVote(msg.sender, target);
+        
     }
 
     /* If a vote has concluded, or if it lasted its full duration but not passed,
-     * then anyone may cancel it.
+     * then anyone may reinitialise it.
      */
     function closeVote(address target) 
     	public
     {
-    	require((inConfirmationPeriod(target) && !votePasses(target)) ||
-    			voteHasTerminated(target));
+    	require((confirming(target) && !votePasses(target)) ||
+    			waiting(target));
     	voteStartTime[target] = 0;
         votesFor[target] = 0;
         votesAgainst[target] = 0;
@@ -314,7 +363,7 @@ contract ConfiscationCourt is Owned {
     	onlyOwner
     {
 
-    	require(inConfirmationPeriod(target));
+    	require(confirming(target));
     	require(votePasses(target));
 
     	nomin.confiscateBalance(target);
