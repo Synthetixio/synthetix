4d55f816700ab63b1c8631a5f8e04ffa8e91b401
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-16 11:09:01 +1100

    Havven contract cleanup.

--- contracts/Havven.sol
+++ contracts/Havven.sol
@@ -8,16 +8,79 @@ author:     Block8 Technologies, in partnership with Havven
 
             Anton Jurisevic
 
-date:       2018-1-15
+date:       2018-1-16
 
-checked:    Samuel Brooks
-approved:   Samuel Brooks
+checked:    -
+approved:   -
 
 -----------------------------------------------------------------
 MODULE DESCRIPTION
 -----------------------------------------------------------------
 
-Havven backing token contract scaffold.
+Havven token contract. Havvens are transferrable ERC20 tokens,
+and also give their holders certain rights and privileges.
+An owner of havvens is entitled to a share in the fees levied on
+nomin transactions, and additionally may participate in nomin
+confiscation votes.
+
+The fee entitlement of a havven holder is their average havven balance over
+the last fee period. This is computed by measuring the area under the graph of
+a user's balance over time, and then when fees are distributed,
+dividing through by the duration of the fee period.
+
+We need only update fee entitlement on transfer when the havven balances of the sender
+and recipient are modified. This is for efficiency, and adds an implicit friction to
+trading in the havven market. A havven holder pays for his own recomputation whenever
+he wants to change his position, which saves the foundation having to maintain a pot
+dedicated to resourcing this.
+
+A hypothetical user's balance history over one fee period, pictorially:
+
+      s ___
+       |   |
+       |   |___ p
+       |___|___|___ __ _  _ 
+       f   t   n
+
+Here, the balance was s between times f and t, at which time a transfer
+occurred, updating the balance to p, until n, when the present transfer occurs.
+
+When a new transfer occurs, at time n, when the balance was at p,
+we must:
+  - Add the area p * (n - t) to the total area recorded so far
+  - Update the last transfer time to p
+So in the case that this graph represents the entire current fee period,
+the average havvens held so far is ((t-f)*s + (n-t)*p) / (n-f).
+The complementary computations must be performed for both sender and
+recipient.
+
+Note that, fees extracted notwithstanding, a transfer keeps global supply
+of havvens invariant. The sum of all balances is constant, and unmodified
+by a transfer. So the sum of all balances multiplied by the duration of
+a fee period is also constant, and this is equivalent to the sum of 
+the area of every user's time/balance graph. Dividing through by that duration
+yields back the total havven supply. So, at the end of a fee period, we really
+do yield a user's average share in the havven supply over that period.
+
+A slight wrinkle is introduced if we consider the time r when the fee period
+rolls over. If the last transfer was before r, but the current transfer is afterwards:
+
+      s __|_
+       |    |
+       |  | |____ p
+       |____|____|___ __ _  _
+          |      
+       f  r t    n
+
+In this situation the area (r-f)*s contributes to the previous fee period, while
+the area (t-r)*s contributes to the current one. We will implicitly consider a
+zero-value transfer to have occurred at time r. Their fee entitlement for the
+previous period will be finalised at the time of their first transfer during the
+current fee period, or when they query or withdraw their fee entitlement.
+
+In the implementation, the duration of different fee periods may be slightly irregular,
+as the check that they have rolled over occurs only when state-changing havven
+operations are performed.
 
 -----------------------------------------------------------------
 LICENCE INFORMATION
@@ -47,81 +110,19 @@ SOFTWARE.
 RELEASE NOTES
 -----------------------------------------------------------------
 
-This is evidently just a skeleton, but this contract will
-need to refer to, and be referenced by, the nomin contract
-in order to facilitate fee and fund-freezing functionality.
-
 -----------------------------------------------------------------
 Block8 Technologies is accelerating blockchain technology
 by incubating meaningful next-generation businesses.
 Find out more at https://www.block8.io/
 -----------------------------------------------------------------
 */
-/* The fee entitlement of a havven holder is their average havven balance over
- * the last fee period. This is computed by measuring the area under the graph of
- * a user's balance over time, and then when fees are distributed,
- * dividing through by the duration of the fee period.
- * 
- * We need only update fee entitlement on transfer when the havven balances of the sender
- * and recipient are modified. This is for efficiency, and adds an implicit friction to
- * trading in the havven market. A havven holder pays for his own recomputation whenever
- * he wants to change his position, which saves the foundation having to maintain a pot
- * dedicated to resourcing this.
- *
- * A hypothetical user's balance history over one fee period, pictorially:
- *
- * s ___
- *  |   |
- *  |   |___ p
- *  |___|___|___ __ _  _ 
- *  f   t   n
- *
- * Here, the balance was s between times f and t, at which time a transfer
- * occurred, updating the balance to p, until n, when the present transfer occurs.
- *
- * When a new transfer occurs, at time n, when the balance was at p,
- * we must:
- *   - Add the area p * (n - t) to the total area recorded so far
- *   - Update the last transfer time to p
- * So in the case that this graph represents the entire current fee period,
- * the average havvens held so far is ((t-f)*s + (n-t)*p) / (n-f).
- * The complementary computations must be performed for both sender and
- * recipient.
- * 
- * Note that, fees extracted notwithstanding, a transfer keeps global supply
- * of havvens invariant. The sum of all balances is constant, and unmodified
- * by a transfer. So the sum of all balances multiplied by the duration of
- * a fee period is also constant, and this is equivalent to the sum of 
- * the area of every user's time/balance graph. Dividing through by that duration
- * yields back the total havven supply. So, at the end of a fee period, we really
- * do yield a user's average share in the havven supply over that period.
- *
- * A slight wrinkle is introduced if we consider the time r when the fee period
- * rolls over. If the last transfer was before r, but the current transfer is afterwards:
- *  
- * s __|_
- *  |    |
- *  |  | |____ p
- *  |____|____|___ __ _  _
- *     |      
- *  f  r t    n
- * 
- * In this situation the area (r-f)*s contributes to the previous fee period, while
- * the area (t-r)*s contributes to the current one. We will implicitly consider a
- * zero-value transfer to have occurred at time r. Their fee entitlement for the
- * previous period will be finalised at the time of their first transfer during the
- * current fee period, or when they query or withdraw their fee entitlement.
- *
- * In the implementation, the duration of different fee periods may be slightly irregular,
- * as the check that they have rolled over occurs only when state-changing havven
- * operations are performed.
- */
-
 
 pragma solidity ^0.4.19;
 
+
 import "ERC20FeeToken.sol";
 import "CollateralisedNomin.sol";
+
 
 contract Havven is ERC20FeeToken {
 
