ad64ca13a9bff05bcc24a20984154dc53a24a059
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-25 15:07:49 +1100

    Update contracts to comply with latest web3.py

--- tests/test_Owned.py
+++ tests/test_Owned.py
@@ -4,18 +4,18 @@ from deploy import owned_contract, MASTER
 
 class TestOwned(unittest.TestCase):
     def test_owner_is_master(self):
-        self.assertEqual(owned_contract.call().owner(), MASTER)
+        self.assertEqual(owned_contract.functions.owner().call(), MASTER)
 
     def test_change_owner(self):
         new_owner = deploy.W3.eth.accounts[1]
-        deploy.mine_tx(owned_contract.transact({'from': MASTER}).setOwner(new_owner))
-        self.assertEqual(owned_contract.call().owner(), new_owner)
-        deploy.mine_tx(owned_contract.transact({'from': new_owner}).setOwner(MASTER))
+        deploy.mine_tx(owned_contract.functions.setOwner(new_owner).transact({'from': MASTER}))
+        self.assertEqual(owned_contract.functions.owner().call(), new_owner)
+        deploy.mine_tx(owned_contract.functions.setOwner(MASTER).transact({'from': new_owner}))
 
     def test_change_invalid_owner(self):
         invalid_account = deploy.W3.eth.accounts[1]
         with self.assertRaises(ValueError) as error:
-            deploy.mine_tx(owned_contract.transact({'from': invalid_account}).setOwner(invalid_account))
+            deploy.mine_tx(owned_contract.functions.setOwner(invalid_account).transact({'from': invalid_account}))
         self.assertTrue("revert" in error.exception.args[0]['message'])
         self.assertEqual(-32000, error.exception.args[0]['code'])
 
