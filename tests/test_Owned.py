31525fb987e506548f490f681c01f44782ae609b
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-26 15:26:59 +1100

    Factor deploy utils out of deploy script.

--- tests/test_Owned.py
+++ tests/test_Owned.py
@@ -1,31 +1,28 @@
 import unittest
-import deploy
-#from deploy import owned_contract, MASTER
 from deploy import MASTER
+from deploy_utils import W3, compile_contracts, attempt_deploy, mine_tx
+from test_utils import assertTransactionReverts
 
 OWNED_SOURCE = "contracts/Owned.sol"
 
 class TestOwned(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        compiled = deploy.compile_contracts([OWNED_SOURCE])
-        cls.owned = deploy.attempt_deploy(compiled, 'Owned', MASTER, [MASTER])
+        compiled = compile_contracts([OWNED_SOURCE])
+        cls.owned = attempt_deploy(compiled, 'Owned', MASTER, [MASTER])
 
     def test_owner_is_master(self):
         self.assertEqual(self.owned.functions.owner().call(), MASTER)
 
     def test_change_owner(self):
-        new_owner = deploy.W3.eth.accounts[1]
-        deploy.mine_tx(self.owned.functions.setOwner(new_owner).transact({'from': MASTER}))
+        new_owner = W3.eth.accounts[1]
+        mine_tx(self.owned.functions.setOwner(new_owner).transact({'from': MASTER}))
         self.assertEqual(self.owned.functions.owner().call(), new_owner)
-        deploy.mine_tx(self.owned.functions.setOwner(MASTER).transact({'from': new_owner}))
+        mine_tx(self.owned.functions.setOwner(MASTER).transact({'from': new_owner}))
 
     def test_change_invalid_owner(self):
-        invalid_account = deploy.W3.eth.accounts[1]
-        with self.assertRaises(ValueError) as error:
-            deploy.mine_tx(self.owned.functions.setOwner(invalid_account).transact({'from': invalid_account}))
-        self.assertTrue("revert" in error.exception.args[0]['message'])
-        self.assertEqual(-32000, error.exception.args[0]['code'])
+        invalid_account = W3.eth.accounts[1]
+        assertTransactionReverts(self, self.owned.functions.setOwner(invalid_account), invalid_account)
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import unittest
+from deploy_utils import mine_tx
 
 def assertCallReverts(testcase, function):
     with testcase.assertRaises(ValueError) as error:
@@ -8,6 +9,6 @@ def assertCallReverts(testcase, function):
 
 def assertTransactionReverts(testcase, function, caller, gas=5000000):
     with testcase.assertRaises(ValueError) as error:
-        testcase.transact({'from': caller, 'gas': gas})
+        mine_tx(function.transact({'from': caller, 'gas': gas}))
     testcase.assertTrue("revert" in error.exception.args[0]['message'])
     testcase.assertEqual(-32000, error.exception.args[0]['code'])
