92b381830c02ee7cf6594e615911242203057748
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-26 15:07:17 +1100

    Test assert reverts utils and safemath tests.

---
+++ tests/test_utils.py
@@ -0,0 +1,13 @@
+import unittest
+
+def assertCallReverts(testcase, function):
+    with testcase.assertRaises(ValueError) as error:
+        function.call()
+    testcase.assertTrue("revert" in error.exception.args[0]['message'])
+    testcase.assertEqual(-32000, error.exception.args[0]['code'])
+
+def assertTransactionReverts(testcase, function, caller, gas=5000000):
+    with testcase.assertRaises(ValueError) as error:
+        testcase.transact({'from': caller, 'gas': gas})
+    testcase.assertTrue("revert" in error.exception.args[0]['message'])
+    testcase.assertEqual(-32000, error.exception.args[0]['code'])
