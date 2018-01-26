a63b15cdadadb8cff0957d485002bfc8f67afefc
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-26 19:49:58 +1100

    Owned test reverts to previous owner in more general way.

--- tests/test_Owned.py
+++ tests/test_Owned.py
@@ -5,6 +5,13 @@ from test_utils import assertTransactionReverts
 
 OWNED_SOURCE = "contracts/Owned.sol"
 
+
+def setUpModule():
+    print("Testing Owned...")
+
+def tearDownModule():
+    print()
+
 class TestOwned(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -15,10 +22,13 @@ class TestOwned(unittest.TestCase):
         self.assertEqual(self.owned.functions.owner().call(), MASTER)
 
     def test_change_owner(self):
+        old_owner = self.owned.functions.owner().call()
         new_owner = W3.eth.accounts[1]
+
         mine_tx(self.owned.functions.setOwner(new_owner).transact({'from': MASTER}))
         self.assertEqual(self.owned.functions.owner().call(), new_owner)
-        mine_tx(self.owned.functions.setOwner(MASTER).transact({'from': new_owner}))
+
+        mine_tx(self.owned.functions.setOwner(old_owner).transact({'from': new_owner}))
 
     def test_change_invalid_owner(self):
         invalid_account = W3.eth.accounts[1]
