b8ae1389b60cf0bef9c0a762d1a9d966c1549a3e
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-29 10:54:58 +1100

    Remove redundant print exceptions flag.

--- tests/test_SafeDecimalMath.py
+++ tests/test_SafeDecimalMath.py
@@ -16,8 +16,7 @@ class TestSafeDecimalMath(unittest.TestCase):
     def setUpClass(cls):
         compiled = compile_contracts([MATH_MODULE_SOURCE],
                                      remappings=['""=contracts'])
-        cls.math = attempt_deploy(compiled, 'PublicMath', MASTER, [],
-                                  print_exception=True)
+        cls.math = attempt_deploy(compiled, 'PublicMath', MASTER, [])
 
         cls.addIsSafe = lambda self, x, y: cls.math.functions.pubAddIsSafe(x, y)
         cls.safeAdd = lambda self, x, y: cls.math.functions.pubSafeAdd(x, y)
