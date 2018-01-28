fb1ef81dbf633d760b09f7a3ed2c614a8910b032
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-29 00:58:12 +1100

    Minor adjustment and documentation improvements in SafeDecimalMath.sol

--- contracts/SafeDecimalMath.sol
+++ contracts/SafeDecimalMath.sol
@@ -138,16 +138,21 @@ contract SafeDecimalMath {
     /* Return the result of multiplying x and y, throwing an exception in case of overflow.
      * A factor of the units is divided out AFTER the product of x and y is evaluated,
      * so that product must be less than 2**256.
+     * Incidentally, the internal division always rounds down: we could have rounded to the nearest integer,
+     * but then we would be spending a significant fraction of a cent (of order a microether
+     * at present gas prices) in order to save less than one part in 0.5 * 10^18 per operation, if the operands
+     * contain small enough fractional components. It would also marginally diminish the 
+     * domain this function is defined upon. 
      */
     function safeDecMul(uint x, uint y)
         pure
         internal
         returns (uint)
     {
-        require(mulIsSafe(x, y));
         // Divide by UNIT to remove the extra factor introduced by the product.
-        // UNIT can't actually be 0.
-        return (x * y) / UNIT;
+        // UNIT be 0.
+        return safeMul(x, y) / UNIT;
+
     }
 
     /* True iff the denominator of x/y is nonzero. */
@@ -173,7 +178,8 @@ contract SafeDecimalMath {
     }
 
     /* Return the result of dividing x by y, throwing an exception in case of overflow or zero divisor.
-     * x must be less than 2^256 / UNIT. */
+     * x must be less than 2^256 / UNIT.
+     * Internal rounding is downward: a similar caveat holds as with safeDecMul().*/
     function safeDecDiv(uint x, uint y)
         pure
         internal
