5d805ef6cd76cec68aa5fd00c539979aa726ef4f
DominicRomanowski <dominic.romanowski@gmail.com>
2018-01-30 09:31:51 +1100

    added comments for all functions in havven

--- tests/test_Havven.py
+++ tests/test_Havven.py
@@ -8,10 +8,26 @@ class TestHavven(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.havven, cls.nomin, cls.court = deploy_havven()
+        cls.construction_block = W3.eth.blockNumber-1
 
-    #####
-    # test ownership
-    #####
+    ###
+    # Test Construction
+    ###
+    ##
+    # Havven - constructor
+    def test_feePeriodStartTime(self):
+        print(W3.eth.getBlock(self.construction_block)['timestamp'])
+        print(self.havven.functions.feePeriodStartTime().call())
+
+    # ERC20 - constructor
+    def test_totalSupply(self):
+        total_supply = 10**8 * UNIT
+        self.assertEquals(self.havven.functions.totalSupply().call(), total_supply)
+
+
+    ###
+    # Test Ownership
+    ###
     def test_owner_is_master(self):
         self.assertEqual(self.havven.functions.owner().call(), MASTER)
 
@@ -28,6 +44,53 @@ class TestHavven(unittest.TestCase):
         invalid_account = W3.eth.accounts[1]
         assertTransactionReverts(self, self.havven.functions.setOwner(invalid_account), invalid_account)
 
+    ###
+    # Mappings
+    ###
+    # currentBalanceSum
+    # lastAverageBalance
+    # penultimateAverageBalance
+    # lastTransferTimestamp
+    # hasWithdrawnLastPeriodFees
+
+    ###
+    # Contract variables
+    ###
+    # feePeriodStartTime
+    # targetFeePeriodDurationSeconds
+    # minFeePeriodDurationSeconds
+    # lastFeePeriodDuration
+    # lastFeesCollected
+
+    ###
+    # Vote Mappings
+    ###
+    # vote
+    # voteTarget
+
+    ###
+    # Functions
+    ###
+
+    # setNomin
+    # setCourt
+    # setTargetFeePeriod
+    # hasVoted
+    # endow
+    # transfer
+    # transferFrom
+    # adjustFeeEntitlement
+    # rolloverFee
+    # withdrawFeeEntitlement
+    # setVotedYea
+    # setVotedNay
+    # cancelVote
+
+    ###
+    # Modifiers
+    ###
+    # postCheckFeePeriodRollover
+    # onlyCourt
 
 if __name__ == '__main__':
     unittest.main()
