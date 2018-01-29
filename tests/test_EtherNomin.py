85505a31c1c6b694c0b0168e0fadc4bb3bd85d44
DominicRomanowski <dominic.romanowski@gmail.com>
2018-01-29 14:31:06 +1100

    fix ethernomintest

--- tests/test_EtherNomin.py
+++ tests/test_EtherNomin.py
@@ -1,7 +1,7 @@
 import unittest
 from deploy import UNIT, MASTER
 
-from utils.deployutils import W3, compile_contracts, attempt_deploy
+from utils.deployutils import W3, compile_contracts, attempt_deploy, mine_tx
 from utils.testutils import assertCallReverts, assertTransactionReverts
 
 
@@ -199,7 +199,7 @@ class TestEtherNomin(unittest.TestCase):
         mine_tx(self.setOracle(pre_oracle).transact({'from': owner}))
 
         # Check if everything works with something in the pool.
-        self.asserTrue(False)
+        self.assertTrue(False)
 
 
     # setPrice
