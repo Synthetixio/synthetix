5766f18bf61882983f91541e9c527a0964764859
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-29 12:58:25 +1100

    EtherNomin updates, publically expose private methods, test setters/getters, constructor.

--- tests/test_EtherNomin.py
+++ tests/test_EtherNomin.py
@@ -1,9 +1,9 @@
 import unittest
 from deploy import UNIT, MASTER
-from deployutils import compile_contracts, attempt_deploy
-from testutils import assertCallReverts
+from deployutils import W3, compile_contracts, attempt_deploy, mine_tx
+from testutils import assertCallReverts, assertTransactionReverts
 
-ETHERNOMIN_SOURCE = "contracts/EtherNomin.sol"
+ETHERNOMIN_SOURCE = "tests/contracts/PublicEtherNomin.sol"
 
 def setUpModule():
     print("Testing EtherNomin...")
@@ -14,8 +14,9 @@ def tearDownModule():
 class TestEtherNomin(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        compiled = compile_contracts([ETHERNOMIN_SOURCE])
-        cls.nomin = attempt_deploy(compiled, 'EtherNomin', MASTER,
+        compiled = compile_contracts([ETHERNOMIN_SOURCE],
+                                     remappings=['""=contracts'])
+        cls.nomin = attempt_deploy(compiled, 'PublicEtherNomin', MASTER,
         						   [MASTER, MASTER, MASTER,
         						    1000 * UNIT, MASTER])
 
@@ -28,6 +29,8 @@ class TestEtherNomin(unittest.TestCase):
         cls.liquidationTimestamp = lambda self: cls.nomin.functions.liquidationTimestamp()
         cls.etherPrice = lambda self: cls.nomin.functions.etherPrice()
         cls.isFrozen = lambda self, address: cls.nomin.functions.isFrozen(address)
+        cls.lastPriceUpdate = lambda self: cls.nomin.functions.publicLastPriceUpdate()
+        cls.stalePeriod = lambda self: cls.nomin.functions.publicStalePeriod()
 
         cls.setOracle = lambda self, address: cls.nomin.functions.setOracle(address)
         cls.setCourt = lambda self, address: cls.nomin.functions.setCourt(address)
@@ -78,6 +81,12 @@ class TestEtherNomin(unittest.TestCase):
     	self.assertEqual(self.oracle().call(), MASTER)
     	self.assertEqual(self.beneficiary().call(), MASTER)
     	self.assertEqual(self.etherPrice().call(), 1000 * UNIT)
+    	self.assertNotEqual(self.lastPriceUpdate().call(), 0)
+    	self.assertEqual(self.stalePeriod().call(), 2 * 24 * 60 * 60) # default two days
+    	self.assertEqual(self.liquidationTimestamp().call(), 2**256 - 1)
+    	self.assertEqual(self.liquidationPeriod().call(), 90 * 24 * 60 * 60) # default ninety days
+    	self.assertEqual(self.poolFeeRate().call(), UNIT / 200) # default 50 basis points
+    	self.assertEqual(self.nominPool().call(), 0)
 
     	# ERC20FeeToken members
     	self.assertEqual(self.name().call(), "Ether-Backed USD Nomins")
@@ -87,3 +96,58 @@ class TestEtherNomin(unittest.TestCase):
     	self.assertEqual(self.transferFeeRate().call(), 2 * UNIT // 1000)
     	self.assertEqual(self.feeAuthority().call(), MASTER)
 
+    def test_getSetOracle(self):
+    	pre_oracle = self.oracle().call()
+    	new_oracle = W3.eth.accounts[1]
+
+    	# Only the owner must be able to set the oracle.
+    	assertTransactionReverts(self, self.setOracle(new_oracle), new_oracle)
+
+    	mine_tx(self.setOracle(new_oracle).transact({'from': MASTER}))
+    	self.assertEqual(self.oracle().call(), new_oracle)
+    	mine_tx(self.setOracle(pre_oracle).transact({'from': MASTER}))
+
+    def test_getSetCourt(self):
+    	pre_court = self.court().call()
+    	new_court = W3.eth.accounts[1]
+
+    	# Only the owner must be able to set the court.
+    	assertTransactionReverts(self, self.setOracle(new_court), new_court)
+
+    	mine_tx(self.setCourt(new_court).transact({'from': MASTER}))
+    	self.assertEqual(self.court().call(), new_court)
+    	mine_tx(self.setCourt(pre_court).transact({'from': MASTER}))
+
+    def test_getSetBeneficiary(self):
+    	pre_beneficiary = self.beneficiary().call()
+    	new_beneficiary = W3.eth.accounts[1]
+
+    	# Only the owner must be able to set the beneficiary.
+    	assertTransactionReverts(self, self.setBeneficiary(new_beneficiary), new_beneficiary)
+
+    	mine_tx(self.setBeneficiary(new_beneficiary).transact({'from': MASTER}))
+    	self.assertEqual(self.beneficiary().call(), new_beneficiary)
+    	mine_tx(self.setBeneficiary(pre_beneficiary).transact({'from': MASTER}))
+
+    def test_getSetPoolFeeRate(self):
+    	pre_rate = self.poolFeeRate().call()
+    	new_rate = UNIT // 10
+
+    	# Only the owner must be able to set the pool fee rate.
+    	assertTransactionReverts(self, self.setPoolFeeRate(new_rate), W3.eth.accounts[1])
+
+    	mine_tx(self.setPoolFeeRate(new_rate).transact({'from': MASTER}))
+    	self.assertEqual(self.poolFeeRate().call(), new_rate)
+    	mine_tx(self.setPoolFeeRate(pre_rate).transact({'from': MASTER}))
+"""
+    def test_getSetStalePeriod(self):
+    	pre_period = self.stalePeriod().call()
+    	new_period = UNIT // 10
+
+    	# Only the owner must be able to set the pool fee rate.
+    	assertTransactionReverts(self, self.setPoolFeeRate(new_rate), W3.eth.accounts[1])
+
+    	mine_tx(self.setPoolFeeRate(new_rate).transact({'from': MASTER}))
+    	self.assertEqual(self.poolFeeRate().call(), new_rate)
+    	mine_tx(self.setPoolFeeRate(pre_rate).transact({'from': MASTER}))
+"""
