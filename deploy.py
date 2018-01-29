52f449a4298aae11eb9f81981fe92d626bb96f45
DominicRomanowski <dominic.romanowski@gmail.com>
2018-01-30 09:31:51 +1100

    added comments for all functions in havven

--- deploy.py
+++ deploy.py
@@ -11,13 +11,13 @@ def deploy_havven():
     compiled = attempt(compile_contracts, [SOLIDITY_SOURCES], "Compiling contracts... ")
 
     # Deploy contracts
-    havven_contract, txr = attempt_deploy(compiled, 'Havven',
+    havven_contract, hvn_txr = attempt_deploy(compiled, 'Havven',
                                           MASTER, [MASTER])
-    nomin_contract, txr = attempt_deploy(compiled, 'EtherNomin',
+    nomin_contract, nom_txr = attempt_deploy(compiled, 'EtherNomin',
                                          MASTER,
                                          [havven_contract.address, MASTER, MASTER,
                                           1000*UNIT, MASTER])
-    court_contract, txr = attempt_deploy(compiled, 'Court',
+    court_contract, court_txr = attempt_deploy(compiled, 'Court',
                                          MASTER,
                                          [havven_contract.address, nomin_contract.address,
                                           MASTER])
@@ -29,7 +29,7 @@ def deploy_havven():
     attempt(mine_txs, [txs], "Linking contracts... ")
 
     print("\nDeployment complete.\n")
-    return havven_contract, nomin_contract, court_contract
+    return havven_contract, nomin_contract, court_contract, hvn_txr, nom_txr, court_txr
 
 
 if __name__ == "__main__":
