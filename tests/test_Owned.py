0ab33a56452913245539b77f34254dc3c0c229e8
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-31 10:18:58 +1100

    Cleaner lambdas in owned test.

--- tests/test_Owned.py
+++ tests/test_Owned.py
@@ -1,7 +1,7 @@
 import unittest
 
 from utils.deployutils import W3, compile_contracts, attempt_deploy, mine_tx, MASTER
-from utils.testutils import assertTransactionReverts
+from utils.testutils import assertReverts
 
 OWNED_SOURCE = "contracts/Owned.sol"
 
@@ -18,21 +18,24 @@ class TestOwned(unittest.TestCase):
         compiled = compile_contracts([OWNED_SOURCE])
         cls.owned, txr = attempt_deploy(compiled, 'Owned', MASTER, [MASTER])
 
+        cls.owner = lambda self: cls.owned.functions.owner().call()
+        cls.setOwner = lambda self, new_owner, sender: cls.owned.functions.setOwner(new_owner).transact({'from': sender})
+
     def test_owner_is_master(self):
-        self.assertEqual(self.owned.functions.owner().call(), MASTER)
+        self.assertEqual(self.owner(), MASTER)
 
     def test_change_owner(self):
-        old_owner = self.owned.functions.owner().call()
+        old_owner = self.owner()
         new_owner = W3.eth.accounts[1]
 
-        mine_tx(self.owned.functions.setOwner(new_owner).transact({'from': MASTER}))
-        self.assertEqual(self.owned.functions.owner().call(), new_owner)
+        mine_tx(self.setOwner(new_owner, MASTER))
+        self.assertEqual(self.owner(), new_owner)
 
-        mine_tx(self.owned.functions.setOwner(old_owner).transact({'from': new_owner}))
+        mine_tx(self.setOwner(old_owner, new_owner))
 
     def test_change_invalid_owner(self):
         invalid_account = W3.eth.accounts[1]
-        assertTransactionReverts(self, self.owned.functions.setOwner(invalid_account), invalid_account)
+        assertReverts(self, self.setOwner, [invalid_account, invalid_account])
 
 if __name__ == '__main__':
     unittest.main()
