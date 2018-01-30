7373601b5bfc18c56f651b376e5b6004b96f320f
ramboze <samuel.brooks@gmail.com>
2018-01-30 17:23:20 +1100

    updated licence headers

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -4,14 +4,12 @@ FILE INFORMATION
 -----------------------------------------------------------------
 file:       ERC20Token.sol
 version:    0.1
-author:     Block8 Technologies, in partnership with Havven
-
-            Anton Jurisevic
+author:     Anton Jurisevic
 
 date:       2018-1-16
 
-checked:    -
-approved:   -
+checked:    Mike Spain
+approved:   Samuel Brooks
 
 -----------------------------------------------------------------
 MODULE DESCRIPTION
@@ -47,11 +45,6 @@ SOFTWARE.
 RELEASE NOTES
 -----------------------------------------------------------------
 
------------------------------------------------------------------
-Block8 Technologies is accelerating blockchain technology
-by incubating meaningful next-generation businesses.
-Find out more at https://www.block8.io/
------------------------------------------------------------------
 */
 
 pragma solidity ^0.4.19;
