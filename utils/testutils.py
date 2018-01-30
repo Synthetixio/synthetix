0bbbd916883d45bd90546ed1a4379107e686a7df
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-31 10:07:34 +1100

    Generic reverts assertion and conversion of SafeDecimalMath to use it.

--- utils/testutils.py
+++ utils/testutils.py
@@ -11,3 +11,9 @@ def assertTransactionReverts(testcase, function, caller, gas=5000000):
         mine_tx(function.transact({'from': caller, 'gas': gas}))
     testcase.assertTrue("revert" in error.exception.args[0]['message'])
     testcase.assertEqual(-32000, error.exception.args[0]['code'])
+
+def assertReverts(testcase, function, args=[]):
+    with testcase.assertRaises(ValueError) as error:
+        function(*args)
+    testcase.assertTrue("revert" in error.exception.args[0]['message'])
+    testcase.assertEqual(-32000, error.exception.args[0]['code'])
