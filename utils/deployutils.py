60c8abe3beadb703113b4d100075af64a59fe15e
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-31 14:08:53 +1100

    snapshot functions.

--- utils/deployutils.py
+++ utils/deployutils.py
@@ -113,3 +113,14 @@ def attempt_deploy(compiled_sol, contract_name, deploy_account, constructor_args
                    [compiled_sol, contract_name, deploy_account, constructor_args],
                    f"Deploying {contract_name}... ",
                    print_status=print_status, print_exception=print_exception)
+
+
+def snapshot():
+    x = W3.providers[0].make_request("evm_snapshot", [])
+    force_mine_block()
+    return x
+
+
+def restore_snapshot(snapshot):
+    W3.providers[0].make_request("evm_revert", [snapshot['result']])
+    force_mine_block()
