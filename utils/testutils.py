60a094a8a1b77562b79b6b23d9917a9c9655f7e6
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-01-31 15:24:32 +1100

    Block time test function.

--- utils/testutils.py
+++ utils/testutils.py
@@ -1,7 +1,12 @@
-from utils.deployutils import mine_tx
+from utils.deployutils import mine_tx, W3
 
 def assertReverts(testcase, function, args=[]):
     with testcase.assertRaises(ValueError) as error:
         function(*args)
     testcase.assertTrue("revert" in error.exception.args[0]['message'])
     testcase.assertEqual(-32000, error.exception.args[0]['code'])
+
+def block_time(block_num=None):
+    if block_num is None:
+        block_num = W3.eth.blockNumber
+    return W3.eth.getBlock(block_num)['timestamp']
