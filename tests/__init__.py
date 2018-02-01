6de33ea3bbb7aadbd55812877bc7aba2fe9185a6
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-02-01 11:32:54 +1100

    Rename test_ERC20 to test_ERC20Tokens

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
 import tests.test_Court
 import tests.test_Deploy
-import tests.test_ERC20
+import tests.test_ERC20Tokens
 import tests.test_EtherNomin
 import tests.test_Havven
 import tests.test_Owned
