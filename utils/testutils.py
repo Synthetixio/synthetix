9148d15c716b83c00a3cb0685a78eb9e1d538871
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-02-02 01:11:32 +1100

    EtherNomins purchase cost and staleness tests.

--- utils/testutils.py
+++ utils/testutils.py
@@ -4,7 +4,8 @@ def assertReverts(testcase, function, args=[]):
     with testcase.assertRaises(ValueError) as error:
         function(*args)
     testcase.assertTrue("revert" in error.exception.args[0]['message'])
-    testcase.assertEqual(-32000, error.exception.args[0]['code'])
+    # ganache-cli beta 6.1.0 does not include a code field.
+    # testcase.assertEqual(-32000, error.exception.args[0]['code'])
 
 def block_time(block_num=None):
     if block_num is None:
