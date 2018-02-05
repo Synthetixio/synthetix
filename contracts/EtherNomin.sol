7a73249c90853468e0f190b6ae674d6d58ce17a5
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-02-05 19:17:00 +1100

    Remove redundant licence headers.

--- contracts/EtherNomin.sol
+++ contracts/EtherNomin.sol
@@ -3,7 +3,7 @@
 FILE INFORMATION
 -----------------------------------------------------------------
 file:       EtherNomin.sol
-version:    0.2
+version:    0.3
 author:     Anton Jurisevic
 
 date:       2018-1-16
@@ -53,33 +53,6 @@ This liquidation period may be extended up to a maximum of 180 days.
 If the contract is recollateralised, the owner may terminate liquidation.
 
 -----------------------------------------------------------------
-LICENCE INFORMATION
------------------------------------------------------------------
-
-Copyright (c) 2018 Havven.io
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
------------------------------------------------------------------
-RELEASE NOTES
------------------------------------------------------------------
-
 */
 
 pragma solidity ^0.4.19;
