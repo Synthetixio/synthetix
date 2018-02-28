85ee73128c7464b4fcf35dfebb4d543e6a09496c
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-02-28 15:29:54 +1100

    Renamed ERC20 token contracts now that they fail to comply with the standard.

--- contracts/TokenState.sol
+++ contracts/TokenState.sol
@@ -2,7 +2,7 @@
 -----------------------------------------------------------------
 FILE INFORMATION
 -----------------------------------------------------------------
-file:       ERC20State.sol
+file:       TokenState.sol
 version:    0.3
 author:     Dominic Romanowski
 
@@ -17,13 +17,17 @@ MODULE DESCRIPTION
 
 A contract that holds the state of an ERC20 compliant token.
 
-This contract is used side by side with the ERC20Token that is implemented for Havven. It provides
-an easy way to upgrade the logic of a contract while maintaining all user balances and allowances,
-to make the changeover as easy as possible.
+This contract is used side by side with the StatefulProxyToken
+that is implemented for Havven. It provides an easy way to
+upgrade the logic of a contract while maintaining all user
+balances and allowances, to make the changeover as easy as
+possible.
 
-The way this would work, is the first deployed contract would create this state contract, and use it
-as the store of balances. When a new contract is deployed, it would link to the existing state contract,
-then the Owner of the state contract would change the associated contract to be the new one.
+The way this would work, is the first deployed contract would
+create this state contract, and use it as the store of balances.
+When a new contract is deployed, it would link to the existing
+state contract, then the Owner of the state contract would
+change the associated contract to be the new one.
 
 -----------------------------------------------------------------
 */
@@ -34,7 +38,7 @@ pragma solidity ^0.4.20;
 import "contracts/Owned.sol";
 
 
-contract ERC20State is Owned {
+contract TokenState is Owned {
 
     // the address of the contract that can modify balances and allowances
     // this can only be changed by the owner of this contract
@@ -44,7 +48,7 @@ contract ERC20State is Owned {
     mapping(address => uint) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    function ERC20State(
+    function TokenState(
         address _owner, uint initialSupply,
         address initialBeneficiary, address _associatedContract
     )
