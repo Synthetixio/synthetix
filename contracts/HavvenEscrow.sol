84d9914a0f17bb87a6b9c146c2d6cf7e423e5e68
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-03-12 02:06:26 +1100

    Add ERC20 balanceOf function to escrow contract.

--- contracts/HavvenEscrow.sol
+++ contracts/HavvenEscrow.sol
@@ -82,6 +82,15 @@ contract HavvenEscrow is Owned, LimitedSetup(8 weeks), SafeDecimalMath {
 
     /* ========== VIEW FUNCTIONS ========== */
 
+    /* A simple alias to totalVestedAccountBalance: provides ERC20 balance integration. */
+    function balanceOf(address account)
+        public
+        view
+        returns (uint)
+    {
+        return totalVestedAccountBalance[account];
+    }
+
     /* The number of vesting dates in an account's schedule. */
     function numVestingEntries(address account)
         public
