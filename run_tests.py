48a5ed099d1aecf7afceb3d9a1cf0963f3d3ba16
Anton Jurisevic <ajur4521@uni.sydney.edu.au>
2018-03-26 23:32:14 +1100

    Properly defer RPC connection for tests.

--- run_tests.py
+++ run_tests.py
@@ -5,22 +5,28 @@ import time
 from unittest import TestSuite, TestLoader, TextTestRunner
 from utils.generalutils import load_test_settings, ganache_error_message
 
-from tests import *
 
 if __name__ == '__main__':
     num_agents = "120"
     eth_per_agent = "1000000000000"
 
-    print("Launching ganache...", end="")
+    print("Launching ganache", end="", flush=True)
     DEVNULL = open(os.devnull, 'wb')
     command = ["ganache-cli", "-a", num_agents, "-e", eth_per_agent]
     try:
         process = subprocess.Popen(command, stdout=DEVNULL, stderr=subprocess.STDOUT)
     except Exception as e:
         raise Exception(ganache_error_message)
+
     # Wait for ganache to initialise properly.
-    time.sleep(5)
-    print("Done.")
+    time.sleep(1)
+    for _ in range(3):
+        print(".", end="", flush=True)
+        time.sleep(1)
+    print(" Done.")
+
+    # Import here to only initiate RPC connection after ganache is running.
+    from tests import *
 
     test_settings = load_test_settings()
 
