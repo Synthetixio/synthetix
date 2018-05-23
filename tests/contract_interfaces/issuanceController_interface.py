04f3e40889827cc43d3c7450471bde17be37c54f
Gavin Conway <gavin.conway@coina.ge>
2018-05-23 11:02:31 +1000

    Added tests for setting of the oracle and price stale period

--- tests/contract_interfaces/issuanceController_interface.py
+++ tests/contract_interfaces/issuanceController_interface.py
@@ -6,8 +6,20 @@ class IssuanceControllerInterface():
         self.contract_name = name
 
         self.priceStalePeriod = lambda: self.contract.functions.priceStalePeriod().call()
-        self.setSomeValue = lambda sender, someValue: mine_tx(
-            self.contract.functions.setSomeValue(someValue).transact({'from': sender}), "setSomeValue", self.contract_name
+        self.oracle = lambda: self.contract.functions.oracle().call()
+        self.owner = lambda: self.contract.functions.owner().call()
+        self.ethPrice = lambda: self.contract.functions.ethPrice().call()
+        self.havvenPrice = lambda: self.contract.functions.havvenPrice().call()
+        self.lastPriceUpdateTime = lambda: self.contract.functions.lastPriceUpdateTime().call()
+        self.selfDestructDelay = lambda: self.contract.functions.selfDestructDelay().call()
+        self.selfDestructBeneficiary = lambda: self.contract.functions.selfDestructBeneficiary().call()
+        self.priceStalePeriod = lambda: self.contract.functions.priceStalePeriod().call()
+
+        self.setOracle = lambda sender, newAddress: mine_tx(
+            self.contract.functions.setOracle(newAddress).transact({'from': sender}), "setOracle", self.contract_name
+        )
+        self.setPriceStalePeriod = lambda sender, newPriceStalePeriod: mine_tx(
+            self.contract.functions.setPriceStalePeriod(newPriceStalePeriod).transact({'from': sender}), "setPriceStalePeriod", self.contract_name
         )
         self.buy = lambda sender, value: mine_tx(
             self.contract.functions.buy().transact({
--- tests/test_IssuanceController.py
+++ tests/test_IssuanceController.py
@@ -1,6 +1,6 @@
 from utils.deployutils import (
     W3, UNIT, MASTER, DUMMY,
-#     fresh_account, fresh_accounts,
+    fresh_account, fresh_accounts,
     mine_tx, attempt_deploy, mine_txs,
     take_snapshot, restore_snapshot
 )
@@ -8,7 +8,6 @@ from utils.testutils import (
     HavvenTestCase, ZERO_ADDRESS,
 )
 from tests.contract_interfaces.issuanceController_interface import IssuanceControllerInterface
-
 
 def setUpModule():
     print("Testing IssuanceController...")
@@ -40,29 +39,83 @@ class TestIssuanceController(HavvenTestCase):
 
         compiled, cls.event_maps = cls.compileAndMapEvents(sources)
 
-        issuanceControllerContract, _ = attempt_deploy(compiled, 'IssuanceController', MASTER,
-                                                    [MASTER, DUMMY, 100 * 60, MASTER, 500 * (10 ** 18), int(0.65 * (10 ** 18))])
+        issuanceControllerContract, _ = attempt_deploy(
+            compiled, 'IssuanceController', MASTER,
+                [
+                    cls.contractOwner,
+                    cls.beneficiary,
+                    cls.delay,
+                    cls.oracleAddress,
+                    cls.ethPrice,
+                    cls.havvenPrice
+                ]
+            )
         
         issuanceController = W3.eth.contract(address=issuanceControllerContract.address, abi=compiled['IssuanceController']['abi'])
-
         return issuanceControllerContract, issuanceController
 
     @classmethod
     def setUpClass(cls):
+        cls.contractOwner = MASTER
+        cls.oracleAddress = fresh_accounts(1)[0]
+        cls.beneficiary = fresh_accounts(1)[0]
+        cls.delay = 100 * 60
+        cls.ethPrice = 500 * (10 ** 18)
+        cls.havvenPrice = int(0.65 * (10 ** 18))
+        cls.priceStalePeriod = 3 * 60 * 60
         cls.issuanceControllerContract, cls.issuanceController = cls.deployContracts()
-
         cls.issuanceController = IssuanceControllerInterface(cls.issuanceControllerContract, "IssuanceController")
 
+    def test_constructor(self):
+        self.assertEqual(self.issuanceController.oracle(), self.oracleAddress)
+        self.assertEqual(self.issuanceController.selfDestructBeneficiary(), self.beneficiary)
+        self.assertEqual(self.issuanceController.selfDestructDelay(), self.delay)
+        self.assertEqual(self.issuanceController.oracle(), self.oracleAddress)
+        self.assertEqual(self.issuanceController.ethPrice(), self.ethPrice)
+        self.assertEqual(self.issuanceController.havvenPrice(), self.havvenPrice)
+        self.assertEqual(self.issuanceController.priceStalePeriod(), self.priceStalePeriod)
 
-    # def test_constructor(self):
-    #     TODO
+    # Oracle address setter and getter tests
 
-    def test_sample(self):
-        self.assertEqual(self.issuanceController.priceStalePeriod(), 10800)
+    def test_getOracleAddress(self):
+        oracleAddress = self.issuanceController.oracle()
+        self.assertEqual(oracleAddress, self.oracleAddress)
 
-    # def test_sample2(self):
-    #     txr = self.issuanceController.setSomeValue(MASTER, 950)
-    #     self.assertEqual(self.issuanceController.getSomeValue(), 950)
+    def test_setOracleAddress(self):
+        newOracleAddress = fresh_accounts(1)[0]
+        self.issuanceController.setOracle(self.contractOwner, newOracleAddress)
+        oracleAddressToCheck = self.issuanceController.oracle()
+        self.assertEqual(newOracleAddress, oracleAddressToCheck)
+
+    def test_cannotSetOracleIfUnauthorised(self):
+        newOracleAddress, notOwner = fresh_accounts(2)
+        originalOracleAddress = self.issuanceController.oracle()
+        self.assertReverts(self.issuanceController.setOracle, notOwner, newOracleAddress)
+        oracleAddressToCheck = self.issuanceController.oracle()
+        self.assertEqual(oracleAddressToCheck, originalOracleAddress)
+
+    # Price stale period setter and getter tests
+
+    def test_getPriceStalePeriod(self):
+        priceStalePeriod = self.issuanceController.priceStalePeriod()
+        self.assertEqual(priceStalePeriod, self.priceStalePeriod)
+
+    def test_setPriceStalePeriod(self):
+        newPriceStalePeriod = 400 * 60 * 60
+        self.issuanceController.setPriceStalePeriod(self.contractOwner, newPriceStalePeriod)
+        priceStalePeriodToCheck = self.issuanceController.priceStalePeriod()
+        self.assertEqual(newPriceStalePeriod, priceStalePeriodToCheck)
+
+    def test_cannotSetPriceStalePeriodIfUnauthorised(self):
+        notOwner = fresh_accounts(1)[0]
+        originalPriceStalePeriod = self.issuanceController.priceStalePeriod()
+        newPriceStalePeriod = originalPriceStalePeriod + 100
+        self.assertReverts(self.issuanceController.setPriceStalePeriod, notOwner, newPriceStalePeriod)
+        priceStalePeriodToCheck = self.issuanceController.priceStalePeriod()
+        self.assertEqual(originalPriceStalePeriod, priceStalePeriodToCheck)
+
+    # Update prices (aka exchange rate) setter and getter tests
+    # TODO
 
     # def test_etherChargedForNominsIsCorrect(self):
     #     amountOfNominsToBuy = 67
