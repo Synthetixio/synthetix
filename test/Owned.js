5b9dc9fbd70a7fc5c201c96beb26d20eda9ac01e
Kevin Brown <kevin.brown@coina.ge>
2018-09-11 15:12:35 +1000

    Owned tests work.

---
+++ test/Owned.js
@@ -0,0 +1,78 @@
+const Owned = artifacts.require('Owned');
+const ZERO_ADDRESS = '0x' + '0'.repeat(40);
+
+contract('Owned - Test contract deployment', function(accounts) {
+	const [deployerAccount, account1] = accounts;
+
+	it('should revert when owner parameter is passed the zero address', async function() {
+		try {
+			await Owned.new(ZERO_ADDRESS, { from: deployerAccount });
+		} catch (error) {
+			assert.include(error.message, 'revert');
+		}
+	});
+
+	// TODO check events on contract creation
+	it('should set owner address on deployment', async function() {
+		const ownedContractInstance = await Owned.new(account1, { from: deployerAccount });
+		const owner = await ownedContractInstance.owner();
+		assert.equal(owner, account1);
+	});
+});
+
+contract('Owned - Pre deployed contract', async function(accounts) {
+	const [deployerAccount, account1, account2, account3, account4] = accounts;
+
+	it('should not nominate new owner when not invoked by current contract owner', async function() {
+		let ownedContractInstance = await Owned.deployed();
+		const nominatedOwner = account3;
+		try {
+			await ownedContractInstance.nominateNewOwner(nominatedOwner, { from: account2 });
+		} catch (error) {
+			assert.include(error.message, 'revert');
+		}
+		const nominatedOwnerFrmContract = await ownedContractInstance.nominatedOwner();
+		assert.equal(nominatedOwnerFrmContract, ZERO_ADDRESS);
+	});
+
+	it('should nominate new owner when invoked by current contract owner', async function() {
+		let ownedContractInstance = await Owned.deployed();
+		const nominatedOwner = account2;
+
+		const txn = await ownedContractInstance.nominateNewOwner(nominatedOwner, { from: account1 });
+		assert.equal(txn.logs[0].event, 'OwnerNominated');
+		assert.equal(txn.logs[0].args.newOwner, nominatedOwner);
+
+		const nominatedOwnerFrmContract = await ownedContractInstance.nominatedOwner();
+		assert.equal(nominatedOwnerFrmContract, nominatedOwner);
+	});
+
+	it('should not accept new owner nomination when not invoked by nominated owner', async function() {
+		let ownedContractInstance = await Owned.deployed();
+		const nominatedOwner = account3;
+
+		try {
+			await ownedContractInstance.acceptOwnership({ from: account4 });
+		} catch (error) {
+			assert.include(error.message, 'revert');
+		}
+		const owner = await ownedContractInstance.owner();
+		assert.notEqual(owner, nominatedOwner);
+	});
+
+	it('should accept new owner nomination when invoked by nominated owner', async function() {
+		let ownedContractInstance = await Owned.deployed();
+		const nominatedOwner = account2;
+
+		const txn = await ownedContractInstance.acceptOwnership({ from: account2 });
+		assert.equal(txn.logs[0].event, 'OwnerChanged');
+		assert.equal(txn.logs[0].args.oldOwner, account1);
+		assert.equal(txn.logs[0].args.newOwner, account2);
+
+		const owner = await ownedContractInstance.owner();
+		const nominatedOwnerFromContact = await ownedContractInstance.nominatedOwner();
+
+		assert.equal(owner, nominatedOwner);
+		assert.equal(nominatedOwnerFromContact, ZERO_ADDRESS);
+	});
+});
