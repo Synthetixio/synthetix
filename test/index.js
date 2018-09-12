3e5eb030527685ed36c18dc287e0d49294fdd598
Kevin Brown <kevin.brown@coina.ge>
2018-09-12 17:54:13 +1000

    Fixed capitalisation to match the rest of the patterns.

--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@ const {
 // So we don't have to constantly import our assert helpers everywhere
 // we'll just tag them onto the assert object for easy access.
 assert.eventEqual = assertEventEqual;
-assert.BNEqual = assertBNEqual;
+assert.bnEqual = assertBNEqual;
 assert.etherEqual = assertEtherEqual;
 
 // And this is our test sandboxing. It snapshots and restores between each test.
