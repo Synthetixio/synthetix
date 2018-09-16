96ec3154411b6f46df72397353448d5a8e71ff17
Gavin Conway <gavin.conway@coina.ge>
2018-09-16 19:42:53 +1000

    Added testing of last update time

--- test/ExchangeRates.js
+++ test/ExchangeRates.js
@@ -1,5 +1,5 @@
 const ExchangeRates = artifacts.require('ExchangeRates');
-const { currentTime } = require('../utils/testUtils');
+const { currentTime, fastForward } = require('../utils/testUtils');
 
 // Helper functions
 
@@ -28,6 +28,7 @@ contract('Exchange Rates', async function(accounts) {
 	// Contract Creation
 
 	it('should set constructor params on deployment', async function() {
+		const creationTime = currentTime();
 		const instance = await ExchangeRates.new(
 			owner,
 			oracle,
@@ -46,9 +47,25 @@ contract('Exchange Rates', async function(accounts) {
 
 		// Ensure that when the rate isn't found, 0 is returned as the exchange rate.
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('OTHER')), '0');
+
+		const lastUpdatedTimeNUSD = await instance.lastRateUpdateTimes.call(
+			web3.utils.asciiToHex('nUSD')
+		);
+		assert.isAtLeast(lastUpdatedTimeNUSD.toNumber(), creationTime);
+
+		const lastUpdatedTimeOTHER = await instance.lastRateUpdateTimes.call(
+			web3.utils.asciiToHex('OTHER')
+		);
+		assert.equal(lastUpdatedTimeOTHER.toNumber(), 0);
+
+		const lastUpdatedTimeHAV = await instance.lastRateUpdateTimes.call(
+			web3.utils.asciiToHex('HAV')
+		);
+		assert.isAtLeast(lastUpdatedTimeHAV.toNumber(), creationTime);
 	});
 
 	it('two of the same currencies in same array should mean that the second one overrides', async function() {
+		const creationTime = currentTime();
 		const firstAmount = '4.33';
 		const secondAmount = firstAmount + 10;
 		const instance = await ExchangeRates.new(
@@ -63,6 +80,9 @@ contract('Exchange Rates', async function(accounts) {
 
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('CART')), secondAmount);
 		assert.etherNotEqual(await instance.rates.call(web3.utils.asciiToHex('CART')), firstAmount);
+
+		const lastUpdatedTime = await instance.lastRateUpdateTimes.call(web3.utils.asciiToHex('CART'));
+		assert.isAtLeast(lastUpdatedTime.toNumber(), creationTime);
 	});
 
 	it('should revert when number of currency keys > new rates length on create', async function() {
@@ -84,6 +104,7 @@ contract('Exchange Rates', async function(accounts) {
 	});
 
 	it('should truncate to 4 bytes if currency key > 4 bytes on create', async function() {
+		const creationTime = currentTime();
 		const amount = '4.33';
 		const instance = await ExchangeRates.new(
 			owner,
@@ -98,6 +119,9 @@ contract('Exchange Rates', async function(accounts) {
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('CATHERINE')), amount);
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('CATH')), amount);
 		assert.etherNotEqual(await instance.rates.call(web3.utils.asciiToHex('CAT')), amount);
+
+		const lastUpdatedTime = await instance.lastRateUpdateTimes.call(web3.utils.asciiToHex('CATH'));
+		assert.isAtLeast(lastUpdatedTime.toNumber(), creationTime);
 	});
 
 	it('shouldnt be able to set exchange rate to 0 on create', async function() {
@@ -115,6 +139,7 @@ contract('Exchange Rates', async function(accounts) {
 	});
 
 	it('should be able to handle lots of currencies on creation', async function() {
+		const creationTime = currentTime();
 		const numberOfCurrencies = 100;
 		const { currencyKeys, rates } = createRandomKeysAndRates(numberOfCurrencies);
 
@@ -124,6 +149,8 @@ contract('Exchange Rates', async function(accounts) {
 
 		for (let i = 0; i < numberOfCurrencies; i++) {
 			assert.equal(await instance.rates.call(currencyKeys[i]), rates[i]);
+			const lastUpdatedTime = await instance.lastRateUpdateTimes.call(currencyKeys[i]);
+			assert.isAtLeast(lastUpdatedTime.toNumber(), creationTime);
 		}
 	});
 
@@ -133,6 +160,8 @@ contract('Exchange Rates', async function(accounts) {
 		const instance = await ExchangeRates.deployed();
 		const timeSent = currentTime();
 
+		await fastForward(1);
+
 		await instance.updateRates(
 			[web3.utils.asciiToHex('lABC'), web3.utils.asciiToHex('lDEF'), web3.utils.asciiToHex('lGHI')],
 			[
@@ -143,22 +172,44 @@ contract('Exchange Rates', async function(accounts) {
 			timeSent,
 			{ from: oracle }
 		);
+
+		await fastForward(10);
+		const updatedTime = timeSent + 10;
 
 		const updatedRate = '64.33';
 		await instance.updateRates(
 			[web3.utils.asciiToHex('lABC')],
 			[web3.utils.toWei(updatedRate, 'ether')],
-			timeSent,
+			updatedTime,
 			{ from: oracle }
 		);
+
+		const updatedTimelDEF = await instance.lastRateUpdateTimes.call(web3.utils.asciiToHex('lDEF'));
+		const updatedTimelGHI = await instance.lastRateUpdateTimes.call(web3.utils.asciiToHex('lGHI'));
 
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('lABC')), updatedRate);
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('lDEF')), '2.4');
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('lGHI')), '3.5');
+
+		const lastUpdatedTimeLABC = await instance.lastRateUpdateTimes.call(
+			web3.utils.asciiToHex('lABC')
+		);
+		assert.equal(lastUpdatedTimeLABC.toNumber(), updatedTime);
+		const lastUpdatedTimeLDEF = await instance.lastRateUpdateTimes.call(
+			web3.utils.asciiToHex('lDEF')
+		);
+		assert.equal(lastUpdatedTimeLDEF.toNumber(), updatedTimelDEF.toNumber());
+		const lastUpdatedTimeLGHI = await instance.lastRateUpdateTimes.call(
+			web3.utils.asciiToHex('lGHI')
+		);
+		assert.equal(lastUpdatedTimeLGHI.toNumber(), updatedTimelGHI.toNumber());
 	});
 
 	it('should be able to update rates of all currencies', async function() {
 		const instance = await ExchangeRates.deployed();
+		const timeSent = currentTime();
+
+		await fastForward(1);
 
 		await instance.updateRates(
 			[web3.utils.asciiToHex('lABC'), web3.utils.asciiToHex('lDEF'), web3.utils.asciiToHex('lGHI')],
@@ -167,9 +218,12 @@ contract('Exchange Rates', async function(accounts) {
 				web3.utils.toWei('2.4', 'ether'),
 				web3.utils.toWei('3.5', 'ether'),
 			],
-			currentTime(),
+			timeSent,
 			{ from: oracle }
 		);
+
+		await fastForward(5);
+		const updatedTime = timeSent + 5;
 
 		const updatedRate1 = '64.33';
 		const updatedRate2 = '2.54';
@@ -181,13 +235,26 @@ contract('Exchange Rates', async function(accounts) {
 				web3.utils.toWei(updatedRate2, 'ether'),
 				web3.utils.toWei(updatedRate3, 'ether'),
 			],
-			currentTime(),
+			updatedTime,
 			{ from: oracle }
 		);
 
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('lABC')), updatedRate1);
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('lDEF')), updatedRate2);
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('lGHI')), updatedRate3);
+
+		const lastUpdatedTimeLABC = await instance.lastRateUpdateTimes.call(
+			web3.utils.asciiToHex('lABC')
+		);
+		assert.equal(lastUpdatedTimeLABC.toNumber(), updatedTime);
+		const lastUpdatedTimeLDEF = await instance.lastRateUpdateTimes.call(
+			web3.utils.asciiToHex('lDEF')
+		);
+		assert.equal(lastUpdatedTimeLDEF.toNumber(), updatedTime);
+		const lastUpdatedTimeLGHI = await instance.lastRateUpdateTimes.call(
+			web3.utils.asciiToHex('lGHI')
+		);
+		assert.equal(lastUpdatedTimeLGHI.toNumber(), updatedTime);
 	});
 
 	it('should emit RatesUpdated event when rate updated', async function() {
@@ -217,26 +284,40 @@ contract('Exchange Rates', async function(accounts) {
 		const numberOfCurrencies = 150;
 		const { currencyKeys, rates } = createRandomKeysAndRates(numberOfCurrencies);
 
-		await instance.updateRates(currencyKeys, rates, currentTime(), { from: oracle });
+		const updatedTime = currentTime();
+		await instance.updateRates(currencyKeys, rates, updatedTime, { from: oracle });
 
 		for (let i = 0; i < numberOfCurrencies; i++) {
 			assert.equal(await instance.rates.call(currencyKeys[i]), rates[i]);
+			const lastUpdatedTime = await instance.lastRateUpdateTimes.call(currencyKeys[i]);
+			assert.equal(lastUpdatedTime.toNumber(), updatedTime);
 		}
 	});
 
 	it('should truncate to 4 bytes if currency key > 4 bytes on update', async function() {
 		const instance = await ExchangeRates.deployed();
 		const rate = '4.33';
+		const timeSent = currentTime();
+		const beforeUpdateTime = await instance.lastRateUpdateTimes.call(
+			web3.utils.asciiToHex('CATHERINE')
+		);
+
 		await instance.updateRates(
 			[web3.utils.asciiToHex('CATHERINE')],
 			[web3.utils.toWei(rate, 'ether')],
-			currentTime(),
+			timeSent,
 			{ from: oracle }
 		);
 
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('CATHERINE')), rate);
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('CATH')), rate);
 		assert.etherNotEqual(await instance.rates.call(web3.utils.asciiToHex('CAT')), rate);
+
+		const lastUpdatedTime = await instance.lastRateUpdateTimes.call(
+			web3.utils.asciiToHex('CATHERINE')
+		);
+		assert.equal(lastUpdatedTime.toNumber(), timeSent);
+		assert.notEqual(lastUpdatedTime.toNumber(), beforeUpdateTime);
 	});
 
 	it('should revert when currency keys length != new rates length on update', async function() {
@@ -301,14 +382,25 @@ contract('Exchange Rates', async function(accounts) {
 		assert.etherNotEqual(await instance.rates.call(web3.utils.asciiToHex('GOLD')), '10');
 		assert.etherNotEqual(await instance.rates.call(web3.utils.asciiToHex('FOOL')), '0.9');
 
+		const updatedTime = currentTime();
+
 		await instance.updateRates(
 			[web3.utils.asciiToHex('GOLD'), web3.utils.asciiToHex('FOOL')],
 			[web3.utils.toWei('10', 'ether'), web3.utils.toWei('0.9', 'ether')],
-			currentTime(),
+			updatedTime,
 			{ from: oracle }
 		);
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('GOLD')), '10');
 		assert.etherEqual(await instance.rates.call(web3.utils.asciiToHex('FOOL')), '0.9');
+
+		const lastUpdatedTimeGOLD = await instance.lastRateUpdateTimes.call(
+			web3.utils.asciiToHex('GOLD')
+		);
+		assert.equal(lastUpdatedTimeGOLD.toNumber(), updatedTime);
+		const lastUpdatedTimeFOOL = await instance.lastRateUpdateTimes.call(
+			web3.utils.asciiToHex('FOOL')
+		);
+		assert.equal(lastUpdatedTimeFOOL.toNumber(), updatedTime);
 	});
 
 	it('should not be able to update rates if they are too far in the future', async function() {
@@ -323,8 +415,6 @@ contract('Exchange Rates', async function(accounts) {
 			)
 		);
 	});
-
-	// TODO: Check that lastRateUpdateTimes has been updated
 
 	// Changing the Oracle address
 
