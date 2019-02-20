da6ab29d8f7bf6ffe3ff57c600cba8b9f43fcf20
Jackson C <jackson@synthetix.io>
2019-02-20 17:03:20 +1100

    fix up linting

--- utils/shouldFail.js
+++ utils/shouldFail.js
@@ -1,31 +1,30 @@
-const should = require('chai')
-  .should();
+const should = require('chai').should();
 
-async function shouldFailWithMessage (promise, message) {
-  try {
-    await promise;
-  } catch (error) {
-    error.message.should.include(message, 'Wrong failure type');
-    return;
-  }
+async function shouldFailWithMessage(promise, message) {
+	try {
+		await promise;
+	} catch (error) {
+		error.message.should.include(message, 'Wrong failure type');
+		return;
+	}
 
-  should.fail(`Expected '${message}' failure not received`);
+	should.fail(`Expected '${message}' failure not received`);
 }
 
-async function reverting (promise) {
-  await shouldFailWithMessage(promise, 'revert');
+async function reverting(promise) {
+	await shouldFailWithMessage(promise, 'revert');
 }
 
-async function throwing (promise) {
-  await shouldFailWithMessage(promise, 'invalid opcode');
+async function throwing(promise) {
+	await shouldFailWithMessage(promise, 'invalid opcode');
 }
 
-async function outOfGas (promise) {
-  await shouldFailWithMessage(promise, 'out of gas');
+async function outOfGas(promise) {
+	await shouldFailWithMessage(promise, 'out of gas');
 }
 
 module.exports = {
-  reverting,
-  throwing,
-  outOfGas,
+	reverting,
+	throwing,
+	outOfGas,
 };
