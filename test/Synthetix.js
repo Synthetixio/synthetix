f31f0e9305bcde1ccb21202fa5ab7133ee111f22
hav-noms <clinton@havven.iogit config --global user.name hav-nomsgit config --global user.email clinton@havven.io>
2019-03-07 17:29:51 +1100

    RewardEscrow tests complete

--- test/Synthetix.js
+++ test/Synthetix.js
@@ -1981,7 +1981,7 @@ contract('Synthetix', async function(accounts) {
 	});
 
 	// Inflationary supply of Synthetix
-	it.only('should allow synthetix contract to mint new supply based on inflationary schedule', async function() {
+	it('should allow synthetix contract to mint new supply based on inflationary schedule', async function() {
 		// Issue
 		const expectedSupplyToMint = toUnit('100000000');
 
