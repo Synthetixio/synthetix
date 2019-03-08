b9e03da5f930983f5774537a12758bfb4e69be8a
Jackson C <jackson@synthetix.io>
2019-03-08 12:53:42 +1100

    fix lint

--- test/Synthetix.js
+++ test/Synthetix.js
@@ -1984,30 +1984,29 @@ contract('Synthetix', async function(accounts) {
 	});
 
 	// Inflationary supply of Synthetix
-	// it('should allow synthetix contract to mint new supply based on inflationary schedule', async function() {
-	// 	// Issue
-	// 	const weeklyIssuance = (75000000 / 52).toPrecision(18);
-	// 	const expectedSupplyToMint = toUnit(weeklyIssuance);
-	//
-	// 	// fast forward EVM to Week 1 in Year 2 schedule starting at UNIX 1552435200+
-	// 	await fastForwardTo(new Date(1552435210 * 1000));
-	//
-	// 	const existingSupply = await synthetix.totalSupply();
-	// 	const currentFeePoolBalance = await synthetix.balanceOf(feePool.address);
-	//
-	// 	await synthetix.mint();
-	//
-	// 	const newTotalSupply = await synthetix.totalSupply();
-	//
-	// 	// Expect supply schedule is updated with new values
-	// 	const currentSchedule = await supplySchedule.getCurrentSchedule();
-	// 	// const schedule = await supplySchedule.schedules[toUnit(currentSchedule)];
-	//
-	// 	console.log('supplySchedule', currentSchedule);
-	// 	assert.bnEqual(newTotalSupply, existingSupply.add(web3.utils.toBN(expectedSupplyToMint)));
-	// 	assert.bnEqual(
-	// 		await synthetix.balanceOf(feePool.address),
-	// 		currentFeePoolBalance.add(web3.utils.toBN(expectedSupplyToMint))
-	// 	);
-	// });
+	it('should allow synthetix contract to mint new supply based on inflationary schedule', async function() {
+		// Issue
+		const weeklyIssuance = (75000000 / 52).toPrecision(18);
+		const expectedSupplyToMint = toUnit(weeklyIssuance);
+
+		// fast forward EVM to Week 1 in Year 2 schedule starting at UNIX 1552435200+
+		await fastForwardTo(new Date(1552435210 * 1000));
+
+		const existingSupply = await synthetix.totalSupply();
+		const currentFeePoolBalance = await synthetix.balanceOf(feePool.address);
+
+		await synthetix.mint();
+
+		const newTotalSupply = await synthetix.totalSupply();
+
+		// Expect supply schedule is updated with new values
+		const currentSchedule = await supplySchedule.getCurrentSchedule();
+
+		console.log('supplySchedule', currentSchedule);
+		assert.bnEqual(newTotalSupply, existingSupply.add(web3.utils.toBN(expectedSupplyToMint)));
+		assert.bnEqual(
+			await synthetix.balanceOf(feePool.address),
+			currentFeePoolBalance.add(web3.utils.toBN(expectedSupplyToMint))
+		);
+	});
 });
