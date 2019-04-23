61c4555414d9e4ff3a1d823733179c0282ede77d
justin j. moses <justinjmoses@gmail.com>
2019-04-23 16:46:26 -0400

    New owner function

--- publish/index.js
+++ publish/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const readline = require('readline');
 const program = require('commander');
-const { gray, green, yellow, red, cyan } = require('chalk');
+const { black, gray, green, yellow, red, cyan, bgYellow } = require('chalk');
 const { table } = require('table');
 require('pretty-error').start();
 require('dotenv').config();
@@ -60,12 +60,20 @@ const loadAndCheckRequiredSources = ({ deploymentPath, network }) => {
 	}
 	const deployment = JSON.parse(fs.readFileSync(deploymentFile));
 
+	const ownerActionsFile = path.join(deploymentPath, OWNER_ACTIONS_FILENAME);
+	if (!fs.existsSync(ownerActionsFile)) {
+		fs.writeFileSync(ownerActionsFile, JSON.stringify({}, null, 2));
+	}
+	const ownerActions = JSON.parse(fs.readFileSync(ownerActionsFile));
+
 	return {
 		config,
 		configFile,
 		synths,
 		deployment,
 		deploymentFile,
+		ownerActions,
+		ownerActionsFile,
 	};
 };
 
@@ -206,6 +214,8 @@ program
 				synths,
 				deployment,
 				deploymentFile,
+				ownerActions,
+				ownerActionsFile,
 			} = loadAndCheckRequiredSources({
 				deploymentPath,
 				network,
@@ -343,18 +353,13 @@ program
 
 			// track an action we cannot perform because we aren't an OWNER (so we can iterate later in the owner step)
 			const appendOwnerAction = ({ key, action, target }) => {
-				const ownerActionsFilename = path.join(deploymentPath, OWNER_ACTIONS_FILENAME);
-				if (!fs.existsSync(ownerActionsFilename)) {
-					fs.writeFileSync(ownerActionsFilename, JSON.stringify({}, null, 2));
-				}
-				const ownerActions = JSON.parse(fs.readFileSync(ownerActionsFilename));
 				ownerActions[key] = {
 					target,
 					action,
 					complete: false,
 					link: `${etherscanLinkPrefix}/address/${target}#writeContract`,
 				};
-				fs.writeFileSync(ownerActionsFilename, JSON.stringify(ownerActions, null, 2));
+				fs.writeFileSync(ownerActionsFile, JSON.stringify(ownerActions, null, 2));
 				console.log(cyan(`Cannot invoke ${key} as not owner. Appended to actions.`));
 			};
 
@@ -902,6 +907,95 @@ program
 			} else {
 				console.log(gray('No change required.'));
 			}
+		}
+	});
+
+program
+	.command('owner')
+	.description('Owner script - a list of transactions required by the owner.')
+	.option(
+		'-d, --deployment-path <value>',
+		`Path to a folder that has your input configuration file ${CONFIG_FILENAME} and where your ${DEPLOYMENT_FILENAME} files will go`
+	)
+	.option('-o, --new-owner <value>', 'The address of you as owner (please include the 0x prefix)')
+	.option('-n, --network <value>', 'The network to run off.', x => x.toLowerCase(), 'kovan')
+	.action(async ({ network, newOwner, deploymentPath }) => {
+		ensureNetwork(network);
+
+		if (!newOwner || !w3utils.isAddress(newOwner)) {
+			console.error(red('Invalid new owner to nominate. Please check the option and try again.'));
+			process.exit(1);
+		}
+		// ensure all nominated owners are accepted
+		const { config, deployment, ownerActions, ownerActionsFile } = loadAndCheckRequiredSources({
+			deploymentPath,
+			network,
+		});
+
+		const { providerUrl, privateKey, etherscanLinkPrefix } = loadConnections({ network });
+		const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
+		web3.eth.accounts.wallet.add(privateKey);
+		const account = web3.eth.accounts.wallet[0].address;
+		console.log(gray(`Using account with public key ${account}`));
+
+		console.log(gray('Looking for contracts whose ownership we should accept'));
+
+		const confirmOrEnd = async message => {
+			try {
+				await confirmAction(
+					message +
+						cyan(
+							'\nPlease type "y" when transaction completed, or enter "n" to cancel and resume this later? (y/n) '
+						)
+				);
+			} catch (err) {
+				console.log(gray('Operation cancelled'));
+				process.exit();
+			}
+		};
+		for (const contract of Object.keys(config)) {
+			const { address, source } = deployment.targets[contract];
+			const { abi } = deployment.sources[source];
+			const deployedContract = new web3.eth.Contract(abi, address);
+
+			// ignore contracts that don't support Owned
+			if (!deployedContract.methods.owner) {
+				continue;
+			}
+			const currentOwner = await deployedContract.methods.owner().call();
+			const nominatedOwner = await deployedContract.methods.nominatedOwner().call();
+
+			if (currentOwner === newOwner) {
+				console.log(gray(`${newOwner} is already the owner of ${contract}`));
+			} else if (nominatedOwner === newOwner) {
+				await confirmOrEnd(
+					yellow(
+						`YOUR TASK: Invoke ${contract}.acceptOwnership() via ${etherscanLinkPrefix}/address/${address}#writeContract`
+					)
+				);
+			} else {
+				console.log(
+					cyan(
+						`Cannot acceptOwnership on ${contract} as nominatedOwner: ${nominatedOwner} isn't the newOwner ${newOwner} you specified. Have you run the nominate command yet?`
+					)
+				);
+			}
+		}
+
+		console.log(
+			gray('Running through operations during deployment that couldnt complete as not owner.')
+		);
+
+		for (const [key, entry] of Object.entries(ownerActions)) {
+			const { action, link, complete } = entry;
+			if (complete) continue;
+
+			await confirmOrEnd(
+				yellow('YOUR TASK: ') + `Invoke ${bgYellow(black(action))} (${key}) via ${cyan(link)}`
+			);
+
+			entry.complete = true;
+			fs.writeFileSync(ownerActionsFile, JSON.stringify(ownerActions, null, 2));
 		}
 	});
 
