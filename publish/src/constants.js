ad561cff95e84c769e89f3de57dbd039075d9e84
justin j. moses <justinjmoses@gmail.com>
2019-05-10 14:29:27 -0400

    Refactoring of publish script for clarity

---
+++ publish/src/constants.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+	COMPILED_FOLDER: 'compiled',
+	FLATTENED_FOLDER: 'flattened',
+	CONFIG_FILENAME: 'config.json',
+	SYNTHS_FILENAME: 'synths.json',
+	OWNER_ACTIONS_FILENAME: 'owner-actions.json',
+	DEPLOYMENT_FILENAME: 'deployment.json',
+	ZERO_ADDRESS: '0x' + '0'.repeat(40),
+};
