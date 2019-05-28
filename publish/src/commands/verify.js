618563446dca33706c855ae353523cdc9af568f3
justin j. moses <justinjmoses@gmail.com>
2019-05-28 12:28:11 -0400

    HBN-905 Deployment script fixes

--- publish/src/commands/verify.js
+++ publish/src/commands/verify.js
@@ -58,7 +58,7 @@ module.exports = program =>
 				);
 			}
 
-			const { etherscanUrl } = loadConnections({ network });
+			const { etherscanUrl, etherscanLinkPrefix } = loadConnections({ network });
 			console.log(gray(`Starting ${network.toUpperCase()} contract verification on Etherscan...`));
 
 			const tableData = [];
@@ -97,9 +97,7 @@ module.exports = program =>
 					const deployedBytecode = result.data.result[0].input;
 
 					// add the transacton and timestamp to the json file
-					deployment.targets[name].txn = `https://${network}.etherscan.io/tx/${
-						result.data.result[0].hash
-					}`;
+					deployment.targets[name].txn = `${etherscanLinkPrefix}/tx/${result.data.result[0].hash}`;
 					deployment.targets[name].timestamp = new Date(result.data.result[0].timeStamp * 1000);
 
 					fs.writeFileSync(deploymentFile, stringify(deployment));
