8f3b95d1205f2b4d6b62124bd07f593773800743
hav-noms <clinton@havven.iogit config --global user.name hav-nomsgit config --global user.email clinton@havven.io>
2019-06-26 22:25:25 +1000

    Contracts to match MAINNET

--- contracts/Synth.sol
+++ contracts/Synth.sol
@@ -31,14 +31,14 @@ per fee period.
 pragma solidity 0.4.25;
 
 import "./ExternStateToken.sol";
-import "./IFeePool.sol";
+import "./FeePool.sol";
 import "./Synthetix.sol";
 
 contract Synth is ExternStateToken {
 
     /* ========== STATE VARIABLES ========== */
 
-    IFeePool public feePool;
+    FeePool public feePool;
     Synthetix public synthetix;
 
     // Currency key which identifies this Synth to the Synthetix system
@@ -48,7 +48,7 @@ contract Synth is ExternStateToken {
 
     /* ========== CONSTRUCTOR ========== */
 
-    constructor(address _proxy, TokenState _tokenState, Synthetix _synthetix, IFeePool _feePool,
+    constructor(address _proxy, TokenState _tokenState, Synthetix _synthetix, FeePool _feePool,
         string _tokenName, string _tokenSymbol, address _owner, bytes4 _currencyKey
     )
         ExternStateToken(_proxy, _tokenState, _tokenName, _tokenSymbol, 0, DECIMALS, _owner)
@@ -75,7 +75,7 @@ contract Synth is ExternStateToken {
         emitSynthetixUpdated(_synthetix);
     }
 
-    function setFeePool(IFeePool _feePool)
+    function setFeePool(FeePool _feePool)
         external
         optionalProxy_onlyOwner
     {
