47a82eb6447288ad0636d887fcff389bf6405709
Jackson C <jackson@synthetix.io>
2019-09-12 14:51:51 +1000

    add stub functions for TransferFee back to Synthetix / FeePool for legacy sETH synth

--- contracts/interfaces/IFeePool.sol
+++ contracts/interfaces/IFeePool.sol
@@ -12,4 +12,5 @@ contract IFeePool {
     function feePaid(bytes32 currencyKey, uint amount) external;
     function appendAccountIssuanceRecord(address account, uint lockedAmount, uint debtEntryIndex) external;
     function setRewardsToDistribute(uint amount) external;
+    function transferFeeIncurred(uint value) public view returns (uint);
 }
