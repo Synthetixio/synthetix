00253f4329e2b68d15a6bdc74fac6839382221cf
zyfrank <zy_frank@hotmail.com>
2019-09-23 23:12:10 +0800

    format

--- contracts/test-helpers/MockUniswapExchange.sol
+++ contracts/test-helpers/MockUniswapExchange.sol
@@ -5,31 +5,31 @@ import "../interfaces/IERC20.sol";
 
 contract MockUniswapExchange {
 
-	address sEthAddress;
+    address sEthAddress;
 
-	// receive test ether
-	function() external payable { 
+    // receive test ether
+    function() external payable { 
     }
-	
-	function setSethAddress(address _sEthAddress) external {
-		sEthAddress = _sEthAddress;
-	}
+    
+    function setSethAddress(address _sEthAddress) external {
+        sEthAddress = _sEthAddress;
+    }
 
     function ethToTokenTransferInput(uint minSeth, uint deadline, address target) external payable returns (uint){
-		require(minSeth <= msg.value);
-		IERC20(sEthAddress).transfer(target, msg.value);
-		return (msg.value);
-	}
+        require(minSeth <= msg.value);
+        IERC20(sEthAddress).transfer(target, msg.value);
+        return (msg.value);
+    }
 
-	function tokenToEthTransferInput(uint sEthAmt, uint minEth, uint deadline, address target) external returns (uint) {
-	   require (minEth <= sEthAmt);
+    function tokenToEthTransferInput(uint sEthAmt, uint minEth, uint deadline, address target) external returns (uint) {
+       require (minEth <= sEthAmt);
        IERC20(sEthAddress).transferFrom(msg.sender, address(this), sEthAmt);
-	   target.transfer(sEthAmt);
-	   return sEthAmt;
-	}
+       target.transfer(sEthAmt);
+       return sEthAmt;
+    }
     
     
-	/*function getEthToTokenInputPrice(uint) external view returns (uint);
+    /*function getEthToTokenInputPrice(uint) external view returns (uint);
     function getEthToTokenOutputPrice(uint) external view returns (uint);
     function getTokenToEthInputPrice(uint) external view returns (uint);
     function getTokenToEthOutputPrice(uint) external view returns (uint);
