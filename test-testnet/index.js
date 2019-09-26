8e7323fce53b528fd2212e5489d71673ab75a5eb
Jackson C <jackson@synthetix.io>
2019-09-26 14:49:20 +1000

    update testnet script for checking exchangeRates and debtLedger first + confirmation from user to transact

--- test-testnet/index.js
+++ test-testnet/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const Web3 = require('web3');
 
-const { yellow, green, red } = require('chalk');
+const { yellow, gray, red, green } = require('chalk');
 
 const commander = require('commander');
 const program = new commander.Command();
@@ -14,7 +14,21 @@ require('dotenv').config();
 
 const snx = require('../index');
 
-const { loadConnections } = require('../publish/src/util');
+const { loadConnections, confirmAction } = require('../publish/src/util');
+
+const logExchangeRates = (currencyKeys, rates) => {
+	const results = [];
+	for (let i = 0; i < rates.length; i++) {
+		const rate = Web3.utils.fromWei(rates[i]);
+		results.push({
+			key: currencyKeys[i].name,
+			price: rate,
+		});
+	}
+	for (const rate of results) {
+		console.log(green(`currencyKey: ${rate.key}, price: ${rate.price}`));
+	}
+};
 
 program
 	.option('-n, --network <value>', 'The network to run off.', x => x.toLowerCase(), 'kovan')
@@ -23,6 +37,18 @@ program
 
 		const sources = snx.getSource({ network });
 		const targets = snx.getTarget({ network });
+
+		const synths = snx.getSynths({ network });
+
+		const cryptoSynths = synths
+			.filter(({ asset }) => asset !== 'USD')
+			.filter(
+				({ category }) => category === 'crypto' || category === 'internal' || category === 'index'
+			);
+
+		const forexSynths = synths
+			.filter(({ asset }) => asset !== 'USD')
+			.filter(({ category }) => category === 'forex' || category === 'commodity');
 
 		const { providerUrl, privateKey, etherscanLinkPrefix } = loadConnections({ network });
 		const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
@@ -36,8 +62,8 @@ program
 			// We are using the testnet deployer account, so presume they have some testnet ETH
 			const user1 = web3.eth.accounts.create();
 			web3.eth.accounts.wallet.add(user1);
-			console.log(green(`Created test account ${user1.address}`));
-			console.log(green(`Owner account ${owner.address}`));
+			console.log(gray(`Created test account ${user1.address}`));
+			console.log(gray(`Owner account ${owner.address}`));
 
 			// store keys in local file in case error and need to recover account
 			fs.appendFileSync(
@@ -46,15 +72,63 @@ program
 			);
 			console.log(`Test privkeys: ${user1.privateKey}`);
 
-			/** TODO: Add check System's debt matches reported totalIssuedSynths on synthetix.
-			 * 	If SynthetixState.debtLedgerLenght() > 0, then Synthetix.totalIssuedSynths(XDR)
-			 * 	must be > 0 as there should be debt in form of synths registered.
-			 * 	Terminate testnet test immediately if false.
-			 * 	Whole synthetixState debt system will be broken by any burning / issuing actions.
-			 **/
+			/**  VIEWS OF SYNTHETIX STATUS **/
+
+			const exchangeRates = new web3.eth.Contract(
+				sources['ExchangeRates'].abi,
+				targets['ExchangeRates'].address
+			);
+			const currencyKeys = [{ name: 'SNX' }].concat(cryptoSynths).concat(forexSynths);
+			const currencyKeysBytes = currencyKeys.map(key => web3.utils.asciiToHex(key.name));
+
+			const ratesAreStale = await exchangeRates.methods.anyRateIsStale(currencyKeysBytes).call();
+
+			console.log(green(`RatesAreStale - ${ratesAreStale}`));
+			if (ratesAreStale) {
+				throw Error('Rates are stale');
+			}
+
+			// View all current ExchangeRates
+			const rates = await exchangeRates.methods.ratesForCurrencies(currencyKeysBytes).call();
+
+			logExchangeRates(currencyKeys, rates);
+
+			// Synthetix contract
+			const Synthetix = new web3.eth.Contract(
+				sources['Synthetix'].abi,
+				targets['ProxySynthetix'].address
+			);
+
+			const SynthetixState = new web3.eth.Contract(
+				sources['SynthetixState'].abi,
+				targets['SynthetixState'].address
+			);
+
+			// Check totalIssuedSynths and debtLedger matches
+			const totalIssuedSynths = await Synthetix.methods.totalIssuedSynths(sUSD).call();
+			const debtLedgerLength = await SynthetixState.methods.debtLedgerLength().call();
+
+			console.log(
+				green(
+					`TotalIssuedSynths in sUSD: ${web3.utils.fromWei(
+						totalIssuedSynths
+					)} - debtLedgerLenght: ${debtLedgerLength}`
+				)
+			);
+
+			if (debtLedgerLength > 0 && totalIssuedSynths === 0) {
+				throw Error('DebtLedger has debt but totalIssuedSynths is 0');
+			}
+
+			try {
+				await confirmAction(yellow(`Do you want to continue? (y/n) `));
+			} catch (err) {
+				console.log(gray(`Operation terminated`));
+				return;
+			}
 
 			// #1 - Send the account some test ether
-			console.log(green(`Transferring 0.1 test ETH to ${user1.address}`));
+			console.log(gray(`Transferring 0.1 test ETH to ${user1.address}`));
 			const { transactionHash: txn0Hash } = await web3.eth.sendTransaction({
 				from: owner.address,
 				to: user1.address,
@@ -64,12 +138,7 @@ program
 			});
 			console.log(yellow(`Success. ${etherscanLinkPrefix}/tx/${txn0Hash}`));
 
-			const Synthetix = new web3.eth.Contract(
-				sources['Synthetix'].abi,
-				targets['ProxySynthetix'].address
-			);
-
-			console.log(green(`Transferring 2000 SNX to user1 (${user1.address})`));
+			console.log(gray(`Transferring 2000 SNX to user1 (${user1.address})`));
 			const { transactionHash: txn1Hash } = await Synthetix.methods
 				.transfer(user1.address, web3.utils.toWei('2000'))
 				.send({
@@ -80,7 +149,7 @@ program
 			console.log(yellow(`Success. ${etherscanLinkPrefix}/tx/${txn1Hash}`));
 
 			// #2 - Mint some sUSD from test account
-			console.log(green(`Issuing 100 sUSD from (${user1.address}`));
+			console.log(gray(`Issuing 100 sUSD from (${user1.address}`));
 			const amountToIssue = web3.utils.toWei('100');
 			const { transactionHash: txn2Hash } = await Synthetix.methods
 				.issueSynths(sUSD, amountToIssue)
@@ -97,10 +166,10 @@ program
 
 			// get balance
 			const balance = await SynthsUSD.methods.balanceOf(user1.address).call();
-			console.log(green(`User1 has sUSD balanceOf - ${web3.utils.fromWei(balance)}`));
+			console.log(gray(`User1 has sUSD balanceOf - ${web3.utils.fromWei(balance)}`));
 
 			// deposit to Depot
-			console.log(green(`Deposit 60 sUSD to depot from (${user1.address})`));
+			console.log(gray(`Deposit 60 sUSD to depot from (${user1.address})`));
 			const amountToDeposit = web3.utils.toWei('60');
 			const { transactionHash: txn3Hash } = await SynthsUSD.methods
 				.transfer(Depot.options.address, amountToDeposit)
@@ -112,7 +181,7 @@ program
 			console.log(yellow(`Success. ${etherscanLinkPrefix}/tx/${txn3Hash}`));
 
 			// #4 withdraw deposited synths from Depot
-			console.log(green(`Withdraw 60 sUSD from Depot for (${user1.address})`));
+			console.log(gray(`Withdraw 60 sUSD from Depot for (${user1.address})`));
 			const { transactionHash: txn4Hash } = await Depot.methods.withdrawMyDepositedSynths().send({
 				from: user1.address,
 				gas,
@@ -122,10 +191,10 @@ program
 
 			// check balance
 			const balanceAfter = await SynthsUSD.methods.balanceOf(user1.address).call();
-			console.log(green(`User1 has sUSD balanceOf - ${web3.utils.fromWei(balanceAfter)}`));
+			console.log(gray(`User1 has sUSD balanceOf - ${web3.utils.fromWei(balanceAfter)}`));
 
 			// #5 Exchange sUSD to sETH
-			console.log(green(`Exchange sUSD --> sETH for user - (${user1.address})`));
+			console.log(gray(`Exchange sUSD --> sETH for user - (${user1.address})`));
 			const amountToExchange = web3.utils.toWei('100');
 			const { transactionHash: txn5Hash } = await Synthetix.methods
 				.exchange(sUSD, amountToExchange, sETH, user1.address)
@@ -139,10 +208,10 @@ program
 			// check sETH balance after exchange
 			const SynthsETH = new web3.eth.Contract(sources['Synth'].abi, targets['ProxysETH'].address);
 			const sETHBalance = await SynthsETH.methods.balanceOf(user1.address).call();
-			console.log(green(`User1 has sETH balanceOf - ${web3.utils.fromWei(sETHBalance)}`));
+			console.log(gray(`User1 has sETH balanceOf - ${web3.utils.fromWei(sETHBalance)}`));
 
 			// #6 Exchange balance of sETH back to sUSD
-			console.log(green(`Exchange sETH --> sUSD for user - (${user1.address})`));
+			console.log(gray(`Exchange sETH --> sUSD for user - (${user1.address})`));
 			const { transactionHash: txn6Hash } = await Synthetix.methods
 				.exchange(sETH, sETHBalance, sUSD, user1.address)
 				.send({
@@ -153,8 +222,8 @@ program
 			console.log(yellow(`Success. ${etherscanLinkPrefix}/tx/${txn6Hash}`));
 
 			// #7 Burn all remaining sUSD to unlock SNX
-			const remainingSynthsUSD = SynthsUSD.methods.balanceOf(user1.address).call();
-			console.log(green(`Burn all remaining synths for user - (${user1.address})`));
+			const remainingSynthsUSD = await SynthsUSD.methods.balanceOf(user1.address).call();
+			console.log(gray(`Burn all remaining synths for user - (${user1.address})`));
 			const { transactionHash: txn7Hash } = await Synthetix.methods
 				.burnSynths(sUSD, remainingSynthsUSD)
 				.send({
@@ -167,13 +236,13 @@ program
 			// check transferable SNX after burning
 			const transferableSNX = await Synthetix.methods.transferableSynthetix(user1.address).call();
 			console.log(
-				green(
+				gray(
 					`Transferable SNX of ${web3.utils.fromWei(transferableSNX)} for user (${user1.address}`
 				)
 			);
 
 			// #8 Transfer SNX back to owner
-			console.log(green(`Transferring SNX back to owner (${user1.address}`));
+			console.log(gray(`Transferring SNX back to owner (${user1.address}`));
 			const { transactionHash: txn8Hash } = await Synthetix.methods
 				.transfer(user1.address, transferableSNX)
 				.send({
@@ -194,7 +263,7 @@ program
 			).toString();
 
 			console.log(
-				green(
+				gray(
 					`Transferring remaining test ETH back to owner (${web3.utils.fromWei(
 						testETHBalanceMinusTxnCost
 					)})`
@@ -210,7 +279,7 @@ program
 			console.log(yellow(`Success. ${etherscanLinkPrefix}/tx/${txn9Hash}`));
 
 			console.log();
-			console.log(green(`Integration test on ${network.toUpperCase()} completed successfully.`));
+			console.log(gray(`Integration test on ${network.toUpperCase()} completed successfully.`));
 		} catch (err) {
 			if (/Transaction has been reverted/.test(err)) {
 				const txnHash = err.message.match(/(?:"transactionHash":\s")(\w+)(")/)[1];
