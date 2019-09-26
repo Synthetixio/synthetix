6f990f0e1d4ef694733b5c5c5ff0aecace60c7ba
Anton Jurisevic <zyzek@users.noreply.github.com>
2019-09-26 17:26:34 +1000

    Remove image from section separator.

--- docs/js/sectionSeparators.js
+++ docs/js/sectionSeparators.js
@@ -1,6 +1,7 @@
 // Replaces all instances of the section-sep tag with a small,
 // centered 5% opacity Synthetix logo with some top margin.
-const separator = '<img src="../../img/logos/synthetix_logo_light.png">'
+//const separator = '<img src="../../img/logos/synthetix_logo_light.png">'
+const separator = '';
 const separators = document.getElementsByTagName('section-sep');
 for (let sep of separators) {
     sep.innerHTML = separator;
