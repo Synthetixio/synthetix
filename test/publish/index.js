8a027af5b69b45f213e12e7d944020dc7356293e
justin j. moses <justinjmoses@gmail.com>
2019-12-13 15:40:03 +1100

    Actually fixing it this time around

--- test/publish/index.js
+++ test/publish/index.js
@@ -885,7 +885,7 @@ describe('publish scripts', function() {
 								describe('when Synthetix.totalIssuedSynths is invoked', () => {
 									it('then it returns some number successfully as no rates are stale', async () => {
 										const response = await Synthetix.methods.totalIssuedSynths(sUSD).call();
-										assert.strictEqual(response.toNumber() >= 0, true);
+										assert.strictEqual(Number(response) >= 0, true);
 									});
 								});
 
