67b40e66da2389400df46d23b6e500cae866163f
Justin J. Moses <justinjmoses@gmail.com>
2020-01-29 12:32:30 +1100

    Cleanup of ISynthetix interface

--- contracts/interfaces/ISynthetix.sol
+++ contracts/interfaces/ISynthetix.sol
@@ -12,15 +12,9 @@ import "../interfaces/IFeePool.sol";
 import "../interfaces/IExchangeRates.sol";
 import "../Synth.sol";
 
+
 contract ISynthetix {
-
     // ========== PUBLIC STATE VARIABLES ==========
-
-    IFeePool public feePool;
-    ISynthetixEscrow public escrow;
-    ISynthetixEscrow public rewardEscrow;
-    ISynthetixState public synthetixState;
-    IExchangeRates public exchangeRates;
 
     uint public totalSupply;
 
@@ -29,36 +23,48 @@ contract ISynthetix {
     // ========== PUBLIC FUNCTIONS ==========
 
     function balanceOf(address account) public view returns (uint);
+
     function transfer(address to, uint value) public returns (bool);
+
     function transferFrom(address from, address to, uint value) public returns (bool);
 
-    function effectiveValue(bytes32 sourceCurrencyKey, uint sourceAmount, bytes32 destinationCurrencyKey) public view returns (uint);
-
-    function synthInitiatedExchange(
-        address from,
-        bytes32 sourceCurrencyKey,
-        uint sourceAmount,
-        bytes32 destinationCurrencyKey,
-        address destinationAddress) external returns (bool);
-    function exchange(
-        bytes32 sourceCurrencyKey,
-        uint sourceAmount,
-        bytes32 destinationCurrencyKey) external returns (bool);
-    function collateralisationRatio(address issuer) public view returns (uint);
-    function totalIssuedSynths(bytes32 currencyKey)
+    function effectiveValue(bytes32 sourceCurrencyKey, uint sourceAmount, bytes32 destinationCurrencyKey)
         public
         view
         returns (uint);
+
+    function exchange(bytes32 sourceCurrencyKey, uint sourceAmount, bytes32 destinationCurrencyKey) external returns (bool);
+
+    function issueSynths(uint amount) external;
+
+    function issueMaxSynths() external;
+
+    function burnSynths(uint amount) external;
+
+    function settle(bytes32 currencyKey) external returns (bool);
+
+    function collateralisationRatio(address issuer) public view returns (uint);
+
+    function totalIssuedSynths(bytes32 currencyKey) public view returns (uint);
+
     function getSynthByCurrencyKey(bytes32 currencyKey) public view returns (ISynth);
+
     function getSynthByAddress(address synth) external view returns (bytes32);
+
     function debtBalanceOf(address issuer, bytes32 currencyKey) public view returns (uint);
 
-    function remainingIssuableSynths(address issuer)
-        public
-        view
-        returns (uint, uint);
+    function remainingIssuableSynths(address issuer) public view returns (uint, uint);
 
-    function emitSynthExchange(address account, bytes32 fromCurrencyKey, uint256 fromAmount, bytes32 toCurrencyKey, uint256 toAmount, address toAddress) external;
-    function emitExchangeReclaim(address account, bytes32 currencyKey, uint256 amount) external;
-    function emitExchangeRebate(address account, bytes32 currencyKey, uint256 amount) external;
+    function emitSynthExchange(
+        address account,
+        bytes32 fromCurrencyKey,
+        uint fromAmount,
+        bytes32 toCurrencyKey,
+        uint toAmount,
+        address toAddress
+    ) external;
+
+    function emitExchangeReclaim(address account, bytes32 currencyKey, uint amount) external;
+
+    function emitExchangeRebate(address account, bytes32 currencyKey, uint amount) external;
 }
