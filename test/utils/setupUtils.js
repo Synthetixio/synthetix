15ca986f34b8d4905a7410d77576bd5a2d32fffd
justin j. moses <justinjmoses@gmail.com>
2020-02-10 11:53:58 +0800

    Refatoring ExchangeRates tests - better structure and cleanup

--- test/utils/setupUtils.js
+++ test/utils/setupUtils.js
@@ -43,14 +43,16 @@ module.exports = {
 		});
 	},
 
-	async onlyGivenAddressCanInvoke({ fnc, args, address, accounts }) {
+	async onlyGivenAddressCanInvoke({ fnc, args, address, accounts, skipPassCheck = false }) {
 		for (const user of accounts) {
 			if (user === address) {
 				continue;
 			}
 			await assert.revert(fnc(...args, { from: user }));
 		}
-		await fnc(...args, { from: address });
+		if (!skipPassCheck) {
+			await fnc(...args, { from: address });
+		}
 	},
 
 	// Helper function that can issue synths directly to a user without having to have them exchange anything
