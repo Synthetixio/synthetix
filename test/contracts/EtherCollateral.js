32b0657eba52e5b3c1d182ed50f6523c699b7475
Jackson C <jackson@synthetix.io>
2020-02-12 11:26:09 +1100

    lint fix

--- test/contracts/EtherCollateral.js
+++ test/contracts/EtherCollateral.js
@@ -33,7 +33,7 @@ contract('EtherCollateral', async accounts => {
 
 	const sETH = toBytes32('sETH');
 	const sUSD = toBytes32('sUSD');
-	const SNX = toBytes32('SNX');
+	// const SNX = toBytes32('SNX');
 
 	const ISSUACE_RATIO = toUnit('0.666666666666666667');
 	const ZERO_BN = toUnit('0');
@@ -41,7 +41,7 @@ contract('EtherCollateral', async accounts => {
 	const [
 		deployerAccount,
 		owner,
-		oracle,
+		// oracle,
 		depotDepositor,
 		address1,
 		address2,
@@ -59,20 +59,20 @@ contract('EtherCollateral', async accounts => {
 		sETHContract,
 		FEE_ADDRESS;
 
-	const updateRatesWithDefaults = async () => {
-		const timestamp = await currentTime();
+	// const updateRatesWithDefaults = async () => {
+	// 	const timestamp = await currentTime();
 
-		console.log('call depot.updatePrices');
-		await depot.updatePrices(toUnit('190'), toUnit('1.20'), timestamp, {
-			from: oracle,
-		});
+	// 	console.log('call depot.updatePrices');
+	// 	await depot.updatePrices(toUnit('190'), toUnit('1.20'), timestamp, {
+	// 		from: oracle,
+	// 	});
 
-		console.log('call exchangeRates.updateRates');
-		await exchangeRates.updateRates([sETH, SNX], ['190', '1.20'].map(toUnit), timestamp, {
-			from: oracle,
-		});
-		console.log('called exchangeRates.updateRates');
-	};
+	// 	console.log('call exchangeRates.updateRates');
+	// 	await exchangeRates.updateRates([sETH, SNX], ['190', '1.20'].map(toUnit), timestamp, {
+	// 		from: oracle,
+	// 	});
+	// 	console.log('called exchangeRates.updateRates');
+	// };
 
 	// const fastForwardAndUpdateRates = async seconds => {
 	// 	console.log('fastForwardAndUpdateRates', seconds);
