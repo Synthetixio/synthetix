2a0c46e5b3be59ab468cac981a94ba0fcfaabc11
Jackson C <jackson@synthetix.io>
2020-02-13 11:46:08 +1100

    remove .only

--- test/contracts/MultiCollateralSynth.js
+++ test/contracts/MultiCollateralSynth.js
@@ -10,7 +10,7 @@ const Proxy = artifacts.require('Proxy');
 const { toUnit, ZERO_ADDRESS } = require('../utils/testUtils');
 const { toBytes32 } = require('../..');
 
-contract.only('MultiCollateralSynth', accounts => {
+contract('MultiCollateralSynth', accounts => {
 	const [
 		deployerAccount,
 		owner, // Oracle next, is not needed
