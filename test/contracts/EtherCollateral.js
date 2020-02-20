ab580e48b4e39bf62092bab9da252a2154a831bd
justin j. moses <justinjmoses@gmail.com>
2020-02-20 09:10:15 +0400

    Minor improvement to revert check of HARD_CAP in EtherCollateral

--- test/contracts/EtherCollateral.js
+++ test/contracts/EtherCollateral.js
@@ -334,7 +334,10 @@ contract('EtherCollateral', async accounts => {
 					await assert.revert(etherCollateral.setInterestRate(newInterestRate, { from: owner }));
 				});
 				it('owner sets accountLoanLimit over HARD_CAP', async () => {
-					await assert.revert(etherCollateral.setAccountLoanLimit(1200, { from: owner }));
+					await assert.revert(
+						etherCollateral.setAccountLoanLimit(1200, { from: owner }),
+						'Owner cannot set higher than HARD_CAP'
+					);
 				});
 				describe('non owner attempts to set', async () => {
 					it('issueFeeRate', async () => {
