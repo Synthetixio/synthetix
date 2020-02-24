d60a002e74eb0e1940931797d8623d724ab1c6cd
justin j. moses <justinjmoses@gmail.com>
2020-02-25 00:22:54 +0400

    Removing old legacy tests now covered by new ones

--- legacy/tests/contracts/TokenRecipient.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.24;
-
-contract TokenRecipient {
-    event TokenFallbackCalled(address from, uint value, bytes data);
-
-    function tokenFallback(address from, uint value, bytes data) public {
-        emit TokenFallbackCalled(from, value, data);
-    }
-}
