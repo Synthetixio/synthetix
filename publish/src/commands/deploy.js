26df86e465e74856fa3136ae1f5382c7d07358b6
Jackson <jackson@havven.io>
2020-02-28 16:46:29 +1100

    update deploy.js to set SynthetixEscrow to proxyERC20Synthetix  (#443)
    * update setSynthetix on synthetixEscrow to proxyERC20Synthetix
    
    Co-authored-by: CLINT <clinton@synthetix.io>
    Co-authored-by: justin j. moses <justinjmoses@gmail.com>
    Co-authored-by: Synthetix Team <50597730+synthetixio-team@users.noreply.github.com>

--- publish/src/commands/deploy.js
+++ publish/src/commands/deploy.js
@@ -680,28 +680,6 @@ const deploy = async ({
 		});
 	}
 
-	// Skip setting unless redeploying either of these,
-	if (config['Synthetix'].deploy || config['SynthetixEscrow'].deploy) {
-		// Note: currently on mainnet SynthetixEscrow.methods.synthetix() does NOT exist
-		// it is "havven" and the ABI we have here is not sufficient
-		if (network === 'mainnet') {
-			appendOwnerAction({
-				key: `SynthetixEscrow.setHavven(Synthetix)`,
-				target: addressOf(synthetixEscrow),
-				action: `setHavven(${addressOf(synthetix)})`,
-			});
-		} else {
-			await runStep({
-				contract: 'SynthetixEscrow',
-				target: synthetixEscrow,
-				read: 'synthetix',
-				expected: input => input === addressOf(synthetix),
-				write: 'setSynthetix',
-				writeArg: addressOf(synthetix),
-			});
-		}
-	}
-
 	if (supplySchedule && synthetix) {
 		await runStep({
 			contract: 'SupplySchedule',
@@ -758,6 +736,32 @@ const deploy = async ({
 			write: 'setSynthetixProxy',
 			writeArg: addressOf(proxyERC20Synthetix),
 		});
+	}
+
+	// ----------------
+	// Setting proxyERC20 Synthetix for synthetixEscrow
+	// ----------------
+
+	// Skip setting unless redeploying either of these,
+	if (config['Synthetix'].deploy || config['SynthetixEscrow'].deploy) {
+		// Note: currently on mainnet SynthetixEscrow.methods.synthetix() does NOT exist
+		// it is "havven" and the ABI we have here is not sufficient
+		if (network === 'mainnet') {
+			appendOwnerAction({
+				key: `SynthetixEscrow.setHavven(Synthetix)`,
+				target: addressOf(synthetixEscrow),
+				action: `setHavven(${addressOf(proxyERC20Synthetix)})`,
+			});
+		} else {
+			await runStep({
+				contract: 'SynthetixEscrow',
+				target: synthetixEscrow,
+				read: 'synthetix',
+				expected: input => input === addressOf(proxyERC20Synthetix),
+				write: 'setSynthetix',
+				writeArg: addressOf(proxyERC20Synthetix),
+			});
+		}
 	}
 
 	// ----------------
