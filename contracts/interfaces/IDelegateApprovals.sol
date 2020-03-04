5acee313a3d1351c584edc385fe924dc26fb95a2
hav-noms <clinton@synthetix.io>
2020-03-04 14:51:02 +1100

    clean IDelegateApprovals

--- contracts/interfaces/IDelegateApprovals.sol
+++ contracts/interfaces/IDelegateApprovals.sol
@@ -2,8 +2,6 @@ pragma solidity 0.4.25;
 
 
 interface IDelegateApprovals {
-    function rateForCurrency(bytes32 currencyKey) external view returns (uint);
-
     function canBurnFor(address owner, address delegate) external view returns (bool);
 
     function canIssueFor(address owner, address delegate) external view returns (bool);
