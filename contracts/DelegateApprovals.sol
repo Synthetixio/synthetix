9cd3bbd28069dabeaf9886c0e39e3281a158c1c5
Jackson C <jackson@synthetix.io>
2020-03-05 14:44:40 +1100

    upgrade delegateApprovals to use eternalStorage and new functions

--- contracts/DelegateApprovals.sol
+++ contracts/DelegateApprovals.sol
@@ -22,42 +22,130 @@ The concept is similar to the ERC20 interface where a wallet can
 approve an authorised party to spend on the authorising party's
 behalf in the allowance interface.
 
-Withdrawing approval deletes the approval for the given delegate.
-
-This contract inherits state for upgradeability / associated
-contract.
-
 -----------------------------------------------------------------
 */
 pragma solidity 0.4.25;
 
-import "./State.sol";
+import "./EternalStorage.sol";
+import "./MixinResolver.sol";
 
 
-contract DelegateApprovals is State {
-    // Approvals - [authoriser][delegate]
+contract DelegateApprovals is MixinResolver {
+    bytes32 public constant BURN_FOR_ADDRESS = "BurnForAddress";
+    bytes32 public constant ISSUE_FOR_ADDRESS = "IssueForAddress";
+    bytes32 public constant CLAIM_FOR_ADDRESS = "ClaimForAddress";
+    bytes32 public constant EXCHANGE_FOR_ADDRESS = "ExchangeForAddress";
+    bytes32 public constant APPROVE_ALL = "ApproveAll";
+
+    /* ========== ADDRESS RESOLVER CONFIGURATION ========== */
+
+    bytes32 private constant CONTRACT_DELEGATEAPPROVALETERNALSTORAGE = "DelegateApprovalEternalStorage";
+
+    bytes32[24] private addressesToCache = [CONTRACT_DELEGATEAPPROVALETERNALSTORAGE];
+
     // Each authoriser can have multiple delegates
     mapping(address => mapping(address => bool)) public approval;
 
     /**
      * @dev Constructor
      * @param _owner The address which controls this contract.
-     * @param _associatedContract The contract whose approval state this composes.
+     * @param _resolver The resolver address.
      */
-    constructor(address _owner, address _associatedContract) public State(_owner, _associatedContract) {}
+    constructor(address _owner, address _resolver) public MixinResolver(_owner, _resolver, addressesToCache) {}
 
-    function setApproval(address authoriser, address delegate) external onlyAssociatedContract {
-        approval[authoriser][delegate] = true;
-        emit Approval(authoriser, delegate);
+    /* ========== VIEWS ========== */
+
+    function delegateApprovalEternalStorage() internal view returns (EternalStorage) {
+        return
+            EternalStorage(
+                requireAndGetAddress(
+                    CONTRACT_DELEGATEAPPROVALETERNALSTORAGE,
+                    "Missing DelegateApprovalEternalStorage address"
+                )
+            );
     }
 
-    function withdrawApproval(address authoriser, address delegate) external onlyAssociatedContract {
-        delete approval[authoriser][delegate];
-        emit WithdrawApproval(authoriser, delegate);
+    // hash of actionName + address of authoriser + address for the delegate
+    function canBurnFor(address authoriser, address delegate) external view returns (bool) {
+        return _checkApproval(BURN_FOR_ADDRESS, authoriser, delegate);
+    }
+
+    function canIssueFor(address authoriser, address delegate) external view returns (bool) {
+        return _checkApproval(ISSUE_FOR_ADDRESS, authoriser, delegate);
+    }
+
+    function canClaimFor(address authoriser, address delegate) external view returns (bool) {
+        return _checkApproval(CLAIM_FOR_ADDRESS, authoriser, delegate);
+    }
+
+    function canExchangeFor(address authoriser, address delegate) external view returns (bool) {
+        return _checkApproval(EXCHANGE_FOR_ADDRESS, authoriser, delegate);
+    }
+
+    function _approvedAll(address authoriser, address delegate) internal view returns (bool) {
+        return
+            delegateApprovalEternalStorage().getBooleanValue(_getKey(APPROVE_ALL, authoriser, delegate));
+    }
+
+    // internal function to check approval based on action 
+    // if approved for all actions then will return true 
+    // before checking specific approvals 
+    function _checkApproval(bytes32 action, address authoriser, address delegate) internal view returns (bool) {
+        if (_approvedAll(authoriser, delegate)) return true;
+
+        return delegateApprovalEternalStorage().getBooleanValue(_getKey(action, authoriser, delegate));
+    }
+
+    // util to get key based on actionName + address of authoriser + address for the delegate
+    function _getKey(bytes32 _action, address _authoriser, address _delegate) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_action, _authoriser, _delegate));
+    }
+
+    /* ========== SETTERS ========== */
+    function approveAllDelegatePowers(address delegate) external {
+        require(delegate != address(0), "Can't delegate to address(0)");
+        delegateApprovalEternalStorage().setBooleanValue(_getKey(APPROVE_ALL, msg.sender, delegate), true);
+    }
+
+    function removeAllDelegatePowers(address delegate) external {
+        delegateApprovalEternalStorage().deleteBooleanValue(_getKey(APPROVE_ALL, msg.sender, delegate));
+    }
+
+    function approveBurnOnBehalf(address delegate) external {
+        require(delegate != address(0), "Can't delegate to address(0)");
+        delegateApprovalEternalStorage().setBooleanValue(_getKey(BURN_FOR_ADDRESS, msg.sender, delegate), true);
+    }    
+    
+    function removeBurnOnBehalf(address delegate) external {
+        delegateApprovalEternalStorage().deleteBooleanValue(_getKey(BURN_FOR_ADDRESS, msg.sender, delegate));
+    }
+
+    function approveIssueOnBehalf(address delegate) external {
+        require(delegate != address(0), "Can't delegate to address(0)");
+        delegateApprovalEternalStorage().setBooleanValue(_getKey(ISSUE_FOR_ADDRESS, msg.sender, delegate), true);
+    }    
+    
+    function removeIssueOnBehalf(address delegate) external {
+        delegateApprovalEternalStorage().deleteBooleanValue(_getKey(ISSUE_FOR_ADDRESS, msg.sender, delegate));
+    }
+
+    function approveClaimOnBehalf(address delegate) external {
+        require(delegate != address(0), "Can't delegate to address(0)");
+        delegateApprovalEternalStorage().setBooleanValue(_getKey(CLAIM_FOR_ADDRESS, msg.sender, delegate), true);
+    }    
+   
+    function removeClaimOnBehalf(address delegate) external {
+        delegateApprovalEternalStorage().deleteBooleanValue(_getKey(CLAIM_FOR_ADDRESS, msg.sender, delegate));
+    }
+
+    function approveExchangeOnBehalf(address delegate) external {
+        require(delegate != address(0), "Can't delegate to address(0)");
+        delegateApprovalEternalStorage().setBooleanValue(_getKey(EXCHANGE_FOR_ADDRESS, msg.sender, delegate), true);
+    }
+
+    function removeExchangeOnBehalf(address delegate) external {
+        delegateApprovalEternalStorage().deleteBooleanValue(_getKey(EXCHANGE_FOR_ADDRESS, msg.sender, delegate));
     }
 
     /* ========== EVENTS ========== */
-
-    event Approval(address indexed authoriser, address delegate);
-    event WithdrawApproval(address indexed authoriser, address delegate);
 }
