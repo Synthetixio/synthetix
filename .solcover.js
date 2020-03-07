2c33a441cc9bfedf5a34b6f2a0a889c030b1f545
cgewecke <christophergewecke@gmail.com>
2020-03-06 18:07:44 -0800

    Use same ganache settings for "coverage" as "test"

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,8 @@
 module.exports = {
-	skipFiles: ['test-helpers/PublicSafeDecimalMath.sol', 'test-helpers/PublicMath.sol'],
+  skipFiles: ['test-helpers/PublicSafeDecimalMath.sol', 'test-helpers/PublicMath.sol'],
+  client: require('ganache-cli'), // use ganache-cli version listed in dev deps
+  providerOptions: {
+    default_balance_ether: 10000000000000, // extra zero just in case (coverage consumes more gas)
+    time: new Date("2019-03-06T00:00:00"),
+  }
 };
