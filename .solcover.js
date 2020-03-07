ca9d7bb20adcd722e73c1101be6a0c44aca87494
cgewecke <christophergewecke@gmail.com>
2020-03-06 19:27:49 -0800

    Skip two gas sensitive Depot tests (using coverage mocha options)

--- .solcover.js
+++ .solcover.js
@@ -4,5 +4,9 @@ module.exports = {
   providerOptions: {
     default_balance_ether: 10000000000000, // extra zero just in case (coverage consumes more gas)
     time: new Date("2019-03-06T00:00:00"),
+  },
+  mocha: {
+    grep: "@cov-skip", // Find everything with this tag
+    invert: true       // Run the grep's inverse set.
   }
 };
