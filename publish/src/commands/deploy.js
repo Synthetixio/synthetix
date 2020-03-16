9598c0a4593ddea10545edb6892bc1cf520593ad
Jackson C <jackson@synthetix.io>
2020-03-16 14:30:00 +1100

    update deploy script to allow small amounts of ISynths to be left when setInversePricing

--- publish/src/commands/deploy.js
+++ publish/src/commands/deploy.js
@@ -1013,10 +1013,10 @@ const deploy = async ({
 					console.log(gray(`Detected a new inverted synth for ${currencyKey}. Proceeding to add.`));
 					// Then a new inverted synth is being added (as there's no previous rate for it)
 					await setInversePricing({ freeze: false, freezeAtUpperLimit: false });
-				} else if (Number(totalSynthSupply) === 0) {
+				} else if (Number(totalSynthSupply) <= w3utils.toWei('10')) {
 					console.log(
 						gray(
-							`Inverted synth at ${currencyKey} has 0 total supply and its inverted parameters have changed. ` +
+							`Inverted synth at ${currencyKey} has less than 10 total supply and its inverted parameters have changed. ` +
 								`Proceeding to reconfigure its parameters as instructed, unfreezing it if currently frozen.`
 						)
 					);
@@ -1028,7 +1028,7 @@ const deploy = async ({
 					console.log(
 						redBright(
 							`⚠⚠⚠ WARNING: The parameters for the inverted synth ${currencyKey} ` +
-								`have changed and it has non-zero totalSupply. This use-case is not supported by the deploy script. ` +
+								`have changed and it has a large non-zero totalSupply. This use-case is not supported by the deploy script. ` +
 								`This should be done as a purge() and setInversePricing() separately`
 						)
 					);
