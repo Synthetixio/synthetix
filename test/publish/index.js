63256eb6522e3071561e2f312853c155791b0d73
justin j. moses <justinjmoses@gmail.com>
2020-03-22 15:45:55 -0400

    Fixing gas limits

--- test/publish/index.js
+++ test/publish/index.js
@@ -514,6 +514,7 @@ describe('publish scripts', function() {
 											privateKey: accounts.deployer.private,
 											subclass: 'PurgeableSynth',
 											synthsToReplace: ['sBTC'],
+											methodCallGasLimit: gasLimit,
 										});
 									});
 									describe('and deployer invokes purge', () => {
@@ -527,6 +528,7 @@ describe('publish scripts', function() {
 												privateKey: accounts.deployer.private,
 												addresses: [accounts.first.public],
 												synthsToPurge: ['sBTC'],
+												gasLimit,
 											});
 										});
 										it('then their sUSD balance is 4990 + sBTCBalanceAfterExchange', async () => {
