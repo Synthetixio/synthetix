58e1a96792ec2b6f71a9999c34b898d6603f00a3
justin j. moses <justinjmoses@gmail.com>
2020-04-03 13:24:59 -0400

    Minor updates

--- .solcover.js
+++ .solcover.js
@@ -6,6 +6,10 @@ module.exports = {
 		default_balance_ether: 10000000000000, // extra zero just in case (coverage consumes more gas)
 		time: new Date('2019-03-06T00:00:00'),
 		network_id: 55,
+		gas: 17592186044415, // 0xfffffffffff
+		gasLimit: 9e12,
+		callGasLimit: 9e12,
+		allowUnlimitedContractSize: true,
 	},
 	mocha: {
 		grep: '@cov-skip', // Find everything with this tag
