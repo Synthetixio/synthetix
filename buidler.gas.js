a4a14f07caea681c5d92b25e9df7d919499a0eb4
Mudit Gupta <guptamudit@ymail.com>
2020-05-03 19:34:34 +0530

    wip

--- buidler.gas.js
+++ buidler.gas.js
@@ -3,41 +3,18 @@
 const { usePlugin } = require('@nomiclabs/buidler/config');
 
 usePlugin('@nomiclabs/buidler-truffle5'); // uses and exposes web3 via buidler-web3 plugin
-usePlugin('solidity-coverage');
 usePlugin('buidler-gas-reporter');
 
-const {
-	constants: { inflationStartTimestampInSecs },
-} = require('.');
+const config = require('./buidler.config');
 
-const GAS_PRICE = 20e9; // 20 GWEI
-
-const baseNetworkConfig = {
-	allowUnlimitedContractSize: true,
-	blockGasLimit: 0x1fffffffffffff,
-	initialDate: new Date(inflationStartTimestampInSecs * 1000).toISOString(),
-	gasPrice: GAS_PRICE,
-};
-
-module.exports = {
-	GAS_PRICE,
-	solc: {
-		version: '0.5.16',
-	},
-	paths: {
-		sources: './contracts',
-		tests: './test/contracts',
-		artifacts: './build/artifacts',
-		cache: './build/cache',
-	},
+module.exports = Object.assign({}, config, {
 	networks: {
-		buidlerevm: baseNetworkConfig,
 		localhost: Object.assign(
 			{
 				url: 'http://localhost:8545',
 				timeout: 60e4,
 			},
-			baseNetworkConfig
+			config.baseNetworkConfig
 		)
 	},
 	gasReporter: {
@@ -45,4 +22,4 @@ module.exports = {
 		currency: 'USD',
 		outputFile: 'test-gas-used.log',
 	},
-};
+});
