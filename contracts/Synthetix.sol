4dd8039edcbb09fa3da4154e5e0d0601fd6caa99
justin j. moses <justinjmoses@gmail.com>
2020-05-21 17:02:44 -0400

    Cleanup of synthetix contract

--- contracts/Synthetix.sol
+++ contracts/Synthetix.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.5.16;
 
 // Inheritance
+import "./interfaces/IERC20.sol";
 import "./ExternStateToken.sol";
 import "./MixinResolver.sol";
 import "./interfaces/ISynthetix.sol";
 
 // Internal references
-import "./TokenState.sol";
 import "./interfaces/ISynth.sol";
-import "./interfaces/IERC20.sol";
+import "./TokenState.sol";
 import "./interfaces/ISystemStatus.sol";
 import "./interfaces/IExchanger.sol";
 import "./interfaces/IEtherCollateral.sol";
@@ -138,16 +138,12 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
         (, anyRateStale) = exchangeRates().ratesAndStaleForCurrencies(currencyKeysWithSNX);
     }
 
-    /**
-     * @notice Total amount of synths issued by the system, priced in currencyKey
-     * @param currencyKey The currency to value the synths in
-     */
     function _totalIssuedSynths(bytes32 currencyKey, bool excludeEtherCollateral) internal view returns (uint) {
         IExchangeRates exRates = exchangeRates();
         uint total = 0;
         uint currencyRate = exRates.rateForCurrency(currencyKey);
 
-        uint[] memory rates = exRates.ratesForCurrencies(availableCurrencyKeys());
+        uint[] memory rates = exRates.ratesForCurrencies(_availableCurrencyKeysWithOptionalSNX(false));
 
         for (uint i = 0; i < availableSynths.length; i++) {
             // What's the total issued value of that synth in the destination currency?
@@ -168,23 +164,15 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
         return total.divideDecimalRound(currencyRate);
     }
 
-    /**
-     * @notice Total amount of synths issued by the system priced in currencyKey
-     * @param currencyKey The currency to value the synths in
-     */
-    function totalIssuedSynths(bytes32 currencyKey) public view returns (uint) {
+    function totalIssuedSynths(bytes32 currencyKey) external view returns (uint) {
         return _totalIssuedSynths(currencyKey, false);
     }
 
-    /**
-     * @notice Total amount of synths issued by the system priced in currencyKey, excluding ether collateral
-     * @param currencyKey The currency to value the synths in
-     */
-    function totalIssuedSynthsExcludeEtherCollateral(bytes32 currencyKey) public view returns (uint) {
+    function totalIssuedSynthsExcludeEtherCollateral(bytes32 currencyKey) external view returns (uint) {
         return _totalIssuedSynths(currencyKey, true);
     }
 
-    function availableCurrencyKeys() public view returns (bytes32[] memory) {
+    function availableCurrencyKeys() external view returns (bytes32[] memory) {
         return _availableCurrencyKeysWithOptionalSNX(false);
     }
 
@@ -255,10 +243,13 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
         // track these events specifically.
     }
 
-    /**
-     * @notice ERC20 transfer function.
-     */
-    function transfer(address to, uint value) public optionalProxy systemActive noSynthOrSynthetixRateStale returns (bool) {
+    function transfer(address to, uint value)
+        external
+        optionalProxy
+        systemActive
+        noSynthOrSynthetixRateStale
+        returns (bool)
+    {
         // Ensure they're not trying to exceed their staked SNX amount
         require(value <= transferableSynthetix(messageSender), "Cannot transfer staked or escrowed SNX");
 
@@ -268,14 +259,11 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
         return true;
     }
 
-    /**
-     * @notice ERC20 transferFrom function.
-     */
     function transferFrom(
         address from,
         address to,
         uint value
-    ) public optionalProxy systemActive noSynthOrSynthetixRateStale returns (bool) {
+    ) external optionalProxy systemActive noSynthOrSynthetixRateStale returns (bool) {
         // Ensure they're not trying to exceed their locked amount
         require(value <= transferableSynthetix(from), "Cannot transfer staked or escrowed SNX");
 
@@ -374,10 +362,6 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
 
     // ========== Issuance/Burning ==========
 
-    /**
-     * @notice The maximum synths an issuer can issue against their total synthetix quantity.
-     * This ignores any already issued synths, and is purely giving you the maximimum amount the user can issue.
-     */
     function maxIssuableSynths(address _issuer) public view returns (uint) {
         // What is the value of their SNX balance in the destination currency?
         uint destinationValue = exchangeRates().effectiveValue("SNX", collateral(_issuer), sUSD);
@@ -386,15 +370,6 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
         return destinationValue.multiplyDecimal(synthetixState().issuanceRatio());
     }
 
-    /**
-     * @notice The current collateralisation ratio for a user. Collateralisation ratio varies over time
-     * as the value of the underlying Synthetix asset changes,
-     * e.g. based on an issuance ratio of 20%. if a user issues their maximum available
-     * synths when they hold $10 worth of Synthetix, they will have issued $2 worth of synths. If the value
-     * of Synthetix changes, the ratio returned by this function will adjust accordingly. Users are
-     * incentivised to maintain a collateralisation ratio as close to the issuance ratio as possible by
-     * altering the amount of fees they're able to claim from the system.
-     */
     function collateralisationRatio(address _issuer) public view returns (uint) {
         uint totalOwnedSynthetix = collateral(_issuer);
         if (totalOwnedSynthetix == 0) return 0;
@@ -403,12 +378,6 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
         return debtBalance.divideDecimalRound(totalOwnedSynthetix);
     }
 
-    /**
-     * @notice If a user issues synths backed by SNX in their wallet, the SNX become locked. This function
-     * will tell you how many synths a user has to give back to the system in order to unlock their original
-     * debt position. This is priced in whichever synth is passed in as a currency key, e.g. you can price
-     * the debt in sUSD, or any other synth you wish.
-     */
     function debtBalanceOf(address _issuer, bytes32 currencyKey) public view returns (uint) {
         ISynthetixState state = synthetixState();
 
@@ -435,7 +404,7 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
         (initialDebtOwnership, debtEntryIndex) = state.issuanceData(_issuer);
 
         // What's the total value of the system excluding ETH backed synths in their requested currency?
-        totalSystemValue = totalIssuedSynthsExcludeEtherCollateral(currencyKey);
+        totalSystemValue = _totalIssuedSynths(currencyKey, true);
 
         // If it's zero, they haven't issued, and they have no debt.
         if (initialDebtOwnership == 0) return (0, totalSystemValue);
@@ -456,12 +425,8 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
         debtBalance = highPrecisionBalance.preciseDecimalToDecimal();
     }
 
-    /**
-     * @notice The remaining synths an issuer can issue against their total synthetix balance.
-     * @param _issuer The account that intends to issue
-     */
     function remainingIssuableSynths(address _issuer)
-        public
+        external
         view
         returns (
             // Don't need to check for synth existing or stale rates because maxIssuableSynths will do it for us.
@@ -480,12 +445,6 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
         }
     }
 
-    /**
-     * @notice The total SNX owned by this account, both escrowed and unescrowed,
-     * against which synths can be issued.
-     * This includes those already being used as collateral (locked), and those
-     * available for further issuance (unlocked).
-     */
     function collateral(address account) public view returns (uint) {
         uint balance = tokenState.balanceOf(account);
 
@@ -500,11 +459,6 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
         return balance;
     }
 
-    /**
-     * @notice The number of SNX that are free to be transferred for an account.
-     * @dev Escrowed SNX are not transferable, so they are not included
-     * in this calculation.
-     */
     function transferableSynthetix(address account) public view returns (uint) {
         // How many SNX do they have, excluding escrow?
         // Note: We're excluding escrow here because we're interested in their transferable amount
@@ -525,12 +479,6 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
         }
     }
 
-    /**
-     * @notice Mints the inflationary SNX supply. The inflation shedule is
-     * defined in the SupplySchedule contract.
-     * The mint() function is publicly callable by anyone. The caller will
-     receive a minter reward as specified in supplySchedule.minterReward().
-     */
     function mint() external issuanceActive returns (bool) {
         require(address(rewardsDistribution()) != address(0), "RewardsDistribution not set");
 
@@ -571,7 +519,7 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
     // ========== MODIFIERS ==========
 
     modifier onlyExchanger() {
-        require(msg.sender == address(exchanger()), "Only the exchanger contract can invoke this function");
+        require(msg.sender == address(exchanger()), "Only Exchanger can invoke this");
         _;
     }
 
@@ -586,7 +534,7 @@ contract Synthetix is IERC20, ExternStateToken, MixinResolver, ISynthetix {
     }
 
     modifier noSynthOrSynthetixRateStale() {
-        require(!_anySynthOrSNXRateIsStale(), "At least one synth or SNX rate is stale");
+        require(!_anySynthOrSNXRateIsStale(), "A synth or SNX rate is stale");
         _;
     }
 
