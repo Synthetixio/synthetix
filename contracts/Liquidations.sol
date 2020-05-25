42d7abeadf36e2d48508e4dc9ff07ec10662a78b
Jackson C <jackosmacko@gmail.com>
2020-05-25 17:24:11 +1000

    cleanup

--- contracts/Liquidations.sol
+++ contracts/Liquidations.sol
@@ -21,7 +21,7 @@ contract Liquidations is Owned, MixinResolver, ILiquidations {
 
     struct LiquidationEntry {
         bool isFlagged;
-        uint timestamp;
+        uint deadline;
     }
 
     bytes32 private constant sUSD = "sUSD";
@@ -39,7 +39,7 @@ contract Liquidations is Owned, MixinResolver, ILiquidations {
 
     // Storage keys
     bytes32 public constant LIQUIDATION_FLAG = "LiquidationFlag";
-    bytes32 public constant LIQUIDATION_TIMESTAMP = "LiquidationTimestamp";
+    bytes32 public constant LIQUIDATION_DEADLINE = "LiquidationDeadline";
 
     uint public liquidationDelay = 2 weeks; // liquidation time delay after address flagged
     uint public liquidationRatio = (10 * SafeDecimalMath.unit()) / 15; // collateral ratio when account can be flagged for liquidation
@@ -53,7 +53,7 @@ contract Liquidations is Owned, MixinResolver, ILiquidations {
     }
 
     // refactor to synthetix storage eternal storage contract once that's ready
-    function liquidationEternalStorage() internal view returns (IssuanceEternalStorage) {
+    function liquidationEternalStorage() internal view returns (EternalStorage) {
         return
             EternalStorage(
                 requireAndGetAddress(CONTRACT_LIQUIDATIONETNERALSTORAGE, "Missing LiquidationEternalStorage address")
@@ -62,9 +62,9 @@ contract Liquidations is Owned, MixinResolver, ILiquidations {
 
     /* ========== VIEWS ========== */
     function isOpenForLiquidation(address _account) external view returns (bool) {
-        LiquidationEntry memory liquidation = _getLiquidationForAccount(_account);
+        LiquidationEntry memory liquidation = _getLiquidationEntryForAccount(_account);
 
-        uint ratio = synthetix().collateralisationRatio(account);
+        uint ratio = synthetix().collateralisationRatio(_account);
 
         // Liquidation closed if collateral ratio less than equal liquidation target cap
         if (ratio <= liquidationTargetRatio) {
@@ -72,7 +72,7 @@ contract Liquidations is Owned, MixinResolver, ILiquidations {
         }
 
         // only need to check c-ratio is >= liquidationRatio, liquidation cap is checked above
-        if (ratio >= liquidationRatio && liquidation.isFlagged && liquidation.timestamp.add(liquidationDelay) > now) {
+        if (ratio >= liquidationRatio && liquidation.isFlagged && now.add(liquidationDelay) > liquidation.deadline) {
             return true;
         }
 
@@ -83,48 +83,43 @@ contract Liquidations is Owned, MixinResolver, ILiquidations {
     // returns is flagged false if not set
     function _getLiquidationEntryForAccount(address account) internal view returns (LiquidationEntry memory _liquidation) {
         _liquidation.isFlagged = liquidationEternalStorage().getBooleanValue(_getKey(LIQUIDATION_FLAG, account));
-        _liquidation.timestamp = liquidationEternalStorage().getUIntValue(_getKey(LIQUIDATION_TIMESTAMP, account));
-    };
+        _liquidation.deadline = liquidationEternalStorage().getUIntValue(_getKey(LIQUIDATION_DEADLINE, account));
+    }
 
     function _getKey(
         bytes32 _scope,
         address _account
     ) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(_scope, _account));
-    };
+    }
 
     /* ========== SETTERS ========== */
     function setLiquidationDelay(uint _time) external onlyOwner {
         liquidationDelay = _time;
         // emit event
-    };
+    }
 
     function setLiquidationRatio(uint _liquidationRatio) external onlyOwner {
         liquidationRatio = _liquidationRatio;
         // emit event
-    };
+    }
 
     function setLiquidationTargetRatio(uint target) external;
 
     /* ========== MUTATIVE FUNCTIONS ========== */
-    function _setLastIssueEvent(address account) internal {
-        // Set the timestamp of the last issueSynths
-        issuanceEternalStorage().setUIntValue(keccak256(abi.encodePacked(LAST_ISSUE_EVENT, account)), block.timestamp);
-    }
+    function flagAccountForLiquidation(address account) external {}
 
-    function flagAccountForLiquidation(address account) external {};
+    function removeAccountInLiquidation(address account) external {}
 
-    function removeAccountInLiquidation(address account) external {};
-
-    function checkAndRemoveAccountInLiquidation(address account) external {};
+    function checkAndRemoveAccountInLiquidation(address account) external {}
 
     function _storeLiquidationEntry(address _account, bool _flag) internal {
         // set liquidation flag state
-        liquidationEternalStorage().setUIntValue(_getKey(LIQUIDATION_FLAG, _account), _flag);
+        liquidationEternalStorage().setBooleanValue(_getKey(LIQUIDATION_FLAG, _account), _flag);
 
-        // record liquidation timestamp
-        liquidationEternalStorage().setUIntValue(_getKey(LIQUIDATION_TIMESTAMP, _account), now);
-    };
+        // record liquidation deadline
+        liquidationEternalStorage().setUIntValue(_getKey(LIQUIDATION_DEADLINE, _account), now.add(liquidationDelay));
+    }
 
     /* ========== MODIFIERS ========== */
 
