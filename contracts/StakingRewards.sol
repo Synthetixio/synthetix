1da96040a4de508f5594c367efbda6cdcbdd9220
Clinton Ennis <clinton@synthetix.io>
2020-06-10 15:17:52 +1000

    Implemented IStakingRewards

--- contracts/StakingRewards.sol
+++ contracts/StakingRewards.sol
@@ -5,9 +5,11 @@ import "openzeppelin-solidity-2.3.0/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity-2.3.0/contracts/token/ERC20/SafeERC20.sol";
 import "openzeppelin-solidity-2.3.0/contracts/utils/ReentrancyGuard.sol";
 
+// Inheritance
+import "./interfaces/IStakingRewards.sol";
 import "./RewardsDistributionRecipient.sol";
 
-contract StakingRewards is RewardsDistributionRecipient, ReentrancyGuard {
+contract StakingRewards is IStakingRewards, RewardsDistributionRecipient, ReentrancyGuard {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
