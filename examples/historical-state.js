cd2d2d7ff6fff5c3b04d84f3eb84c23217a33968
justin j. moses <justinjmoses@gmail.com>
2020-07-01 14:28:39 -0700

    include Etherscan API key for ABIs

--- examples/historical-state.js
+++ examples/historical-state.js
@@ -3,6 +3,8 @@
 const commander = require('commander');
 const program = new commander.Command();
 
+const axios = require('axios');
+
 const { gray, yellow, cyan } = require('chalk');
 
 const Web3 = require('web3');
@@ -13,42 +15,76 @@ program
 	.description('Inspect historical state of Synthetix at some given block')
 	.arguments('[args...]')
 	.option('-b, --block-number <value>', 'Block')
-	.option('-c, --contract <value>', 'The contract label', 'ProxyERC20')
+	.option('-c, --contract <value>', 'The contract label or address', 'ProxyERC20')
 	.option('-s, --source <value>', 'The label of the source contract', 'Synthetix')
 	.option('-m, --method <value>', 'The method name', 'totalIssuedSynths')
 	.option('-n, --network <value>', 'The network to run off.', x => x.toLowerCase(), 'mainnet')
 	.option('-i, --infura-project-id <value>', 'An infura project ID with access to archive state')
-	.action(async (_, { network, contract, source, blockNumber, method, infuraProjectId, args }) => {
-		args = args.length ? args : [toBytes32('sUSD')];
+	.option('-e, --etherscan-key <value>', 'Etherscan api key')
+	.action(
+		async (
+			_,
+			{ network, contract, source, blockNumber, method, infuraProjectId, etherscanKey, args }
+		) => {
+			args = args.length ? args : [toBytes32('sUSD')];
 
-		if (!infuraProjectId) {
-			require('dotenv').config();
-			infuraProjectId = process.env.INFURA_PROJECT_ID;
-			if (!infuraProjectId) {
-				throw Error('Missing infura project ID');
+			if (!infuraProjectId || !etherscanKey) {
+				require('dotenv').config();
+				infuraProjectId = infuraProjectId || process.env.INFURA_PROJECT_ID;
+				etherscanKey = etherscanKey || process.env.ETHERSCAN_KEY;
+				if (!infuraProjectId) {
+					throw Error('Missing infura project ID');
+				}
 			}
-		}
 
-		const providerUrl = `https://${network}.infura.io/v3/${infuraProjectId}`;
-		const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
+			const providerUrl = `https://${network}.infura.io/v3/${infuraProjectId}`;
+			const web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
 
-		const getContract = ({ contract }) =>
-			new web3.eth.Contract(
-				getSource({ network, contract: source || getTarget({ network, contract }).source }).abi,
-				getTarget({ network, contract }).address
+			const contractAddress = /^0x/.test(contract)
+				? contract
+				: getTarget({ network, contract }).address;
+
+			const etherscanUrl =
+				network === 'mainnet'
+					? 'https://api.etherscan.io/api'
+					: `https://api-${network}.etherscan.io/api`;
+
+			let abi;
+			if (blockNumber) {
+				// try fetch ABI from etherscan (as our version might be to new)
+				const result = await axios.get(etherscanUrl, {
+					params: {
+						module: 'contract',
+						action: 'getabi',
+						address: contractAddress,
+						apikey: process.env.ETHERSCAN_KEY || 'demo',
+					},
+				});
+				try {
+					abi = JSON.parse(result.data.result);
+				} catch (err) {
+					console.log(gray('Error: could not fetch ABI from Etherscan', err));
+				}
+			}
+
+			abi =
+				abi ||
+				getSource({ network, contract: source || getTarget({ network, contract }).source }).abi;
+
+			const Contract = new web3.eth.Contract(abi, contractAddress);
+
+			const response = await Contract.methods[method](...args).call(blockNumber);
+
+			console.log(
+				gray('Block'),
+				blockNumber ? cyan(blockNumber) : gray('(latest)'),
+				gray(`${contract}.${method}(${args}):`)
 			);
-
-		const Contract = getContract({ contract });
-
-		const response = await Contract.methods[method](...args).call(blockNumber);
-
-		console.log(
-			gray('Block'),
-			blockNumber ? cyan(blockNumber) : gray('(latest)'),
-			gray(`${contract}.${method}(${args}):`),
-			yellow(response)
-		);
-	});
+			console.log(
+				yellow(typeof response === 'object' ? JSON.stringify(response, null, '\t') : response)
+			);
+		}
+	);
 
 // perform as CLI tool if args given
 if (require.main === module) {
