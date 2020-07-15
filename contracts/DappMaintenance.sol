19efd2d3dfdda43cf0ad80773598c9385dcd66d9
Clement Balestrat <clement.balestrat@gmail.com>
2020-07-15 16:16:42 +1000

    dappMaintenance added

---
+++ contracts/DappMaintenance.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.5.16;
+
+import "./Owned.sol";
+
+
+/**
+ * @title DappMaintenance contract.
+ * @dev When the Synthetix system is on maintenance (upgrade, release...etc) the dApps also need
+ * to be put on maintenance so no transactions can be done. The DappMaintenance contract is here to keep a state of
+ * the dApps which indicates if yes or no, they should be up or down.
+ */
+contract DappMaintenance is Owned {
+    bool public isPausedMintr = false;
+    bool public isPausedSX = false;
+
+    /**
+     * @dev Constructor
+     */
+    constructor(address _owner) public Owned(_owner) {
+        require(_owner != address(0), "Owner address cannot be 0");
+        owner = _owner;
+        emit OwnerChanged(address(0), _owner);
+    }
+
+    function setMaintenanceModeAll(bool isPaused) external onlyOwner {
+        isPausedMintr = isPaused;
+        isPausedSX = isPaused;
+        emit MintrMaintenance(isPaused);
+        emit SXMaintenance(isPaused);
+    }
+
+    function setMaintenanceModeMintr(bool isPaused) external onlyOwner {
+        isPausedMintr = isPaused;
+        emit MintrMaintenance(isPausedMintr);
+    }
+
+    function setMaintenanceModeSX(bool isPaused) external onlyOwner {
+        isPausedSX = isPaused;
+        emit SXMaintenance(isPausedSX);
+    }
+
+    event MintrMaintenance(bool isPaused);
+    event SXMaintenance(bool isPaused);
+}
