ea4fe12aecc43c696432dab3ac6ac2e91dd80751
Anton Jurisevic <zyzek@users.noreply.github.com>
2020-07-18 21:29:38 +1000

    Fix binary option market data utility compilation.

--- contracts/BinaryOptionMarketData.sol
+++ contracts/BinaryOptionMarketData.sol
@@ -2,13 +2,12 @@ pragma solidity ^0.5.16;
 pragma experimental ABIEncoderV2;
 
 // Inheritance
-import "./interfaces/IERC20.sol";
-import "./interfaces/IBinaryOption.sol";
-import "./interfaces/IBinaryOptionMarket.sol";
-import "./interfaces/IBinaryOptionMarketManager.sol";
+import "./BinaryOption.sol";
+import "./BinaryOptionMarket.sol";
+import "./BinaryOptionMarketManager.sol";
+
 
 contract BinaryOptionMarketData {
-
     struct OptionValues {
         uint long;
         uint short;
@@ -32,20 +31,20 @@ contract BinaryOptionMarketData {
     // used for things that don't change over the lifetime of the contract
     struct MarketParameters {
         address creator;
-        IBinaryOptionMarket.Options options;
-        IBinaryOptionMarket.Times times;
-        IBinaryOptionMarket.OracleDetails oracleDetails;
-        IBinaryOptionMarketManager.Fees fees;
-        IBinaryOptionMarketManager.CreatorLimits creatorLimits;
+        BinaryOptionMarket.Options options;
+        BinaryOptionMarket.Times times;
+        BinaryOptionMarket.OracleDetails oracleDetails;
+        BinaryOptionMarketManager.Fees fees;
+        BinaryOptionMarketManager.CreatorLimits creatorLimits;
     }
 
     struct MarketData {
         OraclePriceAndTimestamp oraclePriceAndTimestamp;
-        IBinaryOptionMarket.Prices prices;
+        BinaryOptionMarket.Prices prices;
         Deposits deposits;
         Resolution resolution;
-        IBinaryOptionMarket.Phase phase;
-        IBinaryOptionMarket.Side result;
+        BinaryOptionMarket.Phase phase;
+        BinaryOptionMarket.Side result;
         OptionValues totalBids;
         OptionValues totalClaimableSupplies;
         OptionValues totalSupplies;
@@ -57,58 +56,58 @@ contract BinaryOptionMarketData {
         OptionValues balances;
     }
 
-    function getMarketParameters(IBinaryOptionMarket market) public view returns (MarketParameters memory) {
-
-        (IBinaryOption long, IBinaryOption short) = market.options();
+    function getMarketParameters(BinaryOptionMarket market) public view returns (MarketParameters memory) {
+        (BinaryOption long, BinaryOption short) = market.options();
         (uint biddingEndDate, uint maturityDate, uint expiryDate) = market.times();
         (bytes32 key, uint strikePrice, uint finalPrice) = market.oracleDetails();
         (uint poolFee, uint creatorFee, uint refundFee) = market.fees();
 
         MarketParameters memory data = MarketParameters(
             market.creator(),
-            IBinaryOptionMarket.Options(long, short),
-            IBinaryOptionMarket.Times(biddingEndDate,maturityDate,expiryDate),
-            IBinaryOptionMarket.OracleDetails(key, strikePrice, finalPrice),
-            IBinaryOptionMarketManager.Fees(poolFee, creatorFee, refundFee),
-            IBinaryOptionMarketManager.CreatorLimits(0, 0)
+            BinaryOptionMarket.Options(long, short),
+            BinaryOptionMarket.Times(biddingEndDate, maturityDate, expiryDate),
+            BinaryOptionMarket.OracleDetails(key, strikePrice, finalPrice),
+            BinaryOptionMarketManager.Fees(poolFee, creatorFee, refundFee),
+            BinaryOptionMarketManager.CreatorLimits(0, 0)
         );
 
         // Stack too deep otherwise.
         (uint capitalRequirement, uint skewLimit) = market.creatorLimits();
-        data.creatorLimits = IBinaryOptionMarketManager.CreatorLimits(capitalRequirement, skewLimit);
+        data.creatorLimits = BinaryOptionMarketManager.CreatorLimits(capitalRequirement, skewLimit);
         return data;
     }
 
-    function getMarketData(IBinaryOptionMarket market) public view returns (MarketData memory) {
-
+    function getMarketData(BinaryOptionMarket market) public view returns (MarketData memory) {
         (uint price, uint updatedAt) = market.oraclePriceAndTimestamp();
         (uint longClaimable, uint shortClaimable) = market.totalClaimableSupplies();
         (uint longSupply, uint shortSupply) = market.totalSupplies();
         (uint longBids, uint shortBids) = market.totalBids();
         (uint longPrice, uint shortPrice) = market.prices();
 
-        return MarketData(
-            OraclePriceAndTimestamp(price, updatedAt),
-            IBinaryOptionMarket.Prices(longPrice, shortPrice),
-            Deposits(market.deposited(), market.exercisableDeposits()),
-            Resolution(market.resolved(), market.canResolve()),
-            market.phase(),
-            market.result(),
-            OptionValues(longBids, shortBids),
-            OptionValues(longClaimable, shortClaimable),
-            OptionValues(longSupply, shortSupply)
-        );
+        return
+            MarketData(
+                OraclePriceAndTimestamp(price, updatedAt),
+                BinaryOptionMarket.Prices(longPrice, shortPrice),
+                Deposits(market.deposited(), market.exercisableDeposits()),
+                Resolution(market.resolved(), market.canResolve()),
+                market.phase(),
+                market.result(),
+                OptionValues(longBids, shortBids),
+                OptionValues(longClaimable, shortClaimable),
+                OptionValues(longSupply, shortSupply)
+            );
     }
 
-    function getAccountMarketData(IBinaryOptionMarket market, address account) public view returns (AccountData memory) {
+    function getAccountMarketData(BinaryOptionMarket market, address account) public view returns (AccountData memory) {
         (uint longBid, uint shortBid) = market.bidsOf(account);
         (uint longClaimable, uint shortClaimable) = market.claimableBalancesOf(account);
         (uint longBalance, uint shortBalance) = market.balancesOf(account);
 
-        return AccountData(
-            OptionValues(longBid, shortBid),
-            OptionValues(longClaimable, shortClaimable),
-            OptionValues(longBalance, shortBalance)
-        );
+        return
+            AccountData(
+                OptionValues(longBid, shortBid),
+                OptionValues(longClaimable, shortClaimable),
+                OptionValues(longBalance, shortBalance)
+            );
     }
 }
