e90299dc7a706fe4ae90271e2a382323a0615d11
Clinton Ennis <clinton@synthetix.io>
2020-07-22 11:12:04 +1000

    update solidifier reference

--- publish/src/solidity.js
+++ publish/src/solidity.js
@@ -2,7 +2,7 @@
 
 const path = require('path');
 const fs = require('fs');
-const solidifier = require('solidifier/packages/solidifier/index.js');
+const solidifier = require('solidifier');
 const {
 	constants: { COMPILED_FOLDER },
 } = require('../..');
