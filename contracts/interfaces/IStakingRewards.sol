c4dd4413cbbd3c0b40dfee2f9119af2dcb6a82e5
CLINT <clinton@synthetix.io>
2020-07-24 12:27:23 +1000

    Staking rewards Integration (#523)

---
+++ contracts/interfaces/IStakingRewards.sol
@@ -0,0 +1,27 @@
+pragma solidity >=0.4.24;
+
+
+interface IStakingRewards {
+    // Views
+    function lastTimeRewardApplicable() external view returns (uint256);
+
+    function rewardPerToken() external view returns (uint256);
+
+    function earned(address account) external view returns (uint256);
+
+    function getRewardForDuration() external view returns (uint256);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    // Mutative
+
+    function stake(uint256 amount) external;
+
+    function withdraw(uint256 amount) external;
+
+    function getReward() external;
+
+    function exit() external;
+}
