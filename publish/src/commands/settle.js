513a8248c6d2ffcaa2fd725435eb709d1d80aa3a
justin j. moses <justin@synthetix.io>
2020-08-16 12:28:51 -0600

    Deploy SIP-64 and SIP-76 to kovan (#666)

--- publish/src/commands/settle.js
+++ publish/src/commands/settle.js
@@ -18,10 +18,13 @@ const fromBlockMap = {
 	// mainnet: 9518299,
 
 	// blocks from the Acrux deploy (everything prior to this has been settled)
-	kovan: 19220640,
+	// kovan: 19220640, // updated below
 	rinkeby: 6750628,
 	ropsten: 8195362,
 	mainnet: 10364175,
+
+	// blocks from the Pollux deploy
+	kovan: 20291400,
 };
 
 const pathToLocal = name => path.join(__dirname, `${name}.json`);
