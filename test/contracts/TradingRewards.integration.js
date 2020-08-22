3a07c862db47b00aed413ae6204d235192be8f3f
ajs <palebluedot@gmail.com>
2020-08-22 11:52:28 -0300

    Using more currencies in TradingRewards integration tests

--- test/contracts/TradingRewards.integration.js
+++ test/contracts/TradingRewards.integration.js
@@ -2,7 +2,7 @@ const { contract, web3 } = require('@nomiclabs/buidler');
 const { toBN } = web3.utils;
 const { assert, addSnapshotBeforeRestoreAfter } = require('./common');
 const { setupAllContracts } = require('./setup');
-const { currentTime, toUnit } = require('../utils')();
+const { currentTime, toUnit, multiplyDecimal } = require('../utils')();
 const { setExchangeFeeRateForSynths, getDecodedLogs, decodedEventEqual } = require('./helpers');
 const { toBytes32 } = require('../..');
 
@@ -15,9 +15,9 @@ const { toBytes32 } = require('../..');
 contract('TradingRewards (integration tests)', accounts => {
 	const [, owner, account1] = accounts;
 
-	const synths = ['sUSD', 'sETH'];
+	const synths = ['sUSD', 'sETH', 'sBTC'];
 	const synthKeys = synths.map(toBytes32);
-	const [sUSD, sETH] = synthKeys;
+	const [sUSD, sETH, sBTC] = synthKeys;
 
 	let synthetix, exchanger, exchangeRates, rewards, resolver, systemSettings;
 	let sUSDContract, sETHContract;
@@ -26,11 +26,14 @@ contract('TradingRewards (integration tests)', accounts => {
 
 	const zeroAddress = '0x0000000000000000000000000000000000000000';
 
-	const amountIssuedUSD = toUnit('1000');
-	const amountExchangedUSD = toUnit('100');
-	const rateETH = toUnit('100'); // 1 sETH = 100 sUSD
-	const exchangeFeeRate = toUnit('0.005'); // 0.5% fee
-	const feesPaidUSD = toUnit('0.5');
+	const amountIssued = toUnit('1000');
+	const allExchangeFeeRates = toUnit('0.001');
+	const rates = {
+		[sETH]: toUnit('100'),
+		[sBTC]: toUnit('12000'),
+	};
+
+	let feesPaidUSD;
 
 	async function getExchangeLogs({ exchangeTx }) {
 		const logs = await getDecodedLogs({
@@ -41,10 +44,24 @@ contract('TradingRewards (integration tests)', accounts => {
 		return logs.filter(log => log !== undefined);
 	}
 
-	async function executeTrade() {
-		const exchangeTx = await synthetix.exchange(sUSD, amountExchangedUSD, sETH, {
-			from: account1,
-		});
+	async function executeTrade({ account, fromCurrencyKey, fromCurrencyAmount, toCurrencyKey }) {
+		const exchangeTx = await synthetix.exchange(
+			fromCurrencyKey,
+			fromCurrencyAmount,
+			toCurrencyKey,
+			{
+				from: account,
+			}
+		);
+
+		const { amountReceived, fee, exchangeFeeRate } = await exchanger.getAmountsForExchange(
+			fromCurrencyAmount,
+			fromCurrencyKey,
+			toCurrencyKey
+		);
+
+		const rate = rates[toCurrencyKey];
+		feesPaidUSD = multiplyDecimal(fee, rate);
 
 		exchangeLogs = await getExchangeLogs({ exchangeTx });
 	}
@@ -59,6 +76,7 @@ contract('TradingRewards (integration tests)', accounts => {
 				ExchangeRates: exchangeRates,
 				SynthsUSD: sUSDContract,
 				SynthsETH: sETHContract,
+				SynthsBTC: sBTCContract,
 				SystemSettings: systemSettings,
 			} = await setupAllContracts({
 				accounts,
@@ -74,15 +92,17 @@ contract('TradingRewards (integration tests)', accounts => {
 			}));
 		});
 
-		before('mint some sUSD', async () => {
-			await sUSDContract.issue(account1, amountIssuedUSD);
+		before('BRRRRRR', async () => {
+			await sUSDContract.issue(account1, amountIssued);
+			await sETHContract.issue(account1, amountIssued);
+			await sBTCContract.issue(account1, amountIssued);
 		});
 
 		before('set exchange rates', async () => {
 			const oracle = account1;
 			const timestamp = await currentTime();
 
-			await exchangeRates.updateRates([sETH], [rateETH], timestamp, {
+			await exchangeRates.updateRates([sETH, sBTC], Object.values(rates), timestamp, {
 				from: oracle,
 			});
 
@@ -90,14 +110,14 @@ contract('TradingRewards (integration tests)', accounts => {
 				owner,
 				systemSettings,
 				synthKeys,
-				exchangeFeeRates: synthKeys.map(() => exchangeFeeRate),
+				exchangeFeeRates: synthKeys.map(() => allExchangeFeeRates),
 			});
 		});
 
 		it('has expected balances for accounts', async () => {
-			assert.bnEqual(amountIssuedUSD, await sUSDContract.balanceOf(account1));
-
-			assert.bnEqual(toBN(0), await sETHContract.balanceOf(account1));
+			assert.bnEqual(amountIssued, await sUSDContract.balanceOf(account1));
+			assert.bnEqual(amountIssued, await sETHContract.balanceOf(account1));
+			assert.bnEqual(amountIssued, await sBTCContract.balanceOf(account1));
 		});
 
 		it('has expected parameters', async () => {
@@ -117,7 +137,12 @@ contract('TradingRewards (integration tests)', accounts => {
 				addSnapshotBeforeRestoreAfter();
 
 				before('perform an exchange and get tx logs', async () => {
-					await executeTrade();
+					await executeTrade({
+						account: account1,
+						fromCurrencyKey: sUSD,
+						fromCurrencyAmount: toUnit('100'),
+						toCurrencyKey: sETH,
+					});
 				});
 
 				it('emitted a SynthExchange event', async () => {
@@ -144,35 +169,75 @@ contract('TradingRewards (integration tests)', accounts => {
 				assert.isTrue(await exchanger.tradingRewardsEnabled());
 			});
 
-			describe('when performing an regular exchange', () => {
-				addSnapshotBeforeRestoreAfter();
+			const itCorrectlyPerformsAnExchange = ({
+				account,
+				fromCurrencyKey,
+				fromCurrencyAmount,
+				toCurrencyKey,
+			}) => {
+				describe('when performing a regular exchange', () => {
+					addSnapshotBeforeRestoreAfter();
 
-				before('perform an exchange and get tx logs', async () => {
-					await executeTrade();
-				});
+					before('perform an exchange and get tx logs', async () => {
+						await executeTrade({
+							account,
+							fromCurrencyKey,
+							fromCurrencyAmount,
+							toCurrencyKey,
+						});
+					});
 
-				it('emitted a SynthExchange event', async () => {
-					assert.isTrue(exchangeLogs.some(log => log.name === 'SynthExchange'));
-				});
+					it('emitted a SynthExchange event', async () => {
+						assert.isTrue(exchangeLogs.some(log => log.name === 'SynthExchange'));
+					});
 
-				it('emitted an ExchangeFeeRecorded event', async () => {
-					assert.isTrue(exchangeLogs.some(log => log.name === 'ExchangeFeeRecorded'));
+					it('emitted an ExchangeFeeRecorded event', async () => {
+						assert.isTrue(exchangeLogs.some(log => log.name === 'ExchangeFeeRecorded'));
 
-					const feeRecordLog = exchangeLogs.find(log => log.name === 'ExchangeFeeRecorded');
-					decodedEventEqual({
-						event: 'ExchangeFeeRecorded',
-						log: feeRecordLog,
-						emittedFrom: rewards.address,
-						args: [account1, feesPaidUSD, 0],
+						const feeRecordLog = exchangeLogs.find(log => log.name === 'ExchangeFeeRecorded');
+						decodedEventEqual({
+							event: 'ExchangeFeeRecorded',
+							log: feeRecordLog,
+							emittedFrom: rewards.address,
+							args: [account, feesPaidUSD, 0],
+						});
+					});
+
+					it('recorded a fee in TradingRewards', async () => {
+						assert.bnEqual(
+							await rewards.getUnaccountedFeesForAccountForPeriod(account1, 0),
+							feesPaidUSD
+						);
 					});
 				});
+			};
 
-				it('recorded a fee in TradingRewards', async () => {
-					assert.bnEqual(
-						await rewards.getUnaccountedFeesForAccountForPeriod(account1, 0),
-						feesPaidUSD
-					);
-				});
+			itCorrectlyPerformsAnExchange({
+				account: account1,
+				fromCurrencyKey: sUSD,
+				fromCurrencyAmount: toUnit('100'),
+				toCurrencyKey: sETH,
+			});
+
+			itCorrectlyPerformsAnExchange({
+				account: account1,
+				fromCurrencyKey: sUSD,
+				fromCurrencyAmount: toUnit('100'),
+				toCurrencyKey: sBTC,
+			});
+
+			itCorrectlyPerformsAnExchange({
+				account: account1,
+				fromCurrencyKey: sETH,
+				fromCurrencyAmount: toUnit('10'),
+				toCurrencyKey: sBTC,
+			});
+
+			itCorrectlyPerformsAnExchange({
+				account: account1,
+				fromCurrencyKey: sBTC,
+				fromCurrencyAmount: toUnit('1'),
+				toCurrencyKey: sETH,
 			});
 
 			describe('when exchangeFeeRate is set to 0', () => {
@@ -191,7 +256,12 @@ contract('TradingRewards (integration tests)', accounts => {
 
 				describe('when performing an exchange', () => {
 					before('perform an exchange and get tx logs', async () => {
-						await executeTrade();
+						await executeTrade({
+							account: account1,
+							fromCurrencyKey: sUSD,
+							fromCurrencyAmount: toUnit('100'),
+							toCurrencyKey: sETH,
+						});
 					});
 
 					it('emitted a SynthExchange event', async () => {
@@ -211,7 +281,7 @@ contract('TradingRewards (integration tests)', accounts => {
 					before('execute exchange with tracking', async () => {
 						const exchangeTx = await synthetix.exchangeWithTracking(
 							sUSD,
-							amountExchangedUSD,
+							toUnit('100'),
 							sETH,
 							account1,
 							toBytes32('1INCH'),
@@ -236,7 +306,7 @@ contract('TradingRewards (integration tests)', accounts => {
 					before('execute exchange with tracking', async () => {
 						const exchangeTx = await synthetix.exchangeWithTracking(
 							sUSD,
-							amountExchangedUSD,
+							toUnit('100'),
 							sETH,
 							zeroAddress, // No originator = 0x0
 							toBytes32('1INCH'),
