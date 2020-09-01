5389e0e0fba02d934d178624eb5c3c206e35866d
ajs <palebluedot@gmail.com>
2020-09-01 14:54:48 -0300

    Progress on prod tests for TradingRewards

--- test/contracts/TradingRewards.prod.js
+++ test/contracts/TradingRewards.prod.js
@@ -1,18 +1,24 @@
-const { chainIdToNetwork, getSource, getTarget, getUsers } = require('../../index.js');
+const { chainIdToNetwork, getSource, getTarget, getUsers, toBytes32 } = require('../../index.js');
 const { assert, addSnapshotBeforeRestoreAfter } = require('./common');
 const { contract, web3 } = require('@nomiclabs/buidler');
+const { toUnit } = require('../utils')();
+const { getDecodedLogs } = require('./helpers');
 
 contract('TradingRewards (prod tests)', () => {
 	let network;
 
 	let owner;
 
-	let tradingRewards, addressResolver, systemSettings;
+	let TradingRewards, AddressResolver, SystemSettings, Synthetix;
+
+	const currencyKeys = ['sUSD', 'sETH'];
+	const currencyKeysBytes = currencyKeys.map(toBytes32);
+	const [sUSD, sETH] = currencyKeysBytes;
 
 	async function enableTradingRewardsIfNeeded(enabled) {
-		const areEnabled = await systemSettings.methods.tradingRewardsEnabled().call();
+		const areEnabled = await SystemSettings.methods.tradingRewardsEnabled().call();
 		if (enabled !== areEnabled) {
-			await systemSettings.methods.setTradingRewardsEnabled(enabled).send({ from: owner });
+			await SystemSettings.methods.setTradingRewardsEnabled(enabled).send({ from: owner });
 		}
 	}
 
@@ -33,17 +39,22 @@ contract('TradingRewards (prod tests)', () => {
 	});
 
 	before('connect to contracts', async () => {
-		tradingRewards = await connectWithContract({ network, contractName: 'TradingRewards' });
-		addressResolver = await connectWithContract({ network, contractName: 'AddressResolver' });
-		systemSettings = await connectWithContract({ network, contractName: 'SystemSettings' });
+		TradingRewards = await connectWithContract({ network, contractName: 'TradingRewards' });
+		AddressResolver = await connectWithContract({ network, contractName: 'AddressResolver' });
+		SystemSettings = await connectWithContract({ network, contractName: 'SystemSettings' });
+		Synthetix = await connectWithContract({
+			network,
+			contractName: 'Synthetix',
+			abiName: 'ProxyERC20',
+		});
 	});
 
 	it('has the expected resolver set', async () => {
-		assert.equal(await tradingRewards.methods.resolver().call(), addressResolver.options.address);
+		assert.equal(await TradingRewards.methods.resolver().call(), AddressResolver.options.address);
 	});
 
-	it('has the expected setting for tradingRewardsEnabled', async () => {
-		assert.isFalse(await systemSettings.methods.tradingRewardsEnabled().call());
+	it('has the expected setting for tradingRewardsEnabled (disabled)', async () => {
+		assert.isFalse(await SystemSettings.methods.tradingRewardsEnabled().call());
 	});
 
 	describe('when trading rewards are disabled', () => {
@@ -53,8 +64,28 @@ contract('TradingRewards (prod tests)', () => {
 			await enableTradingRewardsIfNeeded(false);
 		});
 
-		it('shows trading rewards enabled', async () => {
-			assert.isFalse(await systemSettings.methods.tradingRewardsEnabled().call());
+		it('shows trading rewards disabled', async () => {
+			assert.isFalse(await SystemSettings.methods.tradingRewardsEnabled().call());
+		});
+
+		describe('when an exchange is made', () => {
+			let exchangeTx;
+
+			before(async () => {
+				exchangeTx = await Synthetix.methods.exchange(sUSD, toUnit('1'), sETH).send({
+					from: owner,
+				});
+			});
+
+			it('did not emit an ExchangeFeeRecorded event', async () => {
+				const logs = await getDecodedLogs({
+					hash: exchangeTx.tx,
+					contracts: [Synthetix, TradingRewards],
+				});
+				console.log('logs', logs);
+			});
+
+			it.skip('did not record a fee', async () => {});
 		});
 
 		// TODO
@@ -68,7 +99,7 @@ contract('TradingRewards (prod tests)', () => {
 		});
 
 		it('shows trading rewards enabled', async () => {
-			assert.isTrue(await systemSettings.methods.tradingRewardsEnabled().call());
+			assert.isTrue(await SystemSettings.methods.tradingRewardsEnabled().call());
 		});
 
 		// TODO
