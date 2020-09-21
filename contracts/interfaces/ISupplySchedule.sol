9e287157657e5b3c6975fdd8b1f9332dfddedd1b
i-stam <i.stamelakos@gmail.com>
2020-09-22 00:33:32 +0100

    Add SupplySchedule interface

---
+++ contracts/interfaces/ISupplySchedule.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.4.24;
+
+
+interface ISupplySchedule {
+    // Views
+    function mintableSupply() external view returns (uint);
+
+    function weeksSinceLastIssuance() external view returns (uint);
+    
+    function isMintable() external view returns (bool);
+
+    // Mutative functions
+    function recordMintEvent(uint supplyMinted) external returns (bool);
+
+    function setMinterReward(uint amount) external;
+
+}
