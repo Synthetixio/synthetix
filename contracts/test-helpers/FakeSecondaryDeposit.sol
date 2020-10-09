8b62af1d6cb5a8bec5a3cbce523cbe9854c33630
i-stam <i.stamelakos@gmail.com>
2020-10-09 13:50:07 +0100

    Add mocks and test deposit()

--- contracts/test-helpers/FakeSecondaryDeposit.sol
+++ contracts/test-helpers/FakeSecondaryDeposit.sol
@@ -4,28 +4,52 @@ import "../SecondaryDeposit.sol";
 import "../interfaces/IIssuer.sol";
 
 
+contract CrossDomainMessengerMock {
+    // mock send message
+    function sendMessage(
+        address _target,
+        bytes calldata _message,
+        uint32 _gasLimit
+    ) external {}
+}
+
+
 contract FakeSecondaryDeposit is SecondaryDeposit {
-    IERC20 public _mockSynthetixToken;
-    IIssuer public _mockIssuer;
+    IERC20 public mockSynthetixToken;
+    IIssuer public mockIssuer;
+    address public crossDomainMessengerMock;
 
     constructor(
-        address owner,
-        address resolver,
-        address mockSynthetixToken,
-        address mockIssuer
-    ) public SecondaryDeposit(owner, resolver) {
-        _mockSynthetixToken = IERC20(mockSynthetixToken);
-        _mockIssuer = IIssuer(mockIssuer);
+        address _owner,
+        address _resolver,
+        address _mockSynthetixToken,
+        address _mockIssuer
+    ) public SecondaryDeposit(_owner, _resolver) {
+        mockSynthetixToken = IERC20(_mockSynthetixToken);
+        mockIssuer = IIssuer(_mockIssuer);
+        crossDomainMessengerMock = address(new CrossDomainMessengerMock());
     }
 
     // Synthetix is mocked with an ERC20 token passed via the constructor.
     function synthetixERC20() internal view returns (IERC20) {
-        return IERC20(_mockSynthetixToken);
+        return mockSynthetixToken;
     }
 
     // Issuer mock
     function issuer() internal view returns (IIssuer) {
-        return IIssuer(_mockIssuer);
+        return mockIssuer;
+    }
+
+    function messenger() internal view returns (ICrossDomainMessenger) {
+        return ICrossDomainMessenger(crossDomainMessengerMock);
+    }
+
+    function companion() internal view returns (address) {
+        return address(0);
+    }
+
+    function getMaximumDeposit() internal view returns (uint) {
+        return 5000 ether;
     }
 
     // Easy way to send ETH to the contract. Alternative is to use selfdestruct, but this is easier.
--- test/contracts/SecondaryDeposit.js
+++ test/contracts/SecondaryDeposit.js
@@ -2,21 +2,22 @@ const { artifacts, contract, web3 } = require('@nomiclabs/buidler');
 const { assert } = require('./common');
 const { ensureOnlyExpectedMutativeFunctions } = require('./helpers');
 const { mockToken, mockGenericContractFnc } = require('./setup');
-const { toWei, toBN } = web3.utils;
+const { toWei } = web3.utils;
 const { toUnit } = require('../utils')();
 const helper = require('./TradingRewards.helper');
+const BN = require('bn.js');
 
 const SecondaryDeposit = artifacts.require('SecondaryDeposit');
 const FakeSecondaryDeposit = artifacts.require('FakeSecondaryDeposit');
+
+const { toBytes32 } = require('../..');
 
 contract('SecondaryDeposit (unit tests)', accounts => {
 	const [deployerAccount, owner, account1] = accounts;
 
 	const mockTokenTotalSupply = '1000000';
-	// const zeroAddress = '0x0000000000000000000000000000000000000000';
 	const mockAddress = '0x0000000000000000000000000000000000000001';
-	const maxDeposit = toUnit('5000');
-	let mock;
+	const maxDeposit = toWei('5000');
 	let depositTx;
 
 	it('ensure only known functions are mutative', () => {
@@ -41,64 +42,82 @@ contract('SecondaryDeposit (unit tests)', accounts => {
 				symbol: 'MCK',
 				supply: mockTokenTotalSupply,
 			}));
-
 			helper.incrementExpectedBalance(account1, mockTokenTotalSupply);
+			// transfer 100 tokens to account1
 			await this.token.transfer(account1, 100, { from: owner });
 		});
 
 		it('has the expected parameters', async () => {
 			assert.equal('18', await this.token.decimals());
 			assert.equal(toWei(mockTokenTotalSupply), await this.token.totalSupply());
+			assert.bnEqual(
+				(await this.token.totalSupply()).sub(new BN(100)),
+				await this.token.balanceOf(owner)
+			);
 			assert.equal(100, await this.token.balanceOf(account1));
 		});
 
-		describe('when a mock for Issuer is added', () => {
-			beforeEach(async () => {
-				mock = await artifacts.require('GenericMock').new();
+		describe('when mocks for Issuer and resolver are added', () => {
+			beforeEach('deploy a mock issuer contract', async () => {
+				this.issuerMock = await artifacts.require('GenericMock').new();
 
-				// now instruct the mock Issuer that debtBalanceOf must return 0
+				// now instruct the mock Issuer that debtBalanceOf() must return 0
 				await mockGenericContractFnc({
-					instance: mock,
+					instance: this.issuerMock,
 					mock: 'Issuer',
 					fncName: 'debtBalanceOf',
-					returns: 0,
+					returns: [0],
 				});
 
-				describe('when a FakeSecondaryDeposit contract is deployed', () => {
-					before('deploy deposit contract', async () => {
-						this.deposit = await FakeSecondaryDeposit.new(
-							owner,
-							mockAddress,
-							this.token.address,
-							mock.address,
-							{
-								from: deployerAccount,
-							}
-						);
+				this.resolverMock = await artifacts.require('GenericMock').new();
+
+				// now instruct the mock Issuer that debtBalanceOf() must return 0
+				await mockGenericContractFnc({
+					instance: this.resolverMock,
+					mock: 'AddressResolver',
+					fncName: 'getAddress',
+					returns: [mockAddress],
+				});
+			});
+
+			it('mocked contracs are deployed', async () => {
+				assert.notEqual(this.resolverMock.address, this.issuerMock.address);
+			});
+
+			describe('when a FakeSecondaryDeposit contract is deployed', () => {
+				before('deploy deposit contract', async () => {
+					this.secondaryDeposit = await FakeSecondaryDeposit.new(
+						owner,
+						this.resolverMock.address,
+						this.token.address,
+						this.issuerMock.address,
+						{
+							from: deployerAccount,
+						}
+					);
+				});
+
+				it('has the expected parameters', async () => {
+					assert.bnEqual(await this.secondaryDeposit.maximumDeposit(), maxDeposit);
+					assert.equal(owner, await this.secondaryDeposit.owner());
+					// assert.equal(this.resolverMock.address, await this.secondaryDeposit.resolver());
+				});
+
+				describe('a user tries to deposit', () => {
+					before('user approves and deposits 100 tokens', async () => {
+						await this.token.approve(this.secondaryDeposit.address, 100, { from: account1 });
+						depositTx = await this.secondaryDeposit.deposit(100, { from: account1 });
 					});
 
-					it('has the expected parameters', async () => {
-						assert.bnEqual(await this.deposit.maximumDeposit(), 0);
-						assert.equal(owner, await this.deposit.owner());
-						assert.equal(mockAddress, await this.deposit.resolver());
+					it('tranfers the tokens to the deposit contract', async () => {
+						assert.equal(100, await this.token.balanceOf(this.secondaryDeposit.address));
+						assert.equal(0, await this.token.balanceOf(account1));
 					});
 
-					describe('a user tries to deposit', () => {
-						before('user approves and deposits 100 tokens', async () => {
-							await this.token.approve(this.deposit.address, 100, { from: account1 });
-							depositTx = await this.deposit.deposit(100, { from: account1 });
-						});
-
-						it('tranfers the tokens to the deposit contract', async () => {
-							assert.equal(100, await this.token.balanceOf(this.deposit.address));
-							assert.equal(0, await this.token.balanceOf(account1));
-						});
-
-						it('tranfers the tokens to the deposit contract', async () => {
-							assert.eventEqual(depositTx, 'Deposit', {
-								account: account1,
-								amount: 100,
-							});
+					it('tranfers the tokens to the deposit contract', async () => {
+						assert.eventEqual(depositTx, 'Deposit', {
+							account: account1,
+							amount: 100,
 						});
 					});
 				});
