2807b8bc03c86ce2575153fb19512030eeb2b526
justin j. moses <justin@synthetix.io>
2020-10-12 17:11:55 -0400

    Upgrade solidity-coverage to 0.7.11 (#786) (#793)

--- .solcover.js
+++ .solcover.js
@@ -7,8 +7,6 @@ module.exports = {
 	skipFiles: [
 		'test-helpers',
 		'EscrowChecker.sol',
-		'EtherCollateralsUSD.sol',
-		'BinaryOptionMarketData.sol',
 	],
 	providerOptions: {
 		default_balance_ether: 10000000000000, // extra zero just in case (coverage consumes more gas)
@@ -20,4 +18,9 @@ module.exports = {
 		grep: '@cov-skip', // Find everything with this tag
 		invert: true, // Run the grep's inverse set.
 	},
+	// Reduce instrumentation footprint - volume of solidity code
+	// passed to compiler causes it to crash (See discussion PR #732)
+	// Line and branch coverage will still be reported.
+	measureStatementCoverage: false,
+	measureFunctionCoverage: true,
 };
