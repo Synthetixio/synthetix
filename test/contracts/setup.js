6e3eb8f1ee1d0fca19c4e12f8189ef420fd953b4
justin j. moses <justinjmoses@gmail.com>
2020-11-16 15:00:37 -0500

    Ensure the rebuild is called after importing addresses

--- test/contracts/setup.js
+++ test/contracts/setup.js
@@ -798,6 +798,12 @@ const setupAllContracts = async ({
 				from: owner,
 			}
 		);
+
+		await returnObj['AddressResolver'].rebuildCaches(
+			Object.values(returnObj)
+				.filter(entry => 'rebuildCache' in entry)
+				.map(({ address }) => address)
+		);
 	}
 
 	// if deploying a real Synthetix, then we add the synths
