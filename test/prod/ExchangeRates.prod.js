5f1cf8a42babfcf8ee4284f00e11378afecfd3bc
justin j. moses <justinjmoses@gmail.com>
2020-12-01 18:39:28 -0500

    Fixing skip again

--- test/prod/ExchangeRates.prod.js
+++ test/prod/ExchangeRates.prod.js
@@ -86,7 +86,7 @@ contract('ExchangeRates (prod tests)', accounts => {
 
 	describe('when an exchange is made', () => {
 		let waitingPeriod;
-		before(async () => {
+		before(async function() {
 			if (config.useOvm) {
 				this.skip();
 			}
@@ -101,6 +101,7 @@ contract('ExchangeRates (prod tests)', accounts => {
 			});
 			waitingPeriod = Number(await SystemSettings.waitingPeriodSecs());
 		});
+
 		it('should settle', async () => {
 			await fastForward(waitingPeriod);
 			await Exchanger.settle(user, toBytes32('sETH'), { from: user });
