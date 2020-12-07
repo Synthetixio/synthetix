d42cbe619287454186a93a78ab199b6c31e4a816
Jackson C <jackosmacko@gmail.com>
2020-12-07 21:37:00 +1100

    update BaseRewardEscrowV2 test

--- test/contracts/BaseRewardEscrowV2.js
+++ test/contracts/BaseRewardEscrowV2.js
@@ -4,7 +4,7 @@ const { contract } = require('@nomiclabs/buidler');
 
 const { assert, addSnapshotBeforeRestoreAfterEach } = require('./common');
 
-const { setupAllContracts } = require('./setup');
+const { mockToken, setupAllContracts } = require('./setup');
 
 // const { toUnit } = require('../utils')();
 
@@ -15,14 +15,23 @@ const { ensureOnlyExpectedMutativeFunctions } = require('./helpers');
 // } = require('../..');
 
 contract('BaseRewardEscrowV2', async accounts => {
-	const [, owner, account1] = accounts;
-	let baseRewardEscrowV2;
+	const [, owner, feePoolAccount, account1] = accounts;
+	let baseRewardEscrowV2, synthetix, feePool;
 
 	// Run once at beginning - snapshots will take care of resetting this before each test
 	before(async () => {
+		// Mock SNX
+		({ token: synthetix } = await mockToken({ accounts, name: 'Synthetix', symbol: 'SNX' }));
+
+		feePool = { address: feePoolAccount }; // mock contract with address
+
 		({ BaseRewardEscrowV2: baseRewardEscrowV2 } = await setupAllContracts({
 			accounts,
 			contracts: ['BaseRewardEscrowV2'],
+			mocks: {
+				Synthetix: synthetix,
+				FeePool: feePool,
+			},
 		}));
 	});
 
@@ -54,11 +63,35 @@ contract('BaseRewardEscrowV2', async accounts => {
 			const ownerAddress = await baseRewardEscrowV2.owner();
 			assert.equal(ownerAddress, owner);
 		});
+		it('should set nextEntryId to 1', async () => {
+			const nextEntryId = await baseRewardEscrowV2.nextEntryId();
+			assert.equal(nextEntryId, 1);
+		});
 	});
 
 	describe('There are no escrow entries initially', async () => {
 		it('then numVestingEntries should return 0', async () => {
 			assert.equal(0, await baseRewardEscrowV2.numVestingEntries(account1));
+		});
+		it('then numVestingEntries should return 0', async () => {
+			assert.equal(0, await baseRewardEscrowV2.numVestingEntries(account1));
+		});
+		it('then totalEscrowedAccountBalance should return 0', async () => {
+			assert.equal(0, await baseRewardEscrowV2.totalEscrowedAccountBalance(account1));
+		});
+		it('then totalVestedAccountBalance should return 0', async () => {
+			assert.equal(0, await baseRewardEscrowV2.totalVestedAccountBalance(account1));
+		});
+	});
+	describe('When a vesting entry is appended for account 1', async () => {
+		beforeEach(async () => {});
+		it('then numVestingEntries should return 0', async () => {
+			assert.equal(0, await baseRewardEscrowV2.numVestingEntries(account1));
+		});
+	});
+	describe('Creating a new escrow entry', async () => {
+		beforeEach(async () => {
+			// approve rewardEscrow to spend SNX
 		});
 	});
 });
