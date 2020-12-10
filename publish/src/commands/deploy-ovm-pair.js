ef33ae042be2ab075d83686a35124f41cc39e3dd
Alejandro <palebluedot@gmail.com>
2020-12-10 14:51:01 -0300

    Deposits working in L2 tests

--- publish/src/commands/deploy-ovm-pair.js
+++ publish/src/commands/deploy-ovm-pair.js
@@ -13,7 +13,16 @@ const DATA_PROVIDER_URL = 'http://localhost:8080';
 const deployOvmPair = async () => {
 	// This private key is #4 displayed when starting optimism-integration.
 	// When used on a fresh L2 chain, it passes all safety checks.
-	// publicKey: 0x640e7cc27b750144ED08bA09515F3416A988B6a3
+	// Account #0: 0x023ffdc1530468eb8c8eebc3e38380b5bc19cc5d (10000 ETH)
+	// Private Key: 0x754fde3f5e60ef2c7649061e06957c29017fe21032a8017132c0078e37f6193a
+	// Account #1: 0x0e0e05cf14349469ee3b45dc2fce50e11b9449b8 (10000 ETH)
+	// Private Key: 0xd2ab07f7c10ac88d5f86f1b4c1035d5195e81f27dbe62ad65e59cbf88205629b
+	// Account #2: 0x432c38a44381668eda4a3152209abbfae065b44d (10000 ETH)
+	// Private Key: 0x23d9aeeaa08ab710a57972eb56fc711d9ab13afdecc92c89586e0150bfa380a6
+	// Account #3: 0x5eeabfdd0f31cebf32f8abf22da451fe46eac131 (10000 ETH)
+	// Private Key: 0x5b1c2653250e5c580dcb4e51c2944455e144c57ebd6a0645bd359d2e69ca0f0c
+	// Account #4: 0x640e7cc27b750144ed08ba09515f3416a988b6a3 (10000 ETH)
+	// Private Key: 0xea8b000efb33c49d819e8d6452f681eed55cdf7de47d655887fc0e318906f2e7
 	const privateKey = '0xea8b000efb33c49d819e8d6452f681eed55cdf7de47d655887fc0e318906f2e7';
 
 	await deployInstance({ useOvm: false, privateKey });
