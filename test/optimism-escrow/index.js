509868328a8247109d444ca690e3d761d2648506
i-stam <i.stamelakos@gmail.com>
2020-12-14 00:01:06 +0000

    Add migrateEscrow tests

--- test/optimism-escrow/index.js
+++ test/optimism-escrow/index.js
@@ -21,12 +21,9 @@ describe('Layer 2 production tests', () => {
 	let providerL1, providerL2;
 
 	let ownerL1, user1L1, user1L2;
-	// let ownerL1, ownerL2, user1L1, user1L2;
 
-	let FeePoolL1, RewardEscrowV2L1, SynthetixBridgeToOptimismL1, SynthetixL1;
+	let RewardEscrowV2L1, SynthetixBridgeToOptimismL1, SynthetixL1;
 	let RewardEscrowV2L2, SynthetixBridgeToBaseL2;
-
-	// const zero = ethers.utils.parseEther('0');
 
 	const cache = {
 		bridge: {
@@ -75,7 +72,6 @@ describe('Layer 2 production tests', () => {
 
 	before('set up signers', () => {
 		ownerL1 = providerL1.getSigner(OWNER_ADDRESS);
-		// ownerL2 = providerL2.getSigner(OWNER_ADDRESS);
 		user1L1 = providerL1.getSigner(USER1_ADDRESS);
 		user1L2 = new ethers.Wallet(USER1_PRIVATE_KEY, providerL2);
 	});
@@ -83,7 +79,6 @@ describe('Layer 2 production tests', () => {
 	describe('when instances have been deployed in local L1 and L2 chains', () => {
 		before('connect to contracts', async () => {
 			SynthetixL1 = connectContract({ contract: 'Synthetix' });
-			FeePoolL1 = connectContract({ contract: 'FeePool' });
 			RewardEscrowV2L1 = connectContract({ contract: 'RewardEscrowV2' });
 			RewardEscrowV2L2 = connectContract({
 				contract: 'RewardEscrowV2',
@@ -95,11 +90,6 @@ describe('Layer 2 production tests', () => {
 				contract: 'SynthetixBridgeToBase',
 				useOvm: true,
 			});
-		});
-
-		it('shows the expected owners', async () => {
-			assert.equal(await RewardEscrowV2L1.owner(), OWNER_ADDRESS);
-			assert.equal(await RewardEscrowV2L2.owner(), OWNER_ADDRESS);
 		});
 
 		it('the initial values are the expected ones', async () => {
@@ -122,7 +112,7 @@ describe('Layer 2 production tests', () => {
 			});
 
 			describe('when the user approves the reward escrow to transfer their SNX', () => {
-				before('approve', async () => {
+				before('approve reward escrow ', async () => {
 					SynthetixL1 = SynthetixL1.connect(user1L1);
 
 					await SynthetixL1.approve(RewardEscrowV2L1.address, snxAmount);
@@ -131,15 +121,94 @@ describe('Layer 2 production tests', () => {
 				describe('when the user creates 52 escrow entries', () => {
 					const escrowEntryAmount = ethers.utils.parseEther('1');
 					const duration = MINUTE;
+					const userEntries = [];
+					const escrowNum = 52;
+					let currentId;
+					let totalEscrowed = ethers.BigNumber.from('0');
 					before('create and append escrow entries', async () => {
 						RewardEscrowV2L1 = RewardEscrowV2L1.connect(user1L1);
-						for (let i = 0; i < 52; i++) {
+						for (let i = 0; i < escrowNum; i++) {
+							currentId = await RewardEscrowV2L1.nextEntryId();
 							await RewardEscrowV2L1.createEscrowEntry(USER1_ADDRESS, escrowEntryAmount, duration);
+							userEntries[i] = currentId;
+							totalEscrowed = totalEscrowed.add(escrowEntryAmount);
 						}
 					});
 
 					it('Should create 52 entry IDs', async () => {
-						assert.bnEqual(await RewardEscrowV2L1.nextEntryId(), '53');
+						assert.equal(userEntries.length, escrowNum);
+						assert.bnEqual(await RewardEscrowV2L1.nextEntryId(), (escrowNum + 1).toString());
+					});
+
+					it('should update the L1 escrow balances', async () => {
+						assert.bnEqual(
+							await RewardEscrowV2L1.numVestingEntries(USER1_ADDRESS),
+							escrowNum.toString()
+						);
+						assert.bnEqual(
+							await RewardEscrowV2L1.totalEscrowedAccountBalance(USER1_ADDRESS),
+							totalEscrowed
+						);
+						assert.bnEqual(await RewardEscrowV2L1.totalVestedAccountBalance(USER1_ADDRESS), '0');
+					});
+
+					describe('when the user has no outsanding debt on L1', () => {
+						describe('when the user wants to migrate their escrow and deposit SNX', () => {
+							describe('when the user has approved the L1 bridge to transfer their SNX', () => {
+								const depositAmount = ethers.utils.parseEther('20');
+								before('approve L1 bridge', async () => {
+									SynthetixL1 = SynthetixL1.connect(user1L1);
+									await SynthetixL1.approve(SynthetixBridgeToOptimismL1.address, depositAmount);
+								});
+
+								describe('when the user deposits SNX along with the migration', () => {
+									before('call depositAndMigrateEscrow', async () => {
+										SynthetixBridgeToOptimismL1 = SynthetixBridgeToOptimismL1.connect(user1L1);
+										await SynthetixBridgeToOptimismL1.depositAndMigrateEscrow(
+											depositAmount,
+											userEntries
+										);
+									});
+
+									it('should update the L1 escrow state', async () => {
+										assert.bnEqual(
+											await RewardEscrowV2L1.numVestingEntries(USER1_ADDRESS),
+											escrowNum.toString()
+										);
+										assert.bnEqual(
+											await RewardEscrowV2L1.totalEscrowedAccountBalance(USER1_ADDRESS),
+											'0'
+										);
+										assert.bnEqual(
+											await RewardEscrowV2L1.totalVestedAccountBalance(USER1_ADDRESS),
+											'0'
+										);
+									});
+
+									describe('when a small period of time has elapsed', () => {
+										before('wait', async () => {
+											await wait(10);
+										});
+
+										it('should update the L2 escrow state', async () => {
+											RewardEscrowV2L2 = RewardEscrowV2L2.connect(user1L2);
+											assert.bnEqual(
+												await RewardEscrowV2L2.numVestingEntries(USER1_ADDRESS),
+												escrowNum.toString()
+											);
+											assert.bnEqual(
+												await RewardEscrowV2L2.totalEscrowedAccountBalance(USER1_ADDRESS),
+												totalEscrowed
+											);
+											assert.bnEqual(
+												await RewardEscrowV2L2.totalVestedAccountBalance(USER1_ADDRESS),
+												'0'
+											);
+										});
+									});
+								});
+							});
+						});
 					});
 				});
 			});
