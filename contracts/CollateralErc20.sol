c2aa8f4c0e90a19458ce48a412c37e624203c2f3
Alejandro <palebluedot@gmail.com>
2021-01-25 17:22:43 -0300

    Bugfix on constructor params

--- contracts/CollateralErc20.sol
+++ contracts/CollateralErc20.sol
@@ -28,7 +28,7 @@ contract CollateralErc20 is ICollateralErc20, Collateral {
         uint _minCollateral,
         address _underlyingContract,
         uint _underlyingDecimals
-    ) public Collateral(_state, _owner, _manager, _resolver, _collateralKey, _minCratio, _minCollateral) {
+    ) public Collateral(_owner, _state, _manager, _resolver, _collateralKey, _minCratio, _minCollateral) {
         underlyingContract = _underlyingContract;
 
         underlyingContractDecimals = _underlyingDecimals;
