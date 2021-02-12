b9df7a1b66c8d0e7e0b5d62d79ce83e35204556b
i-stam <i.stamelakos@gmail.com>
2021-02-12 15:18:55 +0000

    Add missing functionality to RealtimeDebtCache

--- contracts/RealtimeDebtCache.sol
+++ contracts/RealtimeDebtCache.sol
@@ -65,11 +65,7 @@ contract RealtimeDebtCache is DebtCache {
 
     function takeDebtSnapshot() external {}
 
-    function updateCachedSynthDebts(bytes32[] calldata currencyKeys) external {}
-
     function updateCachedSynthDebtWithRate(bytes32 currencyKey, uint currencyRate) external {}
-
-    function updateCachedSynthDebtsWithRates(bytes32[] calldata currencyKeys, uint[] calldata currencyRates) external {}
 
     function updateDebtCacheValidity(bool currentlyInvalid) external {}
 }
