c199f68f07b9655b9d401754f4464183335dcb5c
i-stam <i.stamelakos@gmail.com>
2021-02-20 21:45:14 +0000

    Add synthExchange.test.js

--- test/optimism/synthExchange.test.js
+++ test/optimism/synthExchange.test.js
@@ -2,10 +2,10 @@ const ethers = require('ethers');
 const { assert } = require('../contracts/common');
 const { connectContract } = require('./utils/connectContract');
 const { toBytes32 } = require('../..');
-const { assertRevertOptimism } = require('./utils/revertOptimism');
+// const { assertRevertOptimism } = require('./utils/revertOptimism');
 
 const itCanPerformSynthExchange = ({ ctx }) => {
-	describe.only('[SYNTEXCHANGE] when exchanging synths on L2', () => {
+	describe('[SYNTEXCHANGE] when exchanging synths on L2', () => {
 		const amountToDeposit = ethers.utils.parseEther('100');
 
 		let user1L1, user1L2;
