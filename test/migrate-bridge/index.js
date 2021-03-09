3a1c2567902ed33b3a194896b3834bc0ebf9f7fc
Alejandro <palebluedot@gmail.com>
2021-03-09 10:40:14 -0300

    Progress with bridge migration tests

--- test/migrate-bridge/index.js
+++ test/migrate-bridge/index.js
@@ -1,66 +1,108 @@
 const ethers = require('ethers');
 const axios = require('axios');
+const chalk = require('chalk');
 const { toBytes32 } = require('../..');
 const { Watcher } = require('@eth-optimism/watcher');
 const { assert } = require('../contracts/common');
 const { connectContract } = require('../optimism/utils/connectContract');
 
+/*
+ * Tests a migration of bridges with ongoing withdrawals and deposits
+ * while the migration occurs. SNX is only migrated from the original L1 bridge
+ * to the new L1 bridge once all withdrawals that target it are finalized.
+ *
+ * How to run:
+ * 1. Set optimism-integration FRAUD_PROOF_WINDOW_SECONDS to 300 in docker-compose.env.yml
+ * 2. Start optimism-integration `./up.sh`
+ * 3. Deploy an ovm pair `node publish deploy-ovm-pair`
+ * 4. Run the tests: `npm run test:migrate-bridge`
+ *
+ * Notes:
+ * Not intended to be run on CI.
+ * This test is based on test:prod:ovm.
+ * */
 describe('Layer 2 bridge migration tests', () => {
 	let SynthetixL1, SynthetixL2;
+	let SynthetixBridgeToOptimismL1, SynthetixBridgeToBaseL2;
+
+	let providerL1, providerL2;
+
+	let ownerAddress, ownerL1, ownerL2;
+
+	let watcher;
 
 	// --------------------------
 	// Setup
 	// --------------------------
 
 	before('set up providers', () => {
-		this.providerL1 = new ethers.providers.JsonRpcProvider('http://localhost:9545');
-		this.providerL2 = new ethers.providers.JsonRpcProvider('http://localhost:8545');
+		providerL1 = new ethers.providers.JsonRpcProvider('http://localhost:9545');
+		providerL2 = new ethers.providers.JsonRpcProvider('http://localhost:8545');
 	});
 
 	before('set up signers', () => {
-		// See publish/src/commands/deploy-ovm-pair.js
-		this.ownerAddress = '0x640e7cc27b750144ED08bA09515F3416A988B6a3';
-		this.ownerPrivateKey = '0xea8b000efb33c49d819e8d6452f681eed55cdf7de47d655887fc0e318906f2e7';
-		this.user1Address = '0x5eEaBfDD0F31CeBf32f8Abf22DA451fE46eAc131';
-		this.user1PrivateKey = '0x5b1c2653250e5c580dcb4e51c2944455e144c57ebd6a0645bd359d2e69ca0f0c';
+		ownerAddress = '0x640e7cc27b750144ED08bA09515F3416A988B6a3';
+		const ownerPrivateKey = '0xea8b000efb33c49d819e8d6452f681eed55cdf7de47d655887fc0e318906f2e7';
 
-		// These are set up in L1 but not in L2,
-		// that's why we get signers in L1, and create signers in L2.
-		// Also note that L2 doesn't use Ether, so we can just create signers
-		// and it doesn't matter if they don't have Ether.
-		this.ownerL1 = this.providerL1.getSigner(this.ownerAddress);
-		this.ownerL2 = new ethers.Wallet(this.ownerPrivateKey, this.providerL2);
+		ownerL1 = providerL1.getSigner(ownerAddress);
+		ownerL2 = new ethers.Wallet(ownerPrivateKey, providerL2);
 	});
 
 	before('set up watchers', async () => {
 		const response = await axios.get('http://localhost:8080/addresses.json');
 		const addresses = response.data;
 
-		this.watcher = new Watcher({
+		watcher = new Watcher({
 			l1: {
-				provider: this.providerL1,
+				provider: providerL1,
 				messengerAddress: addresses['Proxy__OVM_L1CrossDomainMessenger'],
 			},
 			l2: {
-				provider: this.providerL2,
+				provider: providerL2,
 				messengerAddress: '0x4200000000000000000000000000000000000007',
 			},
 		});
 	});
 
-	after('exit', async () => {
-		// TODO: Optimism watchers leave the process open, so we explicitely kill it
-		process.exit(0);
+	before('send dummy txs', async () => {
+		// For the optimism-integration watchers to work properly when FRAUD_PROOF_WINDOW_SECONDS != 0
+		setInterval(async () => {
+			await ownerL2.sendTransaction({
+				to: ownerAddress,
+				value: 0,
+				gasPrice: 0,
+			});
+		}, 5 * 1000);
+	});
+
+	before('show elapsed time', async () => {
+		let elapsed = 0;
+		const tick = 60;
+
+		setInterval(() => {
+			elapsed += tick;
+
+			console.log(chalk.gray(`${elapsed} seconds`));
+		}, tick * 1000);
 	});
 
 	describe('when instances have been deployed in local L1 and L2 chains', () => {
 		before('connect to contracts', async () => {
-			SynthetixL1 = connectContract({ contract: 'Synthetix', provider: this.providerL1 });
+			SynthetixL1 = connectContract({ contract: 'Synthetix', provider: providerL1 });
 			SynthetixL2 = connectContract({
 				contract: 'Synthetix',
 				source: 'MintableSynthetix',
 				useOvm: true,
-				provider: this.providerL2,
+				provider: providerL2,
+			});
+			SynthetixBridgeToOptimismL1 = connectContract({
+				contract: 'SynthetixBridgeToOptimism',
+				provider: providerL1,
+			});
+			SynthetixBridgeToBaseL2 = connectContract({
+				contract: 'SynthetixBridgeToBase',
+				useOvm: true,
+				provider: providerL2,
 			});
 		});
 
@@ -107,11 +149,11 @@ describe('Layer 2 bridge migration tests', () => {
 			}
 
 			await simulateExchangeRates({
-				provider: this.providerL1,
-				owner: this.ownerL1,
+				provider: providerL1,
+				owner: ownerL1,
 				useOvm: false,
 			});
-			await simulateExchangeRates({ provider: this.providerL2, owner: this.ownerL2, useOvm: true });
+			await simulateExchangeRates({ provider: providerL2, owner: ownerL2, useOvm: true });
 		});
 
 		before('tweak system settings for tests', async () => {
@@ -126,28 +168,141 @@ describe('Layer 2 bridge migration tests', () => {
 				await SystemSettings.setMinimumStakeTime(1);
 			}
 
-			await tweakSettings({ provider: this.providerL1, owner: this.ownerL1, useOvm: false });
-			await tweakSettings({ provider: this.providerL2, owner: this.ownerL2, useOvm: true });
+			await tweakSettings({ provider: providerL1, owner: ownerL1, useOvm: false });
+			await tweakSettings({ provider: providerL2, owner: ownerL2, useOvm: true });
 		});
 
-		// --------------------------
-		// General properties
-		// --------------------------
+		// --------------------------------------------------------
+		// Initiate and finalize a deposit on L1 (original bridge)
+		// --------------------------------------------------------
 
-		describe('[GENERAL] properties', () => {
-			it('shows the expected owners', async () => {
-				assert.equal(await SynthetixL1.owner(), this.ownerAddress);
-				assert.equal(await SynthetixL2.owner(), this.ownerAddress);
-			});
+		const amountToDeposit = ethers.utils.parseEther('200');
 
-			it('shows the instances have the expected total supplies', async () => {
-				assert.bnGte(await SynthetixL1.totalSupply(), ethers.utils.parseEther('0'));
-				assert.bnGte(await SynthetixL2.totalSupply(), ethers.utils.parseEther('0'));
-			});
+		let initialOwnerBalanceL1, initialOwnerBalanceL2;
+
+		before('record initial balances', async () => {
+			initialOwnerBalanceL1 = await SynthetixL1.balanceOf(ownerAddress);
+			initialOwnerBalanceL2 = await SynthetixL2.balanceOf(ownerAddress);
 		});
 
-		// --------------------------
-		// Bridge migration
-		// --------------------------
+		before('infinite approval for L1 bridge', async () => {
+			SynthetixL1 = SynthetixL1.connect(ownerL1);
+
+			const tx = await SynthetixL1.approve(
+				SynthetixBridgeToOptimismL1.address,
+				ethers.utils.parseEther('100000000')
+			);
+			await tx.wait();
+		});
+
+		describe('when a deposit is initialized and finalized with the original bridges', () => {
+			let depositReceipt;
+
+			before('deposit', async () => {
+				SynthetixBridgeToOptimismL1 = SynthetixBridgeToOptimismL1.connect(ownerL1);
+
+				const tx = await SynthetixBridgeToOptimismL1.deposit(amountToDeposit);
+				depositReceipt = await tx.wait();
+			});
+
+			before('listen for completion', async () => {
+				const [transactionHashL2] = await watcher.getMessageHashesFromL1Tx(
+					depositReceipt.transactionHash
+				);
+
+				await watcher.getL2TransactionReceipt(transactionHashL2);
+			});
+
+			it('shows that the owners L1 balance decreased', async () => {
+				assert.bnEqual(
+					await SynthetixL1.balanceOf(ownerAddress),
+					initialOwnerBalanceL1.sub(amountToDeposit)
+				);
+			});
+
+			it('shows that the owners L2 balance increased', async () => {
+				assert.bnEqual(
+					await SynthetixL2.balanceOf(ownerAddress),
+					initialOwnerBalanceL2.add(amountToDeposit)
+				);
+			});
+
+			// ----------------------------------------------
+			// Initiate a withdrawal on L2 (original bridge)
+			// ----------------------------------------------
+
+			const amountToWithdraw = ethers.utils.parseEther('100');
+
+			describe('when a withdrawal is initiated with the original bridges', () => {
+				let withdrawalReceipt;
+
+				before('initiate withdrawal', async () => {
+					SynthetixBridgeToBaseL2 = SynthetixBridgeToBaseL2.connect(ownerL2);
+
+					const tx = await SynthetixBridgeToBaseL2.withdraw(amountToWithdraw);
+					withdrawalReceipt = await tx.wait();
+				});
+
+				it('shows that the owners L2 balance decreased', async () => {
+					assert.bnEqual(
+						await SynthetixL2.balanceOf(ownerAddress),
+						initialOwnerBalanceL2.add(amountToDeposit).sub(amountToWithdraw)
+					);
+				});
+
+				// -------------------------------
+				// Deploy and connect new bridges
+				// -------------------------------
+
+				describe('when new bridges are deployed', () => {
+					// TODO
+
+					// -------------------------------
+					// Make a new withdrawal
+					// -------------------------------
+
+					describe('when a withdrawal is initiated with the new bridges', () => {
+						// TODO
+
+						// -------------------------------
+						// Original withdrawal finalizes
+						// -------------------------------
+
+						describe('when the original withdrawal is finalized', () => {
+							before('listen for completion', async () => {
+								const [transactionHashL1] = await watcher.getMessageHashesFromL2Tx(
+									withdrawalReceipt.transactionHash
+								);
+
+								await watcher.getL1TransactionReceipt(transactionHashL1);
+							});
+
+							it('shows that the owners L1 balance increased', async () => {
+								assert.bnEqual(
+									await SynthetixL1.balanceOf(ownerAddress),
+									initialOwnerBalanceL1.sub(amountToDeposit).add(amountToWithdraw)
+								);
+							});
+
+							// ------------------------------
+							// Migrate SNX to the new bridge
+							// ------------------------------
+
+							describe('when the new withdrawal is finalized', () => {
+								// TODO
+
+								// -------------------------
+								// New withdrawal finalizes
+								// -------------------------
+
+								describe('when the new withdrawal is finalized', () => {
+									// TODO
+								});
+							});
+						});
+					});
+				});
+			});
+		});
 	});
 });
