da75a527d10cca5d3666225f917f88254becf51d
Brett Sun <qisheng.brett.sun@gmail.com>
2021-03-31 16:57:32 +0100

    SIP-X: update ExchangerWithVirtualSynth test behaviour to accomodate base VirtualSynth

--- test/contracts/ExchangerWithVirtualSynth.behaviors.js
+++ test/contracts/ExchangerWithVirtualSynth.behaviors.js
@@ -28,6 +28,9 @@ module.exports = function({ accounts }) {
 			],
 			accounts: accounts.slice(10), // mock using accounts after the first few
 		}));
+
+		const VirtualSynth = artifacts.require('VirtualSynth');
+		this.baseVirtualSynth = await VirtualSynth.new();
 	});
 
 	before(async () => {
@@ -38,7 +41,11 @@ module.exports = function({ accounts }) {
 		whenInstantiated: ({ owner }, cb) => {
 			describe(`when instantiated`, () => {
 				beforeEach(async () => {
-					this.instance = await ExchangerWithVirtualSynth.new(owner, this.resolver.address);
+					this.instance = await ExchangerWithVirtualSynth.new(
+						owner,
+						this.resolver.address,
+						this.baseVirtualSynth.address
+					);
 					await this.instance.rebuildCache();
 				});
 				cb();
