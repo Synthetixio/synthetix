b0d3d7ae4db32fd90969ebd03d12433943a4ccb2
i-stam <i.stamelakos@gmail.com>
2021-04-08 17:14:02 +0100

    Deployment of kovan-ovm test instance with extra synths for gas measurements

--- publish/src/commands/deploy.js
+++ publish/src/commands/deploy.js
@@ -767,7 +767,7 @@ const deploy = async ({
 
 	const debtCache = await deployer.deployContract({
 		name: 'DebtCache',
-		source: useOvm ? 'RealtimeDebtCache' : 'DebtCache',
+		// source: useOvm ? 'RealtimeDebtCache' : 'DebtCache',
 		deps: ['AddressResolver'],
 		args: [account, addressOf(readProxyForResolver)],
 	});
@@ -2352,7 +2352,7 @@ const deploy = async ({
 		if (force || validityChanged) {
 			console.log(yellow(`Refreshing debt snapshot...`));
 			await runStep({
-				gasLimit: useOvm ? 3.5e6 : 2.5e6, // About 1.7 million gas is required to refresh the snapshot with ~40 synths on L1
+				gasLimit: useOvm ? 8.5e6 : 2.5e6, // About 1.7 million gas is required to refresh the snapshot with ~40 synths on L1
 				contract: 'DebtCache',
 				target: debtCache,
 				write: 'takeDebtSnapshot',
