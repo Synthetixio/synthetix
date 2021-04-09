810cf7c01d4483c9a8b72ac8c34784c912624659
David <dbschwartz17@gmail.com>
2021-04-09 13:45:16 -0700

    add accrued interest utility contract and add a test for it

---
+++ contracts/AccruedInterestUtil.sol
@@ -0,0 +1,62 @@
+pragma solidity ^0.5.16;
+
+pragma experimental ABIEncoderV2;
+
+import "./SafeDecimalMath.sol";
+import "./CollateralState.sol";
+import "./interfaces/ICollateralManager.sol";
+import "./interfaces/ICollateralLoan.sol";
+
+interface ICollateralState {
+    function getLoan(address account, uint256 loanID) external view returns (ICollateralLoan.Loan memory loan);
+}
+
+interface ICollateral {
+    function synthsByKey(bytes32) external view returns (bytes32);
+
+    function manager() external view returns (address);
+
+    function state() external view returns (ICollateralState);
+}
+
+contract AccruedInterestUtil is ICollateralLoan {
+    /* ========== LIBRARIES ========== */
+    using SafeMath for uint;
+    using SafeDecimalMath for uint;
+
+    // define and emit events
+    function getAccruedInterest(
+        address userAddress,
+        uint256 loanId,
+        address collateralContract
+    ) external view returns (uint) {
+        ICollateral collateral = ICollateral(collateralContract);
+        ICollateralManager _manager = ICollateralManager(collateral.manager());
+        ICollateralState state = collateral.state();
+        ICollateralLoan.Loan memory loan = state.getLoan(userAddress, loanId);
+
+        // 1. Get the rates we need.
+        (uint entryRate, uint lastRate, uint lastUpdated, ) =
+            loan.short
+                ? _manager.getShortRatesAndTime(loan.currency, loan.interestIndex)
+                : _manager.getRatesAndTime(loan.interestIndex);
+
+        // 2. Get the instantaneous rate.
+        (uint rate, bool invalid) =
+            loan.short ? _manager.getShortRate(collateral.synthsByKey(loan.currency)) : _manager.getBorrowRate();
+
+        require(!invalid, "Rates are invalid");
+
+        // 3. Get the time since we last updated the rate.
+        uint timeDelta = block.timestamp.sub(lastUpdated).mul(SafeDecimalMath.unit());
+
+        // 4. Get the latest cumulative rate. F_n+1 = F_n + F_last
+        uint latestCumulative = lastRate.add(rate.multiplyDecimal(timeDelta));
+
+        // 5. If the loan was just opened, don't record any interest. Otherwise multiple by the amount outstanding.
+        uint interest = loan.interestIndex == 0 ? 0 : loan.amount.multiplyDecimal(latestCumulative.sub(entryRate));
+
+        // 6. return the accrued interest from last time user updated the loan plus accrued interest since then
+        loan.accruedInterest.add(interest);
+    }
+}
---
+++ test/contracts/AccruedInterestUtil.js
@@ -0,0 +1,243 @@
+'use strict';
+
+const { artifacts, contract } = require('hardhat');
+
+const { assert, addSnapshotBeforeRestoreAfterEach } = require('./common');
+
+const { fastForward, toUnit, fromUnit, currentTime } = require('../utils')();
+
+const { setupAllContracts, setupContract } = require('./setup');
+
+const {
+	toBytes32,
+	constants: { ZERO_ADDRESS },
+} = require('../..');
+
+let CollateralManager;
+let CollateralState;
+let CollateralManagerState;
+
+contract('AccruedInterestUtil', async accounts => {
+	const YEAR = 31556926;
+
+	const sUSD = toBytes32('sUSD');
+	const sETH = toBytes32('sETH');
+	const sBTC = toBytes32('sBTC');
+
+	const [deployerAccount, owner, oracle, , account1] = accounts;
+
+	let short,
+		state,
+		managerState,
+		feePool,
+		exchangeRates,
+		addressResolver,
+		sUSDSynth,
+		sBTCSynth,
+		sETHSynth,
+		iBTCSynth,
+		iETHSynth,
+		synths,
+		manager,
+		issuer,
+		debtCache,
+		accruedInterestUtil;
+
+	let tx, loan, id;
+
+	const getid = tx => {
+		const event = tx.logs.find(log => log.event === 'LoanCreated');
+		return event.args.id;
+	};
+
+	const fastForwardAndUpdateRates = async seconds => {
+		await fastForward(seconds);
+		await updateRatesWithDefaults();
+	};
+
+	const issue = async (synth, issueAmount, receiver) => {
+		await synth.issue(receiver, issueAmount, { from: owner });
+	};
+
+	const updateRatesWithDefaults = async () => {
+		const timestamp = await currentTime();
+
+		await exchangeRates.updateRates([sETH], ['100'].map(toUnit), timestamp, {
+			from: oracle,
+		});
+
+		const sBTC = toBytes32('sBTC');
+
+		await exchangeRates.updateRates([sBTC], ['10000'].map(toUnit), timestamp, {
+			from: oracle,
+		});
+	};
+
+	const deployShort = async ({ state, owner, manager, resolver, collatKey, minColat, minSize }) => {
+		return setupContract({
+			accounts,
+			contract: 'CollateralShort',
+			args: [state, owner, manager, resolver, collatKey, minColat, minSize],
+		});
+	};
+
+	const setupShort = async () => {
+		synths = ['sUSD', 'sBTC', 'sETH', 'iBTC', 'iETH'];
+		({
+			ExchangeRates: exchangeRates,
+			SynthsUSD: sUSDSynth,
+			SynthsBTC: sBTCSynth,
+			SynthsETH: sETHSynth,
+			SynthiBTC: iBTCSynth,
+			SynthiETH: iETHSynth,
+			FeePool: feePool,
+			AddressResolver: addressResolver,
+			Issuer: issuer,
+			DebtCache: debtCache,
+			AccruedInterestUtil: accruedInterestUtil,
+		} = await setupAllContracts({
+			accounts,
+			synths,
+			contracts: [
+				'Synthetix',
+				'FeePool',
+				'AddressResolver',
+				'Exchanger',
+				'ExchangeRates',
+				'SystemStatus',
+				'Issuer',
+				'DebtCache',
+				'AccruedInterestUtil',
+			],
+		}));
+
+		managerState = await CollateralManagerState.new(owner, ZERO_ADDRESS, { from: deployerAccount });
+
+		const maxDebt = toUnit(10000000);
+
+		manager = await CollateralManager.new(
+			managerState.address,
+			owner,
+			addressResolver.address,
+			maxDebt,
+			// 5% / 31536000 (seconds in common year)
+			1585489599,
+			0,
+			{
+				from: deployerAccount,
+			}
+		);
+
+		await managerState.setAssociatedContract(manager.address, { from: owner });
+
+		state = await CollateralState.new(owner, ZERO_ADDRESS, { from: deployerAccount });
+
+		short = await deployShort({
+			state: state.address,
+			owner: owner,
+			manager: manager.address,
+			resolver: addressResolver.address,
+			collatKey: sUSD,
+			minColat: toUnit(1.2),
+			minSize: toUnit(0.1),
+		});
+
+		await state.setAssociatedContract(short.address, { from: owner });
+
+		await addressResolver.importAddresses(
+			[toBytes32('CollateralShort'), toBytes32('CollateralManager')],
+			[short.address, manager.address],
+			{
+				from: owner,
+			}
+		);
+
+		await feePool.rebuildCache();
+		await manager.rebuildCache();
+		await issuer.rebuildCache();
+		await debtCache.rebuildCache();
+
+		await manager.addCollaterals([short.address], { from: owner });
+
+		await short.addSynths(
+			['SynthsBTC', 'SynthsETH'].map(toBytes32),
+			['sBTC', 'sETH'].map(toBytes32),
+			{ from: owner }
+		);
+
+		await manager.addShortableSynths(
+			[
+				[toBytes32('SynthsBTC'), toBytes32('SynthiBTC')],
+				[toBytes32('SynthsETH'), toBytes32('SynthiETH')],
+			],
+			['sBTC', 'sETH'].map(toBytes32),
+			{
+				from: owner,
+			}
+		);
+
+		await sUSDSynth.approve(short.address, toUnit(100000), { from: account1 });
+	};
+
+	before(async () => {
+		CollateralManager = artifacts.require(`CollateralManager`);
+		CollateralState = artifacts.require(`CollateralState`);
+		CollateralManagerState = artifacts.require('CollateralManagerState');
+
+		await setupShort();
+	});
+
+	addSnapshotBeforeRestoreAfterEach();
+
+	beforeEach(async () => {
+		await updateRatesWithDefaults();
+
+		await issue(sUSDSynth, toUnit(100000), owner);
+		await issue(sBTCSynth, toUnit(1), owner);
+		await issue(sETHSynth, toUnit(1), owner);
+		await issue(iBTCSynth, toUnit(1), owner);
+		await issue(iETHSynth, toUnit(1), owner);
+
+		// The market is balanced between long and short.
+
+		await debtCache.takeDebtSnapshot();
+	});
+
+	describe('Read the accrued interest rate', async () => {
+		it('should correctly determine the interest on a short', async () => {
+			const oneBTC = toUnit(1);
+			const susdCollateral = toUnit(15000);
+
+			await issue(sUSDSynth, susdCollateral, account1);
+
+			tx = await short.open(susdCollateral, oneBTC, sBTC, { from: account1 });
+			id = getid(tx);
+
+			// after a year we should have accrued 33%.
+
+			await fastForwardAndUpdateRates(YEAR);
+
+			// deposit some collateral to trigger the interest accrual.
+
+			tx = await short.deposit(account1, id, toUnit(1), { from: account1 });
+
+			loan = await state.getLoan(account1, id);
+
+			let interest = Math.round(parseFloat(fromUnit(loan.accruedInterest)) * 10000) / 10000;
+
+			assert.equal(interest, 0.3333);
+
+			await fastForwardAndUpdateRates(YEAR);
+
+			loan = await state.getLoan(account1, id);
+
+			interest = Math.round(parseFloat(fromUnit(loan.accruedInterest)) * 10000) / 10000;
+
+			// TODO make sure the result is correct and fix the test error.
+			assert.equal(
+				await accruedInterestUtil.getAccruedInterest(account1, loan.id, short.address),
+				0.6667
+			);
+		});
+	});
+});
