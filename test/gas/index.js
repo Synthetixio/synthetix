6d572e9b358e266b4240ae56b84b79e26df3d4c1
Alejandro <palebluedot@gmail.com>
2021-04-19 11:31:21 -0300

    Adding synths

--- test/gas/index.js
+++ test/gas/index.js
@@ -5,9 +5,25 @@ const ethers = require('ethers');
 
 let provider;
 let signer;
+let data;
+
+const DATA_FILE = 'test/gas/measurements.json';
+const MAX_SYNTHS = 10;
+const NUM_MEASUREMENTS = 3;
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 describe('Gas measurements', () => {
-	let Synthetix, Issuer, ExchangeRates, DebtCache;
+	let Synthetix, Issuer, ExchangeRates, DebtCache, AddressResolver;
+
+	before('prepare data file', () => {
+		if (fs.existsSync(DATA_FILE)) {
+			fs.unlinkSync(DATA_FILE);
+		}
+
+		data = {
+			minting: [],
+		};
+	});
 
 	before('set up provider & signers', async () => {
 		provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
@@ -22,6 +38,7 @@ describe('Gas measurements', () => {
 		Issuer = _getContract({ name: 'Issuer' });
 		ExchangeRates = _getContract({ name: 'ExchangeRates' });
 		DebtCache = _getContract({ name: 'DebtCache' });
+		AddressResolver = _getContract({ name: 'AddressResolver' });
 	});
 
 	before('remove all synths', async () => {
@@ -31,6 +48,13 @@ describe('Gas measurements', () => {
 		);
 
 		if (synthsToRemove.length > 0) {
+			console.log(
+				chalk.red(
+					`Removing synths: ${synthsToRemove.map(currencyKey =>
+						ethers.utils.parseBytes32String(currencyKey)
+					)}`
+				)
+			);
 			const tx = await Issuer.removeSynths(synthsToRemove);
 			await tx.wait();
 		}
@@ -48,24 +72,97 @@ describe('Gas measurements', () => {
 		await tx.wait();
 	});
 
-	it('test', async () => {
-		const numSynths = await Issuer.availableSynthCount();
-		const currencyKeys = await Issuer.availableCurrencyKeys();
-		const currencies = currencyKeys.map(currencyKey =>
-			ethers.utils.parseBytes32String(currencyKey)
-		);
-		console.log(`Available synths (${numSynths}): ${currencies}`);
-
-		const balance = await Synthetix.balanceOf(signer.address);
-		console.log(balance.toString());
-
-		const tx = await Synthetix.issueSynths(ethers.utils.parseEther('100'));
-		console.log(tx);
-
-		const receipt = await tx.wait();
-		console.log(receipt);
+	after('print data', () => {
+		console.log(JSON.stringify(data, null, 2));
 	});
+
+	for (let numSynths = 1; numSynths <= MAX_SYNTHS; numSynths++) {
+		describe(`When the system has ${numSynths} synths`, () => {
+			before('add synths', async () => {
+				const activeSynths = await Issuer.availableSynthCount();
+				console.log(chalk.blue(`Taking measurements with ${numSynths} synths...`));
+				console.log(chalk.gray(`System synths: ${activeSynths}`));
+
+				if (activeSynths < numSynths) {
+					console.log(chalk.magenta('Adding a synth...'));
+
+					const currency = `s${numSynths - 1}`;
+					const currencyKey = ethers.utils.formatBytes32String(`s${numSynths}`);
+
+					const TokenState = _getContractFactory({ name: 'TokenStatesUSD' });
+					const tokenState = await TokenState.deploy(signer.address, ZERO_ADDRESS);
+
+					const Proxy = _getContractFactory({ name: 'ProxysUSD' });
+					const proxy = await Proxy.deploy(signer.address);
+
+					const Synth = _getContractFactory({ name: 'SynthsUSD' });
+					const synth = await Synth.deploy(
+						proxy.address,
+						tokenState.address,
+						`Mock Synth`,
+						currency,
+						signer.address,
+						currencyKey,
+						0,
+						AddressResolver.address
+					);
+
+					let tx = await Issuer.addSynth(synth.address);
+					await tx.wait();
+
+					const { timestamp } = await provider.getBlock();
+					tx = await ExchangeRates.updateRates(
+						[currencyKey],
+						[ethers.utils.parseEther('1')],
+						timestamp
+					);
+					await tx.wait();
+
+					tx = await DebtCache.takeDebtSnapshot();
+					await tx.wait();
+
+					console.log(chalk.gray(`Updated system synths: ${await Issuer.availableSynthCount()}`));
+				}
+			});
+
+			it('minting', async () => {
+				const target = `minting.${numSynths}`;
+				if (!data[target]) {
+					data[target] = {
+						measurements: [],
+						avg: 0,
+					};
+				}
+
+				let totalGas = 0;
+
+				for (let i = 0; i < NUM_MEASUREMENTS; i++) {
+					const tx = await Synthetix.issueSynths(ethers.utils.parseEther('100'));
+					const receipt = await tx.wait();
+
+					const gas = receipt.cumulativeGasUsed.toNumber();
+					totalGas += gas;
+
+					data[target].measurements.push(gas);
+				}
+
+				data[target].avg = totalGas / NUM_MEASUREMENTS;
+			});
+		});
+	}
 });
+
+function _getContractFactory({ name }) {
+	const deployment = _getDeploymentFile({ filename: 'deployment.json' });
+
+	const target = deployment.targets[name];
+	const source = target.source;
+
+	const bytecode = deployment.sources[source].bytecode;
+	const abi = deployment.sources[source].abi;
+
+	return new ethers.ContractFactory(abi, bytecode, signer);
+}
 
 function _getContract({ name }) {
 	const deployment = _getDeploymentFile({ filename: 'deployment.json' });
