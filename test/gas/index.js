51cef6bf3ab977f5555ff7a26c67884c8990772f
Alejandro <palebluedot@gmail.com>
2021-04-19 14:54:49 -0300

    First gas measurements

--- test/gas/index.js
+++ test/gas/index.js
@@ -5,7 +5,6 @@ const ethers = require('ethers');
 
 let provider;
 let signer;
-let data;
 
 const DATA_FILE = 'test/gas/measurements.json';
 const MAX_SYNTHS = 10;
@@ -13,16 +12,13 @@ const NUM_MEASUREMENTS = 3;
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 describe('Gas measurements', () => {
-	let Synthetix, Issuer, ExchangeRates, DebtCache, AddressResolver;
-
+	let data;
 	before('prepare data file', () => {
 		if (fs.existsSync(DATA_FILE)) {
 			fs.unlinkSync(DATA_FILE);
 		}
 
-		data = {
-			minting: [],
-		};
+		data = {};
 	});
 
 	before('set up provider & signers', async () => {
@@ -33,12 +29,25 @@ describe('Gas measurements', () => {
 		console.log(chalk.blue(`Signer: ${signer.address}`));
 	});
 
+	let Synthetix, Issuer, ExchangeRates, DebtCache, ReadProxyAddressResolver, SystemSettings;
 	before('connect to contracts', async () => {
 		Synthetix = _getContract({ name: 'Synthetix' });
 		Issuer = _getContract({ name: 'Issuer' });
 		ExchangeRates = _getContract({ name: 'ExchangeRates' });
 		DebtCache = _getContract({ name: 'DebtCache' });
-		AddressResolver = _getContract({ name: 'AddressResolver' });
+		ReadProxyAddressResolver = _getContract({ name: 'ReadProxyAddressResolver' });
+		SystemSettings = _getContract({ name: 'SystemSettings' });
+	});
+
+	before('tweak system settings', async () => {
+		let tx = await SystemSettings.setMinimumStakeTime(0);
+		await tx.wait();
+
+		tx = await SystemSettings.setWaitingPeriodSecs(0);
+		await tx.wait();
+
+		tx = await SystemSettings.setRateStalePeriod(100000000000);
+		await tx.wait();
 	});
 
 	before('remove all synths', async () => {
@@ -49,7 +58,7 @@ describe('Gas measurements', () => {
 
 		if (synthsToRemove.length > 0) {
 			console.log(
-				chalk.red(
+				chalk.magenta(
 					`Removing synths: ${synthsToRemove.map(currencyKey =>
 						ethers.utils.parseBytes32String(currencyKey)
 					)}`
@@ -72,47 +81,37 @@ describe('Gas measurements', () => {
 		await tx.wait();
 	});
 
-	after('print data', () => {
-		console.log(JSON.stringify(data, null, 2));
-	});
-
 	for (let numSynths = 1; numSynths <= MAX_SYNTHS; numSynths++) {
 		describe(`When the system has ${numSynths} synths`, () => {
+			let target;
+			before('prepare data', () => {
+				target = data[`${numSynths}_synths`] = {
+					minting: { measurements: [], avg: 0 },
+					burning: { measurements: [], avg: 0 },
+					exchanging: { measurements: [], avg: 0 },
+				};
+			});
+
 			before('add synths', async () => {
 				const activeSynths = await Issuer.availableSynthCount();
 				console.log(chalk.blue(`Taking measurements with ${numSynths} synths...`));
 				console.log(chalk.gray(`System synths: ${activeSynths}`));
 
 				if (activeSynths < numSynths) {
-					console.log(chalk.magenta('Adding a synth...'));
-
 					const currency = `s${numSynths - 1}`;
-					const currencyKey = ethers.utils.formatBytes32String(`s${numSynths}`);
+					console.log(chalk.magenta(`Adding a synth: ${currency}`));
 
-					const TokenState = _getContractFactory({ name: 'TokenStatesUSD' });
-					const tokenState = await TokenState.deploy(signer.address, ZERO_ADDRESS);
-
-					const Proxy = _getContractFactory({ name: 'ProxysUSD' });
-					const proxy = await Proxy.deploy(signer.address);
-
-					const Synth = _getContractFactory({ name: 'SynthsUSD' });
-					const synth = await Synth.deploy(
-						proxy.address,
-						tokenState.address,
-						`Mock Synth`,
-						currency,
-						signer.address,
-						currencyKey,
-						0,
-						AddressResolver.address
-					);
+					const synth = await _deploySynth({
+						currency: `s${numSynths - 1}`,
+						resolverAddress: ReadProxyAddressResolver.address,
+					});
 
 					let tx = await Issuer.addSynth(synth.address);
 					await tx.wait();
 
 					const { timestamp } = await provider.getBlock();
 					tx = await ExchangeRates.updateRates(
-						[currencyKey],
+						[ethers.utils.formatBytes32String(currency)],
 						[ethers.utils.parseEther('1')],
 						timestamp
 					);
@@ -125,32 +124,100 @@ describe('Gas measurements', () => {
 				}
 			});
 
-			it('minting', async () => {
-				const target = `minting.${numSynths}`;
-				if (!data[target]) {
-					data[target] = {
-						measurements: [],
-						avg: 0,
-					};
-				}
+			after('print data', () => {
+				Object.keys(data).map(numSynthsKey => {
+					const entry = data[numSynthsKey];
+					console.log(numSynthsKey);
 
-				let totalGas = 0;
+					Object.keys(entry).map(measurementKey => {
+						const measurement = entry[measurementKey];
+						console.log('  ', measurementKey, measurement.avg);
+					});
+				});
+			});
 
-				for (let i = 0; i < NUM_MEASUREMENTS; i++) {
-					const tx = await Synthetix.issueSynths(ethers.utils.parseEther('100'));
+			it('take measurements', async () => {
+				async function measureGas({ tx, target }) {
 					const receipt = await tx.wait();
 
-					const gas = receipt.cumulativeGasUsed.toNumber();
-					totalGas += gas;
+					const gasUsed = receipt.cumulativeGasUsed.toNumber();
+					target.measurements.push(gasUsed);
 
-					data[target].measurements.push(gas);
+					const numMeasurements = target.measurements.length;
+					target.avg = target.measurements.reduce((acum, val) => acum + val, 0) / numMeasurements;
 				}
 
-				data[target].avg = totalGas / NUM_MEASUREMENTS;
+				for (let i = 0; i < NUM_MEASUREMENTS; i++) {
+					await measureGas({
+						tx: await Synthetix.issueSynths(ethers.utils.parseEther('100')),
+						target: target.minting,
+					});
+					if (numSynths >= 2) {
+						const targetSynth = `s${numSynths - 1}`;
+						await measureGas({
+							tx: await Synthetix.exchange(
+								ethers.utils.formatBytes32String(`sUSD`),
+								ethers.utils.parseEther('1'),
+								ethers.utils.formatBytes32String(targetSynth)
+							),
+							target: target.exchanging,
+						});
+						await measureGas({
+							tx: await Synthetix.exchange(
+								ethers.utils.formatBytes32String(targetSynth),
+								ethers.utils.parseEther('1'),
+								ethers.utils.formatBytes32String(`sUSD`)
+							),
+							target: target.exchanging,
+						});
+					}
+					await measureGas({
+						tx: await Synthetix.burnSynths(ethers.utils.parseEther('50')),
+						target: target.burning,
+					});
+				}
 			});
 		});
 	}
 });
+
+async function _deploySynth({ currency, resolverAddress }) {
+	const currencyKey = ethers.utils.formatBytes32String(currency);
+
+	const TokenState = _getContractFactory({ name: 'TokenStatesUSD' });
+	const tokenState = await TokenState.deploy(signer.address, ZERO_ADDRESS);
+
+	const Proxy = _getContractFactory({ name: 'ProxysUSD' });
+	const proxy = await Proxy.deploy(signer.address);
+
+	const Synth = _getContractFactory({ name: 'SynthsUSD' });
+	const synth = await Synth.deploy(
+		proxy.address,
+		tokenState.address,
+		`Mock Synth`,
+		currency,
+		signer.address,
+		currencyKey,
+		0,
+		resolverAddress
+	);
+
+	let tx;
+
+	tx = await tokenState.setAssociatedContract(synth.address);
+	await tx.wait();
+
+	tx = await proxy.setTarget(synth.address);
+	await tx.wait();
+
+	tx = await synth.setProxy(proxy.address);
+	await tx.wait();
+
+	tx = await synth.rebuildCache();
+	await tx.wait();
+
+	return synth;
+}
 
 function _getContractFactory({ name }) {
 	const deployment = _getDeploymentFile({ filename: 'deployment.json' });
