fba2c78d4d044e182a386c060cff5baa81da76ae
dgornjakovic <dgornjakovic87@gmail.com>
2021-04-30 08:57:20 +0200

    add initialized=true to mastercopy

--- contracts/BinaryOptionMarketMastercopy.sol
+++ contracts/BinaryOptionMarketMastercopy.sol
@@ -5,5 +5,8 @@ import "./BinaryOptionMarket.sol";
 
 // https://docs.synthetix.io/contracts/source/contracts/binaryoptionmarket
 contract BinaryOptionMarketMastercopy is BinaryOptionMarket {
-    constructor() public MinimalProxyFactory() OwnedWithInit() {}
+    constructor() public MinimalProxyFactory() OwnedWithInit() {
+        // Freeze mastercopy on deployment so it can never be initialized with real arguments
+        initialized = true;
+    }
 }
