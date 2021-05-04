c3f9bc2e8c9ae018da355e02e223e2e2dab2685a
Alejandro <palebluedot@gmail.com>
2021-05-04 09:36:06 -0300

    Progress with new bridge-migration script

--- publish/src/commands/migrate-bridge.js
+++ publish/src/commands/migrate-bridge.js
@@ -21,13 +21,11 @@ const migrateBridge = async ({
 	_connect({ network, useFork, gasPrice });
 	_identify({ network });
 
-	await confirmAction('Deploy migrator?');
-
+	await confirmAction(chalk.yellow('Deploy the migrator contract?'));
 	await _deploy();
 	await _verify();
 
-	await confirmAction('Execute migration?');
-
+	await confirmAction('Execute the migration?');
 	await _execute();
 };
 
@@ -36,10 +34,10 @@ async function _execute() {
 }
 
 async function _verify() {
-	assert(snx === await migrator.methods.snx().call());
-	assert(oldBridge === await migrator.methods.oldBridge().call());
-	assert(newBridge === await migrator.methods.newBridge().call());
-	assert(newEscrow === await migrator.methods.newEscrow().call());
+	assert(snx === await migrator.methods.snx().call(), 'Unexpected snx address');
+	assert(oldBridge === await migrator.methods.oldBridge().call(), 'Unexpected old bridge address');
+	assert(newBridge === await migrator.methods.newBridge().call(), 'Unexpected new bridge address');
+	assert(newEscrow === await migrator.methods.newEscrow().call(), 'Unexpected new escrow address');
 }
 
 async function _deploy() {
@@ -62,30 +60,35 @@ async function _deploy() {
 function _identify({ network }) {
 	const users = getUsers({ network });
 
-	deployer = users.deployer;
-	pdao = users.owner;
+	deployer = users.find(u => u.name === 'deployer').address;
+	pdao = users.find(u => u.name === 'owner').address;
+	console.log(chalk.gray(`Deployer: ${deployer}`));
+	console.log(chalk.gray(`pdao: ${pdao}`));
 
 	snx = getTarget({ network, contract: 'Synthetix' }).address;
 	newBridge = getTarget({ network, contract: 'SynthetixBridgeToOptimism' }).address;
 	newEscrow = getTarget({ network, contract: 'SynthetixBridgeEscrow' }).address;
+	console.log(chalk.gray(`Synthetix: ${snx}`));
+	console.log(chalk.gray(`New bridge: ${newBridge}`));
+	console.log(chalk.gray(`New escrow: ${newEscrow}`));
 
 	if (network === 'mainnet') {
 		oldBridge = '0x045e507925d2e05D114534D0810a1abD94aca8d6';
 	} else if (network === 'kovan') {
-		oldBridge = '0xE8Bf8fe5ce9e15D30F478E1647A57CB6B0271228':
+		oldBridge = '0xE8Bf8fe5ce9e15D30F478E1647A57CB6B0271228';
 	} else {
 		throw new Error('Unsupported network');
 	}
+	console.log(chalk.gray(`Old bridge: ${oldBridge}`));
 
-	txParams = {
-		from: deployer,
-		gasPrice: Web3.utils.toWei(gasPrice.toString(), 'gwei'),
-		gas: 8000000,
-	};
+	assert(newBridge !== oldBridge, 'Bridge addresses must be different');
+
+	txParams.from = deployer;
 }
 
 function _connect({ network, useFork, gasPrice }) {
 	ensureNetwork(network);
+	console.log(chalk.gray(`Network: ${network}${ useFork ? '(FORKED)' : '' }`));
 
 	if (useFork && network !== 'mainnet') {
 		throw new Error('Command can only run on a fork if network is mainnet');
@@ -99,6 +102,13 @@ function _connect({ network, useFork, gasPrice }) {
 	web3 = new Web3(new Web3.providers.HttpProvider(providerUrl));
 
 	({ getUsers, getTarget } = wrap({ network, fs, path }));
+
+	txParams = {
+		gasPrice: web3.utils.toWei(gasPrice.toString(), 'gwei'),
+		gas: 8000000,
+	};
+	console.log(chalk.gray(`Gas price: ${gasPrice} gwei (${txParams.gasPrice} wei)`));
+	console.log(chalk.gray(`Gas limit: ${txParams.gas}`));
 }
 
 module.exports = {
@@ -109,15 +119,14 @@ module.exports = {
 			.description(
 				'Migrates a SynthetixBridgeToOptimism (v1) to a SynthetixBridgeToOptimism + SynthetixBridgeEscrow (v2) via a BridgeMigrator contract.'
 			)
-			.option('--network <value>', 'The target network', network => network.toLowerCase(), 'kovan')
+			.option('--network <value>', 'The target network', network => network.toLowerCase())
 			.option('--use-fork', 'Run the migration on a fork of mainnet', false)
-			.option('--gas-price <value>', 'Gas price in GWEI to use in all transactions', parseFloat, 1)
+			.option('--gas-price <value>', 'Gas price in GWEI to use in all transactions', parseFloat, 100)
 			.action(async (...args) => {
 				try {
 					await migrateBridge(...args);
 				} catch (err) {
-					console.error(red(err));
-					console.log(err.stack);
+					console.error(chalk.red(err));
 					process.exitCode = 1;
 				}
 			}),
