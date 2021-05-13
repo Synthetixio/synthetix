72760b339cdb908a23722d191d711b2d16e2262f
liamzebedee <liamzebedee@yahoo.com.au>
2021-05-13 22:43:01 +1000

    fix: up gas limit for takeDebtSnapshot

--- publish/src/commands/deploy.js
+++ publish/src/commands/deploy.js
@@ -2501,7 +2501,7 @@ const deploy = async ({
 		if (force || validityChanged) {
 			console.log(yellow(`Refreshing debt snapshot...`));
 			await runStep({
-				gasLimit: useOvm ? 4.0e6 : 3.85e6, // About 3.34 million gas is required to refresh the snapshot with ~40 synths on L1
+				gasLimit: useOvm ? 4.0e6 : 5.0e6, // About 3.34 million gas is required to refresh the snapshot with ~40 synths on L1
 				contract: 'DebtCache',
 				target: debtCache,
 				write: 'takeDebtSnapshot',
