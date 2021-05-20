1f66a9a802410fe3d4d56cb3e805c65b4817c5f7
Alejandro <palebluedot@gmail.com>
2021-05-20 19:42:11 -0300

    Simplify get optimism revert reason

--- test/optimism/utils/revertOptimism.js
+++ test/optimism/utils/revertOptimism.js
@@ -1,38 +1,3 @@
-const ethers = require('ethers');
-
-function _hexToString(hex) {
-	let str = '';
-
-	const terminator = '**zÛ';
-	for (var i = 0; i < hex.length; i += 2) {
-		str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-
-		if (str.includes(terminator)) {
-			break;
-		}
-	}
-
-	return str.substring(0, str.length - 4);
-}
-
-async function getOptimismRevertReason({ tx, provider }) {
-	try {
-		const code = (await provider.call(tx)).substr(138);
-		const hex = `0x${code}`;
-
-		let reason;
-		if (code.length === '64') {
-			reason = ethers.utils.parseBytes32String(hex);
-		} else {
-			reason = _hexToString(hex);
-		}
-
-		return reason;
-	} catch (suberror) {
-		throw new Error(`Unable to parse revert reason: ${suberror}`);
-	}
-}
-
 async function assertRevertOptimism({ tx, reason, provider }) {
 	let receipt;
 	let revertReason;
@@ -42,12 +7,8 @@ async function assertRevertOptimism({ tx, reason, provider }) {
 
 		receipt = await response.wait();
 	} catch (error) {
-		const txRequest = {
-			to: await error.tx.to,
-			data: await error.tx.data,
-		};
-
-		revertReason = await getOptimismRevertReason({ tx: txRequest, provider });
+		const body = JSON.parse(error.error.error.body);
+		revertReason = body.error.message;
 	}
 
 	if (receipt) {
@@ -62,6 +23,5 @@ async function assertRevertOptimism({ tx, reason, provider }) {
 }
 
 module.exports = {
-	getOptimismRevertReason,
 	assertRevertOptimism,
 };
