d95efe5337fa0d96c3acce89ca8fc4837ea9e3ad
Justin J. Moses <justinjmoses@gmail.com>
2021-05-25 16:21:10 -0600

    Better method

--- contracts/Exchanger.sol
+++ contracts/Exchanger.sol
@@ -661,11 +661,12 @@ contract Exchanger is Owned, MixinSystemSettings, IExchanger {
 
     // SIP-139
     function resetLastExchangeRate(bytes32[] calldata currencyKeys) external onlyOwner {
-        require(!exchangeRates().anyRateIsInvalid(currencyKeys), "Rates for given synths not valid");
+        (uint[] memory rates, bool anyRateInvalid) = exchangeRates().ratesAndInvalidForCurrencies(currencyKeys);
+
+        require(!anyRateInvalid, "Rates for given synths not valid");
 
         for (uint i = 0; i < currencyKeys.length; i++) {
-            bytes32 currencyKey = currencyKeys[i];
-            lastExchangeRate[currencyKey] = exchangeRates().rateForCurrency((currencyKey));
+            lastExchangeRate[currencyKeys[i]] = rates[i];
         }
     }
 
