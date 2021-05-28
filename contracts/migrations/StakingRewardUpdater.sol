8ab3a75721cc3d8fbd84dfb176a4db4ee7fc1ebc
Justin J. Moses <justinjmoses@gmail.com>
2021-05-27 18:35:28 -0600

    Adding backup check

--- contracts/migrations/StakingRewardUpdater.sol
+++ contracts/migrations/StakingRewardUpdater.sol
@@ -3,7 +3,15 @@ pragma solidity ^0.5.16;
 import "../StakingRewards.sol";
 
 contract StakingRewardUpdater {
-    // For all staking reward contracts that have expired, take ownership, set duration, send SNX rewards,
+    address public deployer;
+
+    constructor() public {
+        deployer = msg.sender;
+    }
+
+    // For all staking reward contracts that have expired,
+    // take ownership, set duration, send SNX rewards, notify rewards then return ownership
+    // Requires: this contract be the nominated owner of the staking rewards and sufficient rewards tokens given to each contract.
     function execute(
         StakingRewards[] calldata rewardContracts,
         uint rewardsPerContract,
@@ -36,5 +44,9 @@ contract StakingRewardUpdater {
 
             rewardContract.nominateNewOwner(previousOwner);
         }
+    }
+
+    function returnFunds(IERC20 token) external {
+        token.transfer(deployer, token.balanceOf(address(this)));
     }
 }
