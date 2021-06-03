7d156f71f9844a15a74e419f9dd78c39698e7a17
Leonardo Massazza <lmassazza+github@gmail.com>
2021-06-03 16:25:49 -0300

    Add support to SynthsUSD in L1 and L2 integration tests

--- test/integration/behaviors/erc20.behavior.js
+++ test/integration/behaviors/erc20.behavior.js
@@ -1,36 +1,50 @@
 const ethers = require('ethers');
 const { assert } = require('../../contracts/common');
+const { ensureBalance } = require('../utils/balances');
 
-function itBehavesLikeAnERC20({ ctx }) {
+function itBehavesLikeAnERC20({ ctx, contract }) {
 	describe('erc20 functionality', () => {
 		let owner, user;
-		let Synthetix;
+		let Contract;
 
 		let userBalance;
 
 		const amountToTransfer = ethers.utils.parseEther('1');
 
-		before('target contracts and users', () => {
-			({ Synthetix } = ctx.contracts);
+		before('target contracts and users', async () => {
+			if (contract) {
+				Contract = ctx.contracts[contract];
+				const symbol = await Contract.symbol();
+
+				await ensureBalance({
+					ctx,
+					symbol,
+					user: ctx.users.owner,
+					balance: ethers.utils.parseEther('100'),
+				});
+			} else {
+				const { Synthetix } = ctx.contracts;
+				Contract = Synthetix;
+			}
 
 			owner = ctx.users.owner;
 			user = ctx.users.someUser;
 		});
 
 		before('record user balance', async () => {
-			userBalance = await Synthetix.balanceOf(user.address);
+			userBalance = await Contract.balanceOf(user.address);
 		});
 
-		describe('when the owner transfers SNX to the user', () => {
+		describe('when the owner transfers Tokens to the user', async () => {
 			before('transfer', async () => {
-				Synthetix = Synthetix.connect(owner);
+				Contract = Contract.connect(owner);
 
-				const tx = await Synthetix.transfer(user.address, amountToTransfer);
+				const tx = await Contract.transfer(user.address, amountToTransfer);
 				await tx.wait();
 			});
 
-			it('increases the users balance', async () => {
-				assert.bnEqual(await Synthetix.balanceOf(user.address), userBalance.add(amountToTransfer));
+			it(`increases the users balance`, async () => {
+				assert.bnEqual(await Contract.balanceOf(user.address), userBalance.add(amountToTransfer));
 			});
 		});
 	});
