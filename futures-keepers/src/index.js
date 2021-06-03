e2b34b03fa00a61417291e7b6e15efd26bd31304
liamzebedee <liamzebedee@yahoo.com.au>
2021-06-03 15:39:42 +1000

    remove NonceManager from @ethersproject/experimental
    This didn't appear to be necessary with the OVM. I discovered the nonce conflict was due to a different reason.

--- futures-keepers/src/index.js
+++ futures-keepers/src/index.js
@@ -7,8 +7,8 @@ async function main() {
 	const {
 		FUTURES_MARKET_ETH_ADDRESS,
 		EXCHANGE_RATES_ADDRESS,
-		POLL_INTERVAL,
 		ETH_PRIVATE_KEY,
+		FROM_BLOCK,
 	} = process.env;
 	if (!FUTURES_MARKET_ETH_ADDRESS) {
 		throw new Error('FUTURES_MARKET_ETH_ADDRESS environment variable is not configured.');
@@ -16,20 +16,22 @@ async function main() {
 	if (!EXCHANGE_RATES_ADDRESS) {
 		throw new Error('EXCHANGE_RATES_ADDRESS environment variable is not configured.');
 	}
-	if (!POLL_INTERVAL) {
-		throw new Error('POLL_INTERVAL environment variable is not configured.');
-	}
 	if (!ETH_PRIVATE_KEY) {
 		throw new Error('ETH_PRIVATE_KEY environment variable is not configured.');
 	}
 
 	const privateKey = ETH_PRIVATE_KEY;
-	const pollInterval = parseInt(POLL_INTERVAL);
+	const pollInterval = 1; // TODO
+	const fromBlock = parseInt(FROM_BLOCK);
 
 	// Setup.
 	//
 	const provider = new ethers.providers.JsonRpcProvider();
+	// const signer = new NonceManager(
+	// 	new ethers.Wallet(privateKey, provider)
+	// )
 	const signer = new ethers.Wallet(privateKey, provider);
+	// const signer = new ethers.Wallet(privateKey, provider)
 	const account = await signer.getAddress();
 	console.log(gray(`Connected to Ethereum node at http://localhost:8545`));
 	console.log(gray(`Account: ${account}`));
@@ -41,8 +43,7 @@ async function main() {
 		pollInterval,
 		provider,
 	});
-	const FROM_BLOCK = 423;
-	keeper.run({ fromBlock: FROM_BLOCK || 'latest' });
+	keeper.run({ fromBlock: fromBlock || 'latest' });
 
 	await new Promise((resolve, reject) => {});
 }
