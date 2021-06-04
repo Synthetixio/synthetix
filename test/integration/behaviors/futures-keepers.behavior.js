b028f5ed8f76f577e90d591a03afaeef5d316d69
liamzebedee <liamzebedee@yahoo.com.au>
2021-06-04 18:07:47 +1000

    stress test the futures keeper

--- test/integration/behaviors/futures-keepers.behavior.js
+++ test/integration/behaviors/futures-keepers.behavior.js
@@ -17,7 +17,7 @@ function itConfirmsOrders({ ctx }) {
 		async function setPrice(asset, price) {
 			const { timestamp } = await ctx.provider.getBlock();
 			const tx = await ExchangeRates.updateRates([asset], [parseEther(price)], timestamp);
-			await tx.wait();
+			await tx.wait(1);
 		}
 
 		before('target contracts and users', () => {
@@ -44,7 +44,7 @@ function itConfirmsOrders({ ctx }) {
 
 			before('submit the order', async () => {
 				const tx = await FuturesMarketETH.modifyMarginAndSubmitOrder(margin, leverage);
-				txReceipt = await tx.wait();
+				txReceipt = await tx.wait(1);
 
 				const orderSubmitted = txReceipt.events.filter(
 					({ address, event }) =>
@@ -70,6 +70,67 @@ function itConfirmsOrders({ ctx }) {
 				assert.isAtLeast(events.length, 1);
 			});
 		});
+
+		describe('stress testing', async () => {
+			let users;
+			const leverage = parseEther('1.0');
+			const margin = parseEther('150');
+			let orders;
+			let startBlockNumber;
+
+			before('ensure the users have sUSD', async () => {
+				users = ctx.users.wallets.slice(10, 20);
+				const sUSDAmount = parseEther('200');
+
+				for (const user of users) {
+					await ensureBalance({ ctx: ctx, symbol: 'sUSD', user: user, balance: sUSDAmount });
+				}
+			});
+
+			before('10 orders are submitted in short timeframe', async () => {
+				startBlockNumber = await ctx.provider.getBlockNumber();
+
+				const receipts = await Promise.all(
+					users.map(async user => {
+						FuturesMarketETH = FuturesMarketETH.connect(user);
+						const tx = await FuturesMarketETH.modifyMarginAndSubmitOrder(margin, leverage);
+						const receipt = await tx.wait(1);
+						return receipt;
+					})
+				);
+
+				const findOrderId = txReceipt => {
+					const orderSubmitted = txReceipt.events.filter(
+						({ address, event }) =>
+							address === ProxyFuturesMarketETH.address && event === 'OrderSubmitted'
+					)[0];
+
+					const orderId = orderSubmitted.args.id;
+					return orderId;
+				};
+
+				orders = receipts.map(findOrderId);
+			});
+
+			before('next price update', async () => {
+				await setPrice(sETH, '205');
+			});
+
+			it('processes 10 orders within 1s', async () => {
+				assert.doesNotThrow(() =>
+					Promise.all(
+						orders.map(order =>
+							waitForEvent(
+								FuturesMarketETH,
+								FuturesMarketETH.filters.OrderConfirmed(order),
+								startBlockNumber,
+								1000
+							)
+						)
+					)
+				);
+			});
+		});
 	});
 }
 
@@ -83,7 +144,7 @@ function itLiquidatesOrders({ ctx }) {
 		async function setPrice(asset, price) {
 			const { timestamp } = await ctx.provider.getBlock();
 			const tx = await ExchangeRates.updateRates([asset], [parseEther(price)], timestamp);
-			await tx.wait();
+			await tx.wait(1);
 		}
 
 		before('target contracts and users', () => {
--- test/integration/l2/futures-keepers.l2.integration.js
+++ test/integration/l2/futures-keepers.l2.integration.js
@@ -1,10 +1,10 @@
 const { bootstrapL2 } = require('../utils/bootstrap');
 const { itConfirmsOrders, itLiquidatesOrders } = require('../behaviors/futures-keepers.behavior');
 
-describe.only('futures keepers integration tests (L2)', () => {
+describe('futures keepers integration tests (L2)', () => {
 	const ctx = this;
 	bootstrapL2({ ctx });
 
-	// itConfirmsOrders({ ctx });
+	itConfirmsOrders({ ctx });
 	itLiquidatesOrders({ ctx });
 });
