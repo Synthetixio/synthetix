7a978c2652954dbb15265a10e3dc1a2e2d606e95
Alejandro <palebluedot@gmail.com>
2021-06-08 19:55:36 -0300

    Merge all integration test tasks into one

--- test/integration/utils/watchers.js
+++ test/integration/utils/watchers.js
@@ -1,5 +1,9 @@
+const chalk = require('chalk');
+
 async function finalizationOnL2({ ctx, transactionHash }) {
 	const messageHashes = await ctx.watcher.getMessageHashesFromL1Tx(transactionHash);
+	console.log(chalk.gray(`> Awaiting for ${messageHashes} to finalize on L2...`));
+
 	const promises = messageHashes.map(messageHash =>
 		ctx.watcher.getL2TransactionReceipt(messageHash)
 	);
@@ -9,6 +13,8 @@ async function finalizationOnL2({ ctx, transactionHash }) {
 
 async function finalizationOnL1({ ctx, transactionHash }) {
 	const messageHashes = await ctx.watcher.getMessageHashesFromL2Tx(transactionHash);
+	console.log(chalk.gray(`> Awaiting for ${messageHashes} to finalize on L1...`));
+
 	const promises = messageHashes.map(messageHash =>
 		ctx.watcher.getL1TransactionReceipt(messageHash)
 	);
