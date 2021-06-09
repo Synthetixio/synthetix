2f3ab505a999eef817810964fea6974a763215d3
Alejandro <palebluedot@gmail.com>
2021-06-09 10:29:51 -0300

    Throttling publish test slightly

--- hardhat/tasks/task-node.js
+++ hardhat/tasks/task-node.js
@@ -2,10 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const { knownAccounts, wrap } = require('../..');
 const { red, gray, yellow } = require('chalk');
-const { subtask, task } = require('hardhat/config');
+const { subtask, task, types } = require('hardhat/config');
 const { TASK_NODE_SERVER_READY } = require('hardhat/builtin-tasks/task-names');
 
 task('node', 'Run a node')
+	.addOptionalParam(
+		'miningInterval',
+		'Time interval at which blocks should be mined. Default to 0 means each tx mines a block',
+		0,
+		types.int
+	)
 	.addOptionalParam('targetNetwork', 'Target network to simulate, i.e. mainnet or local', 'local')
 	.setAction(async (taskArguments, hre, runSuper) => {
 		// Enable forking if necessary
@@ -23,9 +29,21 @@ task('node', 'Run a node')
 			console.log(yellow('Forking Mainnet...'));
 		}
 
+		// Set mining options.
+		if (taskArguments.miningInterval !== 0) {
+			console.log(yellow(`Using custom mining interval: ${taskArguments.miningInterval}`));
+			console.log(hre.config.networks.hardhat);
+			hre.config.networks.hardhat.mining = {
+				auto: false,
+				interval: taskArguments.miningInterval,
+			};
+			console.log(hre.config.networks.hardhat);
+		}
+
 		subtask(TASK_NODE_SERVER_READY).setAction(async ({ provider }, hre, runSuper) => {
 			await runSuper();
 
+			// Display targetted network.
 			const network = taskArguments.targetNetwork;
 			console.log(
 				yellow(`Targeting Synthetix in ${network}${taskArguments.fork ? ' (forked)' : ''}`)
