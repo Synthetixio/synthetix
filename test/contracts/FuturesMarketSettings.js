855dfefd5f87fe54da609fc62a910f095d4d617f
i-stam <i.stamelakos@gmail.com>
2021-06-16 16:52:24 +0300

    Add new tests

--- test/contracts/FuturesMarketSettings.js
+++ test/contracts/FuturesMarketSettings.js
@@ -12,6 +12,8 @@ const {
 	ensureOnlyExpectedMutativeFunctions,
 } = require('./helpers');
 
+const BN = require('bn.js');
+
 contract('FuturesMarketSettings', accounts => {
 	let futuresMarketSettings;
 
@@ -108,7 +110,7 @@ contract('FuturesMarketSettings', accounts => {
 			];
 		});
 
-		it('Initially the params are unitialized', async () => {
+		it('Initially the params are uninitialized', async () => {
 			for (const p of params) {
 				const getter = p[3];
 				assert.bnEqual(await getter(baseAsset), '0');
@@ -131,34 +133,108 @@ contract('FuturesMarketSettings', accounts => {
 							accounts,
 						});
 					}
+					const fundingParams = [maxFundingRate, maxFundingRateSkew, maxFundingRateDelta];
+
+					await onlyGivenAddressCanInvoke({
+						fnc: futuresMarketSettings.setAllParameters,
+						args: [
+							baseAsset,
+							takerFee,
+							makerFee,
+							maxLeverage,
+							maxMarketValue,
+							minInitialMargin,
+							fundingParams,
+						],
+						address: owner,
+						accounts,
+					});
 				});
 			});
 
 			describe('when invoked by the owner', async () => {
-				it('should set the params accordingly and emit the corresponding events', async () => {
-					for (const p of params) {
-						const param = toBytes32(p[0]);
-						const value = p[1];
-						const setter = p[2];
-						const getter = p[3];
+				describe('Set params independently', async () => {
+					it('should set the params accordingly and emit the corresponding events', async () => {
+						for (const p of params) {
+							const param = toBytes32(p[0]);
+							const value = p[1];
+							const setter = p[2];
+							const getter = p[3];
 
-						const tx = await setter(baseAsset, value, { from: owner });
+							const tx = await setter(baseAsset, value, { from: owner });
+
+							const decodedLogs = await getDecodedLogs({
+								hash: tx.tx,
+								contracts: [futuresMarketSettings],
+							});
+							assert.equal(decodedLogs.length, 1);
+							decodedEventEqual({
+								event: 'ParameterUpdated',
+								emittedFrom: futuresMarketSettings.address,
+								args: [baseAsset, param, value],
+								log: decodedLogs[0],
+							});
+
+							// And the parameter was actually set properly
+							assert.bnEqual(await getter(baseAsset), value.toString());
+						}
+					});
+				});
+
+				describe('SetAllParameters', async () => {
+					const bn2 = new BN(2);
+					const newTakerFee = takerFee.mul(bn2);
+					const newMakerFee = makerFee.mul(bn2);
+					const newMaxLeverage = maxLeverage.mul(bn2);
+					const newMaxMarketValue = maxMarketValue.mul(bn2);
+					const newMinInitialMargin = minInitialMargin.mul(bn2);
+					const newMaxFundingRate = maxFundingRate.mul(bn2);
+					const newMaxFundingRateSkew = maxFundingRateSkew.mul(bn2);
+					const newMaxFundingRateDelta = maxFundingRateDelta.mul(bn2);
+					const newFundingParams = [
+						newMaxFundingRate,
+						newMaxFundingRateSkew,
+						newMaxFundingRateDelta,
+					];
+					let tx;
+					before(
+						'should set the params accordingly and emit the corresponding events',
+						async () => {
+							tx = await futuresMarketSettings.setAllParameters(
+								baseAsset,
+								newTakerFee,
+								newMakerFee,
+								newMaxLeverage,
+								newMaxMarketValue,
+								newMinInitialMargin,
+								newFundingParams,
+								{ from: owner }
+							);
+						}
+					);
+					it('should set the params accordingly and emit the corresponding events', async () => {
+						const newParams = await futuresMarketSettings.getAllParameters(baseAsset);
 
 						const decodedLogs = await getDecodedLogs({
 							hash: tx.tx,
 							contracts: [futuresMarketSettings],
 						});
-						assert.equal(decodedLogs.length, 1);
-						decodedEventEqual({
-							event: 'ParameterUpdated',
-							emittedFrom: futuresMarketSettings.address,
-							args: [baseAsset, param, value],
-							log: decodedLogs[0],
-						});
+						assert.equal(decodedLogs.length, 8);
 
-						// And the parameter was actually set properly
-						assert.bnEqual(await getter(baseAsset), value.toString());
-					}
+						for (const p of params) {
+							const param = toBytes32(p[0]);
+							const value = p[1].mul(bn2);
+
+							decodedEventEqual({
+								event: 'ParameterUpdated',
+								emittedFrom: futuresMarketSettings.address,
+								args: [baseAsset, param, value],
+								log: decodedLogs[params.indexOf(p)],
+							});
+
+							assert.bnEqual(await newParams[p[0]], value.toString());
+						}
+					});
 				});
 			});
 		});
