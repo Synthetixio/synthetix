3bf52c805d0ce36d2779a1129dc4b20b57e1222b
Justin J. Moses <justinjmoses@gmail.com>
2021-06-30 12:24:51 -0600

    Fix to ensure that in fork mode the env key is not used unless specified

--- publish/src/commands/deploy/index.js
+++ publish/src/commands/deploy/index.js
@@ -189,8 +189,9 @@ const deploy = async ({
 		providerUrl = envProviderUrl;
 	}
 
-	// if not specified, or in a local network, override the private key passed as a CLI option, with the one specified in .env
-	if (network !== 'local' && !privateKey) {
+	// when not in a local network, and not forking, and the privateKey isn't supplied,
+	// use the one from the .env file
+	if (network !== 'local' && !useFork && !privateKey) {
 		privateKey = envPrivateKey;
 	}
 
