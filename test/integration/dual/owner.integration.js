733ba525f3c072bd2a4f3882203d8eddd564b180
Alejandro <palebluedot@gmail.com>
2021-07-13 17:42:14 -0300

    WIP More interesting owner relay integration tests

--- test/integration/dual/owner.integration.js
+++ test/integration/dual/owner.integration.js
@@ -10,16 +10,21 @@ describe.only('owner relay integration tests (L1, L2)', () => {
 	let ownerL1, ownerL2;
 	let OwnerRelayOnEthereum, OwnerRelayOnOptimism, SystemSettingsL2;
 
-	describe('when changing an L2 system setting from L1 via the relay', () => {
-		before('target contracts and users', () => {
-			({ OwnerRelayOnEthereum } = ctx.l1.contracts);
-			({ OwnerRelayOnOptimism, SystemSettings: SystemSettingsL2 } = ctx.l2.contracts);
+	before('target contracts and users', () => {
+		({ OwnerRelayOnEthereum } = ctx.l1.contracts);
+		({ OwnerRelayOnOptimism, SystemSettings: SystemSettingsL2 } = ctx.l2.contracts);
 
-			ownerL1 = ctx.l1.users.owner;
-			ownerL2 = ctx.l2.users.owner;
+		ownerL1 = ctx.l1.users.owner;
+		ownerL2 = ctx.l2.users.owner;
+	});
+
+	describe('when SystemSettings on L2 is owned by an EOA', () => {
+		before('check ownership', async function () {
+			if (await SystemSettingsL2.owner() !== ownerL2.address) {
+				this.skip();
+			}
 		});
 
-		// TODO: What if the relay is already the owner?
 		it('shows that the current owner of SystemSettings is the deployer', async () => {
 			assert.equal(await SystemSettingsL2.owner(), ownerL2.address);
 		});
@@ -55,6 +60,48 @@ describe.only('owner relay integration tests (L1, L2)', () => {
 				it('shows that the current owner of SystemSettings is the L2 relay', async () => {
 					assert.equal(await SystemSettingsL2.owner(), OwnerRelayOnOptimism.address);
 				});
+			});
+		});
+	});
+
+	describe('when SystemSettings on L2 is owned by the relay', () => {
+		it('shows that the current owner of SystemSettings is the L2 relay', async () => {
+			assert.equal(await SystemSettingsL2.owner(), OwnerRelayOnOptimism.address);
+		});
+
+		describe('when changing an L2 system setting with an L1 tx', () => {
+			let minimumStakeTime;
+
+			before('relay setMinimumStakeTime via the bridge', async () => {
+				OwnerRelayOnEthereum = OwnerRelayOnEthereum.connect(ownerL1);
+
+				const calldata = SystemSettingsL2.interface.encodeFunctionData('setMinimumStakeTime', [0]);
+
+				const tx = await OwnerRelayOnEthereum.relay(SystemSettingsL2.address, calldata);
+				relayReceipt = await tx.wait();
+			});
+
+			before('store minimumStakeTime', async () => {
+				minimumStakeTime = await SystemSettingsL2.minimumStakeTime();
+			});
+
+			before('wait for the relay to finalize on L2', async () => {
+				await finalizationOnL2({ ctx, transactionHash: relayReceipt.transactionHash });
+			});
+
+			after('restore minimumStakeTime', async () => {
+				OwnerRelayOnEthereum = OwnerRelayOnEthereum.connect(ownerL1);
+
+				const calldata = SystemSettingsL2.interface.encodeFunctionData('setMinimumStakeTime', [minimumStakeTime]);
+
+				const tx = await OwnerRelayOnEthereum.relay(SystemSettingsL2.address, calldata);
+				relayReceipt = await tx.wait();
+
+				await finalizationOnL2({ ctx, transactionHash: relayReceipt.transactionHash });
+			});
+
+			it('shows that the minimum stake time is now zero', async () => {
+				assert.equal((await SystemSettingsL2.minimumStakeTime()).toString(), '0');
 			});
 		});
 	});
