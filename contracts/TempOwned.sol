9cd39a18779e3607f86885a4d1dbe29eff761e27
Alejandro <palebluedot@gmail.com>
2021-08-16 14:41:46 -0300

    Add require checks to temp owner

--- contracts/TempOwned.sol
+++ contracts/TempOwned.sol
@@ -1,10 +1,14 @@
 pragma solidity ^0.5.16;
 
+
 contract TempOwned {
     address internal tempOwner;
     uint internal tempOwnerEOL;
 
     constructor(address _tempOwner, uint _tempOwnerEOL) public {
+        require(_tempOwner != address(0), "Owner address cannot be 0");
+        require(_tempOwnerEOL > now, "Onvalid temp owner EOL");
+
         tempOwner = _tempOwner;
         tempOwnerEOL = _tempOwnerEOL;
     }
