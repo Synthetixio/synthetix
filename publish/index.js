ab52c2c275a57700091cc48aee6d68e355eec321
Justin J. Moses <justinjmoses@gmail.com>
2021-08-17 16:06:33 -0500

    Adding finalize script, upping commander, updating version update

--- publish/index.js
+++ publish/index.js
@@ -7,9 +7,11 @@ require('dotenv').config();
 
 require('./src/commands/build').cmd(program);
 require('./src/commands/connect-bridge').cmd(program);
+require('./src/commands/deploy-shorting-rewards').cmd(program);
 require('./src/commands/deploy-staking-rewards').cmd(program);
 require('./src/commands/deploy').cmd(program);
 require('./src/commands/extract-staking-balances').cmd(program);
+require('./src/commands/finalize-release').cmd(program);
 require('./src/commands/import-fee-periods').cmd(program);
 require('./src/commands/migrate-binary-option-markets').cmd(program);
 require('./src/commands/nominate').cmd(program);
@@ -25,6 +27,5 @@ require('./src/commands/settle').cmd(program);
 require('./src/commands/verify').cmd(program);
 require('./src/commands/versions-history').cmd(program);
 require('./src/commands/versions-update').cmd(program);
-require('./src/commands/deploy-shorting-rewards').cmd(program);
 
 program.parse(process.argv);
