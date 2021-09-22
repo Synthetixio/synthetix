b02a122464353a761afe4864eee4019e31eccf20
Justin J. Moses <justinjmoses@gmail.com>
2021-09-22 12:18:57 +0200

    Putting initial base fee back

--- hardhat.config.js
+++ hardhat.config.js
@@ -52,6 +52,7 @@ module.exports = {
 			gasPrice: GAS_PRICE,
 			hardfork: 'london',
 			initialDate: new Date(inflationStartTimestampInSecs * 1000).toISOString(),
+			initialBaseFeePerGas: (1e9).toString(), // 1 GWEI
 			// Note: forking settings are injected at runtime by hardhat/tasks/task-node.js
 		},
 		localhost: {
