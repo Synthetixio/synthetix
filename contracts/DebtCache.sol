0bae287386fe135eb9a38a7c4e7b6a6047fcd25e
Jackson C <jackosmacko@gmail.com>
2021-10-11 17:33:34 +1100

    update integration tests to update cache before exchange (as failing on partial synth updates)

--- contracts/DebtCache.sol
+++ contracts/DebtCache.sol
@@ -119,6 +119,9 @@ contract DebtCache is BaseDebtCache {
         // Apply the debt update.
         if (cachedSum != currentSum) {
             uint debt = _cachedDebt;
+            // This requirement should never fail, as the total debt snapshot is the sum of the individual synth
+            // debt snapshots.
+            require(cachedSum <= debt, "Cached synth sum exceeds total debt");
             debt = debt.sub(cachedSum).add(currentSum);
             _cachedDebt = debt;
             emit DebtCacheUpdated(debt);
