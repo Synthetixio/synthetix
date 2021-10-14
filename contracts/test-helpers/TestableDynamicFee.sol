89116c39f60a89339915ab4978913d14740217cc
Lecky <leckylao@gmail.com>
2021-10-14 18:06:20 +1100

    Adding more tests for DynamicFee

--- contracts/test-helpers/TestableDynamicFee.sol
+++ contracts/test-helpers/TestableDynamicFee.sol
@@ -4,7 +4,23 @@ pragma solidity ^0.5.16;
 import "../libraries/DynamicFee.sol";
 
 contract TestableDynamicFee {
-    function testGetDynamicFee(uint[] memory prices) public pure returns (uint) {
+    function testThreshold() public pure returns (uint) {
+        return DynamicFee.threshold();
+    }
+
+    function testWeightDecay() public pure returns (uint) {
+        return DynamicFee.weightDecay();
+    }
+
+    function testGetPriceDifferential(uint price, uint previousPrice) public pure returns (uint) {
+        return DynamicFee.getPriceDifferential(price, previousPrice);
+    }
+
+    function testGetPriceWeight(uint round) public view returns (uint) {
+        return DynamicFee.getPriceWeight(round);
+    }
+
+    function testGetDynamicFee(uint[] memory prices) public view returns (uint) {
         return DynamicFee.getDynamicFee(prices);
     }
 }
--- test/contracts/libraries/TestableDynamicFee.js
+++ test/contracts/libraries/TestableDynamicFee.js
@@ -14,6 +14,35 @@ contract('TestableDynamicFee', () => {
 		testableDynamicFee = await TestableDynamicFee.new();
 	});
 
+	it('Can get threshold', async () => {
+		const threshold = await testableDynamicFee.testThreshold();
+		assert.bnEqual(threshold, toUnit('0.004'));
+	});
+
+	it('Can get weight decay', async () => {
+		const weightDecay = await testableDynamicFee.testWeightDecay();
+		assert.bnEqual(weightDecay, toUnit('0.9'));
+	});
+
+	it('Can get price differential', async () => {
+		const priceDiff = await testableDynamicFee.testGetPriceDifferential(
+			toUnit('102'),
+			toUnit('101')
+		);
+		assert.bnEqual(priceDiff, '9900990099009900');
+	});
+
+	it('Can get price weight', async () => {
+		const priceWeight0 = await testableDynamicFee.testGetPriceWeight('0');
+		assert.bnEqual(priceWeight0, toUnit('1'));
+
+		const priceWeight1 = await testableDynamicFee.testGetPriceWeight('1');
+		assert.bnEqual(priceWeight1, toUnit('0.9'));
+
+		const priceWeight2 = await testableDynamicFee.testGetPriceWeight('2');
+		assert.bnEqual(priceWeight2, toUnit('0.81'));
+	});
+
 	it('Can get dynamic fee', async () => {
 		const prices = [toUnit('102'), toUnit('101'), toUnit('100')];
 		const dynamicFee = await testableDynamicFee.testGetDynamicFee(prices);
