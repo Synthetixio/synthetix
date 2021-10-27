5302a44cd058da97fcafe6c7c3ebcdd50b4605eb
Lecky <leckylao@gmail.com>
2021-10-27 16:55:13 +1100

    fixing setup library linking

--- test/contracts/setup.js
+++ test/contracts/setup.js
@@ -127,7 +127,8 @@ const setupContract = async ({
 		const safeDecimalMath = await artifacts.require('SafeDecimalMath').new();
 		await artifact.link(safeDecimalMath);
 		// eslint-disable-next-line no-constant-condition
-		if (contract === 'Exchanger' || 'ExchangerWithVirtualSynth') {
+		if (contract === ('Exchanger' || 'ExchangerWithVirtualSynth')) {
+			console.log('contract: ', contract);
 			const DynamicFee = artifacts.require('DynamicFee');
 			DynamicFee.link(safeDecimalMath);
 			await artifact.link(await DynamicFee.new());
