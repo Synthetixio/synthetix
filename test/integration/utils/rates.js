f2fa864dccf925b8f47a02588089564f2f423a10
artdgn <arthurdgn@gmail.com>
2021-12-16 14:54:20 +1100

    fix integration tests setup

--- test/integration/utils/rates.js
+++ test/integration/utils/rates.js
@@ -2,15 +2,12 @@ const ethers = require('ethers');
 const { setSystemSetting } = require('./settings');
 const { toBytes32 } = require('../../..');
 
-async function updateExchangeRatesIfNeeded({ ctx }) {
+async function increaseStalePeriodAndCheckRatesAndCache({ ctx }) {
 	await setSystemSetting({ ctx, settingName: 'rateStalePeriod', newValue: '1000000000' });
 
 	if (await _areRatesInvalid({ ctx })) {
-		await _setNewRates({ ctx });
-		if (await _areRatesInvalid({ ctx })) {
-			await _printRatesInfo({ ctx });
-			throw new Error('Rates are still invalid after updating them.');
-		}
+		await _printRatesInfo({ ctx });
+		throw new Error('Rates are invalid.');
 	}
 
 	if (await _isCacheInvalid({ ctx })) {
@@ -71,37 +68,6 @@ async function _getAvailableCurrencyKeys({ ctx }) {
 		.concat(['SNX', 'ETH'].map(toBytes32));
 }
 
-async function _getCurrentRates({ ctx, currencyKeys }) {
-	const { ExchangeRates } = ctx.contracts;
-
-	const rates = [];
-	for (const currencyKey of currencyKeys) {
-		const rate = await ExchangeRates.rateForCurrency(currencyKey);
-
-		// Simualte any exchange rates that are 0.
-		const newRate = rate.toString() === '0' ? ethers.utils.parseEther('1') : rate;
-
-		rates.push(newRate);
-	}
-
-	return rates;
-}
-
-async function _setNewRates({ ctx }) {
-	let { ExchangeRates } = ctx.contracts;
-	const oracle = await ExchangeRates.oracle();
-	const signer = ctx.fork ? ctx.provider.getSigner(oracle) : ctx.users.owner;
-	ExchangeRates = ExchangeRates.connect(signer);
-
-	const currencyKeys = await _getAvailableCurrencyKeys({ ctx });
-	const { timestamp } = await ctx.provider.getBlock();
-
-	const rates = await _getCurrentRates({ ctx, currencyKeys });
-
-	const tx = await ExchangeRates.updateRates(currencyKeys, rates, timestamp);
-	await tx.wait();
-}
-
 async function _updateCache({ ctx }) {
 	let { DebtCache } = ctx.contracts;
 
@@ -122,7 +88,7 @@ async function getRate({ ctx, symbol }) {
 }
 
 module.exports = {
-	updateExchangeRatesIfNeeded,
+	increaseStalePeriodAndCheckRatesAndCache,
 	getRate,
 	updateCache,
 };
