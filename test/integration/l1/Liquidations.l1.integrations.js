182b34cd011d556ac16f4cb1d9277c42d4046f93
Lecky <leckylao@gmail.com>
2022-01-09 18:14:28 +1100

    Fix CI test - increase stake behaviour SNX mint - update setRate to deploy mockAggregator

--- test/integration/l1/Liquidations.l1.integrations.js
+++ test/integration/l1/Liquidations.l1.integrations.js
@@ -1,31 +1,9 @@
 const { bootstrapL1 } = require('../utils/bootstrap');
 const { itCanLiquidate } = require('../behaviors/liquidations.behavior');
-const { ethers } = require('hardhat');
-
-// Load Compiled
-const path = require('path');
-const {
-	constants: { BUILD_FOLDER },
-} = require('../../..');
-const buildPath = path.join(__dirname, '..', '..', '..', `${BUILD_FOLDER}`);
-const { loadCompiledFiles } = require('../../../publish/src/solidity');
-const { compiled } = loadCompiledFiles({ buildPath });
 
 describe('Liquidations (L1)', () => {
 	const ctx = this;
 	bootstrapL1({ ctx });
-
-	before(async () => {
-		const {
-			abi,
-			evm: {
-				bytecode: { object: bytecode },
-			},
-		} = compiled.MockAggregatorV2V3;
-		const MockAggregatorFactory = new ethers.ContractFactory(abi, bytecode, ctx.users.owner);
-		const MockAggregator = await MockAggregatorFactory.deploy();
-		ctx.contracts.MockAggregator = MockAggregator;
-	});
 
 	itCanLiquidate({ ctx });
 });
