1be5282e3d5fda26d41932fb1ad9dd260c2ea497
Lecky <leckylao@gmail.com>
2022-01-09 18:44:38 +1100

    Revert "Fix CI test"
    This reverts commit 182b34cd011d556ac16f4cb1d9277c42d4046f93.

--- test/integration/l2/Liquidations.l2.integration.js
+++ test/integration/l2/Liquidations.l2.integration.js
@@ -1,9 +1,31 @@
 const { bootstrapL2 } = require('../utils/bootstrap');
 const { itCanLiquidate } = require('../behaviors/liquidations.behavior');
+const { ethers } = require('hardhat');
+
+// Load Compiled
+const path = require('path');
+const {
+	constants: { BUILD_FOLDER },
+} = require('../../..');
+const buildPath = path.join(__dirname, '..', '..', '..', `${BUILD_FOLDER}`);
+const { loadCompiledFiles } = require('../../../publish/src/solidity');
+const { compiled } = loadCompiledFiles({ buildPath });
 
 describe('Liquidations (L2)', () => {
 	const ctx = this;
 	bootstrapL2({ ctx });
+
+	before(async () => {
+		const {
+			abi,
+			evm: {
+				bytecode: { object: bytecode },
+			},
+		} = compiled.MockAggregatorV2V3;
+		const MockAggregatorFactory = new ethers.ContractFactory(abi, bytecode, ctx.users.owner);
+		const MockAggregator = await MockAggregatorFactory.deploy();
+		ctx.contracts.MockAggregator = MockAggregator;
+	});
 
 	itCanLiquidate({ ctx });
 });
