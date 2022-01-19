32693f777353d416ff425ab890404151167c0fe9
artdgn <arthurdgn@gmail.com>
2022-01-19 17:24:51 +1100

    fix shadowing in empty futures market

--- contracts/EmptyFuturesMarketManager.sol
+++ contracts/EmptyFuturesMarketManager.sol
@@ -6,18 +6,20 @@ pragma solidity ^0.5.16;
 import "./interfaces/IFuturesMarketManager.sol";
 
 contract EmptyFuturesMarketManager is IFuturesMarketManager {
-    function markets(uint index, uint pageSize) external view returns (address[] memory markets) {
+    function markets(uint index, uint pageSize) external view returns (address[] memory) {
         index;
         pageSize;
-        return markets;
+        address[] memory _markets;
+        return _markets;
     }
 
     function numMarkets() external view returns (uint) {
         return 0;
     }
 
-    function allMarkets() external view returns (address[] memory markets) {
-        return markets;
+    function allMarkets() external view returns (address[] memory) {
+        address[] memory _markets;
+        return _markets;
     }
 
     function marketForAsset(bytes32 asset) external view returns (address) {
@@ -25,9 +27,10 @@ contract EmptyFuturesMarketManager is IFuturesMarketManager {
         return address(0);
     }
 
-    function marketsForAssets(bytes32[] calldata assets) external view returns (address[] memory markets) {
+    function marketsForAssets(bytes32[] calldata assets) external view returns (address[] memory) {
         assets;
-        return markets;
+        address[] memory _markets;
+        return _markets;
     }
 
     function totalDebt() external view returns (uint debt, bool isInvalid) {
