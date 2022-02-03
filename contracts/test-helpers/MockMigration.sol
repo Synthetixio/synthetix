5fd33db20762b548324fe680ee22d2a72cf4a906
Justin J. Moses <justinjmoses@gmail.com>
2022-02-03 15:58:24 -0600

    Removing legacy migrations

--- contracts/test-helpers/MockMigration.sol
+++ contracts/test-helpers/MockMigration.sol
@@ -6,8 +6,6 @@ import "../Owned.sol";
 contract MockMigration is BaseMigration {
     constructor(address _owner) public BaseMigration(_owner) {}
 
-    function canOnlyBeRunByDeployer() external onlyDeployer {}
-
     function acceptOwnership(address someContract) external {
         Owned(someContract).acceptOwnership();
     }
