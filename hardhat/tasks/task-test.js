43d8a9bc5257cdc9d524fdf91b820466f72cef18
Noah Litvin <noah.litvin@gmail.com>
2022-02-04 12:43:18 -0500

    alt config

--- hardhat/tasks/task-test.js
+++ hardhat/tasks/task-test.js
@@ -54,5 +54,10 @@ task('test')
 			hre.config.gasReporter.outputFile = gasOutputFile;
 		}
 
+		hre.config.mocha.reporter = 'mocha-junit-reporter';
+		hre.config.mocha.reporterOptions = {
+			mochaFile: '~/junit/test-results.[hash].xml',
+		};
+
 		await runSuper(taskArguments);
 	});
