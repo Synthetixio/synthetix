4d5e5fe25d1184b2a9ed1e76d3e70b3bda3bc05e
Daniel Beal <git@danb.email>
2022-02-09 09:50:42 -0800

    test fixes

--- test/publish/index.js
+++ test/publish/index.js
@@ -1174,9 +1174,10 @@ describe('publish scripts', () => {
 									.filter(([contract]) => !/^SynthetixBridge/.test(contract))
 									// Same applies to the owner relays
 									.filter(([contract]) => !/^OwnerRelay/.test(contract))
+									// same for external contracts
+									.filter(([contract]) => !/^ext:/.test(contract))
 									// Note: the VirtualSynth mastercopy is null-initialized and shouldn't be checked
 									.filter(([contract]) => !/^VirtualSynthMastercopy/.test(contract))
-									.filter(([contract]) => !/^SingleNetworkAggregatorDebtInfo/.test(contract))
 									.filter(([, { source }]) =>
 										sources[source].abi.find(({ name }) => name === 'resolver')
 									)
