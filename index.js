741406eac8505e68de5013e833d9004a99f5d95d
artdgn <arthurdgn@gmail.com>
2022-04-12 13:55:16 +1000

    update pause reason for futures deployments

--- index.js
+++ index.js
@@ -610,6 +610,7 @@ const getSuspensionReasons = ({ code = undefined } = {}) => {
 		6: 'Index Rebalance',
 		55: 'Circuit Breaker (Phase one)', // https://sips.synthetix.io/SIPS/sip-55
 		65: 'Decentralized Circuit Breaker (Phase two)', // https://sips.synthetix.io/SIPS/sip-65
+		80: 'Futures configuration', // pausing according to deployment configuration
 		99999: 'Emergency',
 	};
 
