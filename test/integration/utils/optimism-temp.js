8d8762518cff875fa17422c2fc85d8894d2f0c03
Justin J. Moses <justinjmoses@gmail.com>
2022-04-14 16:56:27 -0400

    Revert "TEMPAdding console error output"
    This reverts commit 57cc94712ba02c898299bc6f224bdfbda71ba1f7.

--- test/integration/utils/optimism-temp.js
+++ test/integration/utils/optimism-temp.js
@@ -223,7 +223,6 @@ function _printMessengerLog(log) {
 		const argValue = event.args[0];
 		console.log(chalk.gray(`> ${event.name}(${argName}:${argType} = ${argValue})`));
 	} catch (err) {
-		console.error('Error XXX', err);
 		console.error('could not parse messenger log:', log);
 	}
 }
