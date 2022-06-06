c579267ace79212b1e2e8b1b363ddd4d8df235a3
Mark Barrasso <4982406+barrasso@users.noreply.github.com>
2022-06-06 16:28:01 -0400

    SIP-235: Update Issuer Modifier (#1779)

--- contracts/Issuer.sol
+++ contracts/Issuer.sol
@@ -952,11 +952,10 @@ contract Issuer is Owned, MixinSystemSettings, IIssuer {
     }
 
     modifier onlyTrustedMinters() {
-        require(
-            msg.sender == resolver.getAddress(CONTRACT_SYNTHETIXBRIDGETOOPTIMISM) ||
-                msg.sender == resolver.getAddress(CONTRACT_SYNTHETIXBRIDGETOBASE),
-            "Issuer: Only trusted minters can perform this action"
-        );
+        address bridgeL1 = resolver.getAddress(CONTRACT_SYNTHETIXBRIDGETOOPTIMISM);
+        address bridgeL2 = resolver.getAddress(CONTRACT_SYNTHETIXBRIDGETOBASE);
+        require(msg.sender == bridgeL1 || msg.sender == bridgeL2, "Issuer: only trusted minters");
+        require(bridgeL1 == address(0) || bridgeL2 == address(0), "Issuer: one minter must be 0x0");
         _;
     }
 
