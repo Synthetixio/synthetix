2fc75a30ab236dc491b47ba79bb07b2bb86df637
artdgn <arthurdgn@gmail.com>
2022-06-24 12:29:48 +1000

    temp fix fork test setup

--- publish/src/commands/deploy/deploy-core.js
+++ publish/src/commands/deploy/deploy-core.js
@@ -6,7 +6,6 @@ const {
 	constants: { ZERO_ADDRESS },
 	defaults: { TEMP_OWNER_DEFAULT_DURATION },
 } = require('../../../..');
-const { toBytes32 } = require('../../../../index');
 
 module.exports = async ({
 	account,
@@ -137,7 +136,7 @@ module.exports = async ({
 	// get either previous address, or newly deployed address
 	const rewardEscrowV2FrozenAddress =
 		addressOf(rewardEscrowV2Frozen) ||
-		(await readProxyForResolver.getAddress(toBytes32('RewardEscrowV2Frozen')));
+		addressOf(await deployer.getExistingContract({ contract: 'RewardEscrowV2' }));
 
 	// SIP-252: storage contract for RewardEscrowV2
 	await deployer.deployContract({
