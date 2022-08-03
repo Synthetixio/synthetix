89f034b6713f18bc8f4753ad2262c43da567abd3
Mark Barrasso <4982406+barrasso@users.noreply.github.com>
2022-08-03 17:27:24 -0400

    complete tiaki release

--- contracts/migrations/Migration_Tiaki.sol
+++ contracts/migrations/Migration_Tiaki.sol
@@ -1,4 +1,3 @@
-
 pragma solidity ^0.5.16;
 
 import "../BaseMigration.sol";
@@ -22,7 +21,8 @@ contract Migration_Tiaki is BaseMigration {
     // https://etherscan.io/address/0x823bE81bbF96BEc0e25CA13170F5AaCb5B79ba83
     AddressResolver public constant addressresolver_i = AddressResolver(0x823bE81bbF96BEc0e25CA13170F5AaCb5B79ba83);
     // https://etherscan.io/address/0x9729415A58b210b2b8f69cFa5d6C3A7D68c9D217
-    ExchangeRatesWithDexPricing public constant exchangerates_i = ExchangeRatesWithDexPricing(0x9729415A58b210b2b8f69cFa5d6C3A7D68c9D217);
+    ExchangeRatesWithDexPricing public constant exchangerates_i =
+        ExchangeRatesWithDexPricing(0x9729415A58b210b2b8f69cFa5d6C3A7D68c9D217);
 
     // ----------------------------------
     // NEW CONTRACTS DEPLOYED TO BE ADDED
@@ -35,12 +35,15 @@ contract Migration_Tiaki is BaseMigration {
 
     function contractsRequiringOwnership() public pure returns (address[] memory contracts) {
         contracts = new address[](2);
-        contracts[0]= address(addressresolver_i);
-        contracts[1]= address(exchangerates_i);
+        contracts[0] = address(addressresolver_i);
+        contracts[1] = address(exchangerates_i);
     }
 
     function migrate() external onlyOwner {
-        require(ISynthetixNamedContract(new_ExchangeRates_contract).CONTRACT_NAME() == "ExchangeRatesWithDexPricing", "Invalid contract supplied for ExchangeRates");
+        require(
+            ISynthetixNamedContract(new_ExchangeRates_contract).CONTRACT_NAME() == "ExchangeRatesWithDexPricing",
+            "Invalid contract supplied for ExchangeRates"
+        );
 
         // ACCEPT OWNERSHIP for all contracts that require ownership to make changes
         acceptAll();
@@ -125,16 +128,17 @@ contract Migration_Tiaki is BaseMigration {
         }
     }
 
-    
     function addressresolver_importAddresses_0() internal {
         bytes32[] memory addressresolver_importAddresses_names_0_0 = new bytes32[](1);
         addressresolver_importAddresses_names_0_0[0] = bytes32("ExchangeRates");
         address[] memory addressresolver_importAddresses_destinations_0_1 = new address[](1);
         addressresolver_importAddresses_destinations_0_1[0] = address(new_ExchangeRates_contract);
-        addressresolver_i.importAddresses(addressresolver_importAddresses_names_0_0, addressresolver_importAddresses_destinations_0_1);
+        addressresolver_i.importAddresses(
+            addressresolver_importAddresses_names_0_0,
+            addressresolver_importAddresses_destinations_0_1
+        );
     }
 
-    
     function addressresolver_rebuildCaches_1() internal {
         MixinResolver[] memory addressresolver_rebuildCaches_destinations_1_0 = new MixinResolver[](14);
         addressresolver_rebuildCaches_destinations_1_0[0] = MixinResolver(0x0e5fe1b05612581576e9A3dB048416d0B1E3C425);
