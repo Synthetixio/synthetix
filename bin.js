4e742a9718b11bc033998b0e8face2f3214e07a2
Leonardo Massazza <lmassazza@gmail.com>
2022-08-03 16:17:16 +0100

    first commit - add parse command to bin.js

--- bin.js
+++ bin.js
@@ -61,6 +61,53 @@ program
 	});
 
 program
+	.command('multisenddecode <txsdata> [target]')
+	.description('Decode a data payload from a gnosis multi-send staged to Synthetix contracts')
+	.option('-n, --network <value>', 'The network to use', x => x.toLowerCase(), 'mainnet')
+	.option('-z, --use-ovm', 'Target deployment for the OVM (Optimism).')
+	.action(async (txsdata, target, { network, useOvm }) => {
+		if (txsdata.length <= 2) {
+			console.log('data too short');
+		}
+		const cleanData = txsdata.toLowerCase().startsWith('0x')
+			? txsdata.slice(2).toLowerCase()
+			: txsdata.toLowerCase();
+
+		const splitByLen = (s, len) => [s.slice(0, len), s.slice(len)];
+
+		let parts;
+		let rest = cleanData;
+		while (rest.length > 20) {
+			// operation type
+			parts = splitByLen(rest, 2);
+			const operationType = parts[0] === '00' ? 'Call' : 'DelegateCall';
+
+			// destination
+			parts = splitByLen(parts[1], 40);
+			const to = '0x' + parts[0];
+
+			// value
+			parts = splitByLen(parts[1], 64);
+			const txValue = parts[0];
+			const valueDecimal = parseInt(txValue, 16);
+
+			// data Len
+			parts = splitByLen(parts[1], 64);
+			const dataLen = parts[0];
+
+			const dataLenDecimal = parseInt(dataLen, 16);
+
+			// data Len
+			parts = splitByLen(parts[1], dataLenDecimal * 2);
+			const data = dataLenDecimal.toString(16) + parts[0];
+
+			console.log(`Target: ${to} Calltype: ${operationType} Value: ${valueDecimal}`);
+			console.log(util.inspect(decode({ network, data, target, useOvm }), false, null, true));
+			rest = parts[1];
+		}
+	});
+
+program
 	.command('networks')
 	.description('Get networks')
 	.action(async () => {
