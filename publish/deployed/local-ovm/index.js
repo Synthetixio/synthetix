7e5b1dd4f66d1724ab593ff4f870e29bb6045716
joey <joey@cc.snxdao.io>
2023-02-08 12:14:55 +1100

    Add getPerpsMarkets

--- publish/deployed/local-ovm/index.js
+++ publish/deployed/local-ovm/index.js
@@ -5,4 +5,5 @@ module.exports = {
 	feeds: require('./feeds.json'),
 	offchainFeeds: require('./offchain-feeds.json'),
 	futuresMarkets: require('./futures-markets.json'),
+	perpsv2Markets: require('./perpsv2-markets.json'),
 };
