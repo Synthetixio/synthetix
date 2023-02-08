0a618bd824642c396621f6f40222e6ecf7f1de21
joey <joey@cc.snxdao.io>
2023-02-08 15:08:49 +1100

    Add missing index file for local deployment - This is used for one of the smoke tests in the closure keeper. This smoke test have been failing for a while

---
+++ publish/deployed/local/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+	deployment: require('./deployment.json'),
+	synths: require('./synths.json'),
+	rewards: require('./rewards.json'),
+	feeds: require('./feeds.json'),
+	offchainFeeds: require('./offchain-feeds.json'),
+	futuresMarkets: require('./futures-markets.json'),
+	perpsv2Markets: require('./perpsv2-markets.json'),
+};
