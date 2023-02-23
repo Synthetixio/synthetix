ba79f44ab9030fcc251e5c9ad6cf77e31af03717
Leonardo Massazza <lmassazza@gmail.com>
2023-02-23 10:28:59 -0300

    set linked state points

--- contracts/PerpsV2MarketStateLegacyR1.sol
+++ contracts/PerpsV2MarketStateLegacyR1.sol
@@ -9,15 +9,19 @@ import "./StateShared.sol";
 // Libraries
 import "./AddressSetLib.sol";
 
-// https://docs.synthetix.io/contracts/source/contracts/PerpsV2MarketState
-// solhint-disable-next-line max-states-count
-contract PerpsV2MarketState is Owned, StateShared, IPerpsV2MarketBaseTypes {
-    using AddressSetLib for AddressSetLib.AddressSet;
+/**
+ Legacy State holding historical data. Old PerpsV2MarketState.sol
+ Once this contract is linked to the next generation of PerpsV2MarketState, the data is not updateable anymore. 
+ The only contract enabled to operate (if needed) on the data is the next generation PerpsV2MarketState, 
+ in order to achieve that, the new State is the only address included as associated contract.
 
-    // Legacy state link
-    IPerpsV2MarketBaseTypes public legacyState;
-    bool public legacyLinked;
-    uint public legacyFundinSequenceOffset;
+ Atomic data is copied at `migration` time.
+ PerpsV2MarketState ---(consumes)--> PerpsV2MarketStateLegacyR1 
+ */
+
+// https://docs.synthetix.io/contracts/source/contracts/PerpsV2MarketStateLegacyR1
+contract PerpsV2MarketStateLegacyR1 is Owned, StateShared, IPerpsV2MarketBaseTypes {
+    using AddressSetLib for AddressSetLib.AddressSet;
 
     // The market identifier in the perpsV2 system (manager + settings). Multiple markets can co-exist
     // for the same asset in order to allow migrations.
@@ -52,7 +56,7 @@ contract PerpsV2MarketState is Owned, StateShared, IPerpsV2MarketBaseTypes {
      * since the position was confirmed and multiply it by the position size.
      */
     uint32 public fundingLastRecomputed;
-    int128[] internal _fundingSequence;
+    int128[] public fundingSequence;
 
     /*
      * The funding rate last time it was recomputed. The market funding rate floats and requires the previously
@@ -64,7 +68,7 @@ contract PerpsV2MarketState is Owned, StateShared, IPerpsV2MarketBaseTypes {
      * Each user's position. Multiple positions can always be merged, so each user has
      * only have one position at a time.
      */
-    mapping(address => Position) internal _positions;
+    mapping(address => Position) public positions;
 
     // The set of all addresses (positions) .
     AddressSetLib.AddressSet internal _positionAddresses;
@@ -76,18 +80,13 @@ contract PerpsV2MarketState is Owned, StateShared, IPerpsV2MarketBaseTypes {
     uint64 internal _nextPositionId = 1;
 
     /// @dev Holds a mapping of accounts to orders. Only one order per account is supported
-    mapping(address => DelayedOrder) internal _delayedOrders;
-
-    /// @dev Holds a mapping of accounts to flagger address to flag an account. Only one order per account is supported
-    mapping(address => address) public positionFlagger;
-    AddressSetLib.AddressSet internal _flaggedAddresses;
+    mapping(address => DelayedOrder) public delayedOrders;
 
     constructor(
         address _owner,
         address[] memory _associatedContracts,
         bytes32 _baseAsset,
-        bytes32 _marketKey,
-        address _legacyState
+        bytes32 _marketKey
     ) public Owned(_owner) StateShared(_associatedContracts) {
         baseAsset = _baseAsset;
         marketKey = _marketKey;
@@ -96,20 +95,6 @@ contract PerpsV2MarketState is Owned, StateShared, IPerpsV2MarketBaseTypes {
         fundingSequence.push(0);
 
         fundingRateLastRecomputed = 0;
-
-        // Set legacyState
-        legacyState = IPerpsV2MarketBaseTypes(_legacyState);
-    }
-
-    function linkLegacyState(address _legacyState) external onlyOwner {
-        // Confirm same asset/market key
-        // TODO
-        // copy atomic values
-        // TODO
-        // get fundingSequence offset
-        // TODO
-        // set legacyConfigured
-        // TODO
     }
 
     function entryDebtCorrection() external view returns (int128) {
@@ -120,26 +105,8 @@ contract PerpsV2MarketState is Owned, StateShared, IPerpsV2MarketBaseTypes {
         return _nextPositionId;
     }
 
-    function fundingSequence(uint index) external view returns (uint) {
-        // TODO check legacy
-
-        return _fundingSequence[index];
-    }
-
     function fundingSequenceLength() external view returns (uint) {
-        // TODO check legacy
-
-        return legacyFundinSequenceOffset + fundingSequence.length;
-    }
-
-    function isFlagged(address account) external view returns (bool) {
-        return positionFlagger[account] != address(0);
-    }
-
-    function positions(address account) external view returns (Position) {
-        // TODO check legacy
-
-        return _positions[account];
+        return fundingSequence.length;
     }
 
     function getPositionAddressesPage(uint index, uint pageSize)
@@ -148,15 +115,7 @@ contract PerpsV2MarketState is Owned, StateShared, IPerpsV2MarketBaseTypes {
         onlyAssociatedContracts
         returns (address[] memory)
     {
-        // TODO check legacy
-
         return _positionAddresses.getPage(index, pageSize);
-    }
-
-    function delayedOrders(address account) external view returns (DelayedOrder) {
-        // TODO check legacy
-
-        return _delayedOrders[account];
     }
 
     function getDelayedOrderAddressesPage(uint index, uint pageSize)
@@ -165,80 +124,53 @@ contract PerpsV2MarketState is Owned, StateShared, IPerpsV2MarketBaseTypes {
         onlyAssociatedContracts
         returns (address[] memory)
     {
-        // TODO check legacy
-
         return _delayedOrderAddresses.getPage(index, pageSize);
     }
 
-    function getFlaggedAddressesPage(uint index, uint pageSize)
-        external
-        view
-        onlyAssociatedContracts
-        returns (address[] memory)
-    {
-        // TODO check legacy
-
-        return _flaggedAddresses.getPage(index, pageSize);
-    }
-
-    function getPositionAddressesLength() external view returns (uint) {
-        // TODO check legacy
-
+    function getPositionAddressesLength() external view onlyAssociatedContracts returns (uint) {
         return _positionAddresses.elements.length;
     }
 
-    function getDelayedOrderAddressesLength() external view returns (uint) {
-        // TODO check legacy
-
+    function getDelayedOrderAddressesLength() external view onlyAssociatedContracts returns (uint) {
         return _delayedOrderAddresses.elements.length;
     }
 
-    function getFlaggedAddressesLength() external view returns (uint) {
-        return _flaggedAddresses.elements.length;
-    }
-
-    function setMarketKey(bytes32 _marketKey) external onlyLegacyLinked onlyAssociatedContracts {
+    function setMarketKey(bytes32 _marketKey) external onlyAssociatedContracts {
         require(marketKey == bytes32(0) || _marketKey == marketKey, "Cannot change market key");
         marketKey = _marketKey;
     }
 
-    function setBaseAsset(bytes32 _baseAsset) external onlyLegacyLinked onlyAssociatedContracts {
+    function setBaseAsset(bytes32 _baseAsset) external onlyAssociatedContracts {
         require(baseAsset == bytes32(0) || _baseAsset == baseAsset, "Cannot change base asset");
         baseAsset = _baseAsset;
     }
 
-    function setMarketSize(uint128 _marketSize) external onlyLegacyLinked onlyAssociatedContracts {
+    function setMarketSize(uint128 _marketSize) external onlyAssociatedContracts {
         marketSize = _marketSize;
     }
 
-    function setEntryDebtCorrection(int128 entryDebtCorrection) external onlyLegacyLinked onlyAssociatedContracts {
+    function setEntryDebtCorrection(int128 entryDebtCorrection) external onlyAssociatedContracts {
         _entryDebtCorrection = entryDebtCorrection;
     }
 
-    function setNextPositionId(uint64 nextPositionId) external onlyLegacyLinked onlyAssociatedContracts {
+    function setNextPositionId(uint64 nextPositionId) external onlyAssociatedContracts {
         _nextPositionId = nextPositionId;
     }
 
-    function setMarketSkew(int128 _marketSkew) external onlyLegacyLinked onlyAssociatedContracts {
+    function setMarketSkew(int128 _marketSkew) external onlyAssociatedContracts {
         marketSkew = _marketSkew;
     }
 
-    function setFundingLastRecomputed(uint32 lastRecomputed) external onlyLegacyLinked onlyAssociatedContracts {
+    function setFundingLastRecomputed(uint32 lastRecomputed) external onlyAssociatedContracts {
         fundingLastRecomputed = lastRecomputed;
     }
 
-    function pushFundingSequence(int128 _fundingSequence) external onlyLegacyLinked onlyAssociatedContracts {
-        // TODO check legacy
-
+    function pushFundingSequence(int128 _fundingSequence) external onlyAssociatedContracts {
         fundingSequence.push(_fundingSequence);
     }
 
     // TODO: Perform this update when maxFundingVelocity and skewScale are modified.
-    function setFundingRateLastRecomputed(int128 _fundingRateLastRecomputed)
-        external
-        onlyLegacyLinked
-        onlyAssociatedContracts
-    {
+    function setFundingRateLastRecomputed(int128 _fundingRateLastRecomputed) external onlyAssociatedContracts {
         fundingRateLastRecomputed = _fundingRateLastRecomputed;
     }
 
@@ -259,9 +191,7 @@ contract PerpsV2MarketState is Owned, StateShared, IPerpsV2MarketBaseTypes {
         uint128 margin,
         uint128 lastPrice,
         int128 size
-    ) external onlyLegacyLinked onlyAssociatedContracts {
-        // TODO check legacy
-
+    ) external onlyAssociatedContracts {
         positions[account] = Position(id, lastFundingIndex, margin, lastPrice, size);
         _positionAddresses.add(account);
     }
@@ -290,9 +220,7 @@ contract PerpsV2MarketState is Owned, StateShared, IPerpsV2MarketBaseTypes {
         uint256 executableAtTime,
         uint256 intentionTime,
         bytes32 trackingCode
-    ) external onlyLegacyLinked onlyAssociatedContracts {
-        // TODO check legacy
-
+    ) external onlyAssociatedContracts {
         delayedOrders[account] = DelayedOrder(
             isOffchain,
             sizeDelta,
@@ -312,38 +240,17 @@ contract PerpsV2MarketState is Owned, StateShared, IPerpsV2MarketBaseTypes {
      * @dev Only the associated contract may call this.
      * @param account The account whose position should be deleted.
      */
-    function deletePosition(address account) external onlyLegacyLinked onlyAssociatedContracts {
-        // TODO check legacy
-
+    function deletePosition(address account) external onlyAssociatedContracts {
         delete positions[account];
         if (_positionAddresses.contains(account)) {
             _positionAddresses.remove(account);
         }
     }
 
-    function deleteDelayedOrder(address account) external onlyLegacyLinked onlyAssociatedContracts {
-        // TODO check legacy
-
+    function deleteDelayedOrder(address account) external onlyAssociatedContracts {
         delete delayedOrders[account];
         if (_delayedOrderAddresses.contains(account)) {
             _delayedOrderAddresses.remove(account);
         }
-    }
-
-    function flag(address account, address flagger) external onlyLegacyLinked onlyAssociatedContracts {
-        positionFlagger[account] = flagger;
-        _flaggedAddresses.add(account);
-    }
-
-    function unflag(address account) external onlyLegacyLinked onlyAssociatedContracts {
-        delete positionFlagger[account];
-        if (_flaggedAddresses.contains(account)) {
-            _flaggedAddresses.remove(account);
-        }
-    }
-
-    modifier onlyLegacyLinked() {
-        require(legacyLinked, "State not linked");
-        _;
     }
 }
