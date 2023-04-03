be174bbb5d9d2be6d3f255d0ffe54f96f8009811
Leonardo Massazza <lmassazza@gmail.com>
2023-04-03 13:54:21 -0300

    fix typo

--- publish/src/commands/deploy/deploy-perpsv2.js
+++ publish/src/commands/deploy/deploy-perpsv2.js
@@ -323,7 +323,7 @@ const deployPerpsV2Markets = async ({
 			runStep,
 			futuresMarketManager,
 			proxies: [deployedMarketProxy.target.address],
-			someUpdated: someImplementationUpdated,
+			someImplementationUpdated,
 		});
 
 		await importAddresses({
