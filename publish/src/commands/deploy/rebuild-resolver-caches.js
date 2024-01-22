ccbed96452e675f4c4030f74d1d00112f0f920c3
kaleb <62237347+kaleb-keny@users.noreply.github.com>
2024-01-22 21:33:22 +0100

    sip-2044-2045 (#2188)
    * sip-2044-2045
    
    initial commit
    
    * fix releases.json
    
    * lower ordi mmv
    
    * add missing assets
    
    * fix hh fork missing log
    
    ---------
    
    Co-authored-by: Leonardo Massazza <lmassazza@gmail.com>

--- publish/src/commands/deploy/rebuild-resolver-caches.js
+++ publish/src/commands/deploy/rebuild-resolver-caches.js
@@ -78,9 +78,7 @@ module.exports = async ({
 					'Warning: Cannot fetch logs on this network. Known limitation on Tenderly Fork - cannot search back greater than 1k blocks'
 				)
 			);
-		} else if (
-			/eth_getLogs and eth_newFilter are limited to a 10000 blocks range/.test(err.message)
-		) {
+		} else if (/eth_getLogs is limited to a 10,000 range/.test(err.message)) {
 			console.log(
 				yellow.bold(
 					'Warning: Cannot fetch logs on this network. Known limitation on hh fork - cannot search back greater than 10k blocks'
