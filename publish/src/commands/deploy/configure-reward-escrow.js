5643a7fc3c032c2dcbbe5a621b1dc964218516a5
Leonardo Massazza <lmassazza@gmail.com>
2024-01-26 13:40:03 -0300

    restore skipped step

--- publish/src/commands/deploy/configure-reward-escrow.js
+++ publish/src/commands/deploy/configure-reward-escrow.js
@@ -1,20 +1,19 @@
 'use strict';
 
 const { gray } = require('chalk');
-const { toBytes32 } = require('../../../..');
-// const {
-// 	constants: { ZERO_ADDRESS },
-// } = require('../../../..');
+const {
+	constants: { ZERO_ADDRESS },
+} = require('../../../..');
 
 module.exports = async ({ addressOf, deployer, runStep }) => {
-	console.log(gray(`\n------ CONFIGURE REWARD ESCROW V2 (MIGRATION) [SKIPPED PARTIALLY] ------\n`));
+	console.log(gray(`\n------ CONFIGURE REWARD ESCROW V2 (MIGRATION) ------\n`));
 
 	const {
-		AddressResolver,
-		// 	RewardEscrowV2,
-		// 	RewardsDistribution,
-		// 	// Synthetix,
-		// RewardEscrowV2Storage,
+		// AddressResolver,
+		RewardEscrowV2,
+		RewardsDistribution,
+		// Synthetix,
+		RewardEscrowV2Storage,
 		RewardEscrowV2Frozen,
 	} = deployer.deployedContracts;
 
@@ -23,32 +22,32 @@ module.exports = async ({ addressOf, deployer, runStep }) => {
 	const frozenOrPreviousEscrow =
 		RewardEscrowV2Frozen || (await deployer.getExistingContract({ contract: 'RewardEscrowV2' }));
 
-	// close account merging on previous contract
-	// this breaks account merging
-	await runStep({
-		contract: 'RewardEscrowV2Frozen',
-		target: frozenOrPreviousEscrow,
-		read: 'accountMergingDuration',
-		expected: input => input === '0',
-		write: 'setAccountMergingDuration',
-		writeArg: 0,
-		comment: 'Ensure that RewardEscrowV2Frozen account merging is closed',
-	});
+	// // close account merging on previous contract
+	// // this breaks account merging
+	// await runStep({
+	// 	contract: 'RewardEscrowV2Frozen',
+	// 	target: frozenOrPreviousEscrow,
+	// 	read: 'accountMergingDuration',
+	// 	expected: input => input === '0',
+	// 	write: 'setAccountMergingDuration',
+	// 	writeArg: 0,
+	// 	comment: 'Ensure that RewardEscrowV2Frozen account merging is closed',
+	// });
 
-	// set frozen address entry for migrating balances
-	// this breaks creating entries (since SNX cannot be transferred)
-	// note that FeePool will still be able to create entries on the old contract up to the point
-	// its resolver cache is rebuilt (if this is not done atomcally in a migration contract)
-	await runStep({
-		contract: 'AddressResolver',
-		target: AddressResolver,
-		read: 'getAddress',
-		readArg: [toBytes32('RewardEscrowV2Frozen')],
-		expected: input => input === addressOf(frozenOrPreviousEscrow),
-		write: 'importAddresses',
-		writeArg: [[toBytes32('RewardEscrowV2Frozen')], [addressOf(frozenOrPreviousEscrow)]],
-		comment: 'Ensure that RewardEscrowV2Frozen is in the address resolver',
-	});
+	// // set frozen address entry for migrating balances
+	// // this breaks creating entries (since SNX cannot be transferred)
+	// // note that FeePool will still be able to create entries on the old contract up to the point
+	// // its resolver cache is rebuilt (if this is not done atomcally in a migration contract)
+	// await runStep({
+	// 	contract: 'AddressResolver',
+	// 	target: AddressResolver,
+	// 	read: 'getAddress',
+	// 	readArg: [toBytes32('RewardEscrowV2Frozen')],
+	// 	expected: input => input === addressOf(frozenOrPreviousEscrow),
+	// 	write: 'importAddresses',
+	// 	writeArg: [[toBytes32('RewardEscrowV2Frozen')], [addressOf(frozenOrPreviousEscrow)]],
+	// 	comment: 'Ensure that RewardEscrowV2Frozen is in the address resolver',
+	// });
 
 	// // move SNX balances if needed
 	// // this breaks vesting
@@ -66,41 +65,41 @@ module.exports = async ({ addressOf, deployer, runStep }) => {
 	// 	);
 	// }
 
-	// // set state ownership to the rew escrow contract
-	// // this enables new contract to make storage writes
-	// await runStep({
-	// 	contract: 'RewardEscrowV2Storage',
-	// 	target: RewardEscrowV2Storage,
-	// 	read: 'associatedContract',
-	// 	expected: input => input === addressOf(RewardEscrowV2),
-	// 	write: 'setAssociatedContract',
-	// 	writeArg: addressOf(RewardEscrowV2),
-	// 	comment: 'Ensure that RewardEscrowV2 contract is allowed to write to RewardEscrowV2Storage',
-	// });
+	// set state ownership to the rew escrow contract
+	// this enables new contract to make storage writes
+	await runStep({
+		contract: 'RewardEscrowV2Storage',
+		target: RewardEscrowV2Storage,
+		read: 'associatedContract',
+		expected: input => input === addressOf(RewardEscrowV2),
+		write: 'setAssociatedContract',
+		writeArg: addressOf(RewardEscrowV2),
+		comment: 'Ensure that RewardEscrowV2 contract is allowed to write to RewardEscrowV2Storage',
+	});
 
-	// // set the fallback (frozen) for storage contract
-	// // this can only happen once, as this contract is immutable
-	// // This step is performed last because beyond this point any new entries on previous contract will be ignored
-	// // Note: added to RewardEscrowV2Storage to non-upgradable.json after the Aspidiske release.
-	// await runStep({
-	// 	contract: 'RewardEscrowV2Storage',
-	// 	target: RewardEscrowV2Storage,
-	// 	read: 'fallbackRewardEscrow',
-	// 	expected: input => input !== ZERO_ADDRESS, // only configure if not configured
-	// 	write: 'setFallbackRewardEscrow',
-	// 	writeArg: addressOf(frozenOrPreviousEscrow),
-	// 	comment:
-	// 		'Ensure that RewardEscrowV2Storage contract is initialized with address of RewardEscrowV2Frozen',
-	// });
+	// set the fallback (frozne) for storage contract
+	// this can only happen once, as this contract is immutable
+	// This step is performed last because beyond this point any new entries on previous contract will be ignored
+	// Note: added to RewardEscrowV2Storage to non-upgradable.json after the Aspidiske release.
+	await runStep({
+		contract: 'RewardEscrowV2Storage',
+		target: RewardEscrowV2Storage,
+		read: 'fallbackRewardEscrow',
+		expected: input => input !== ZERO_ADDRESS, // only configure if not configured
+		write: 'setFallbackRewardEscrow',
+		writeArg: addressOf(frozenOrPreviousEscrow),
+		comment:
+			'Ensure that RewardEscrowV2Storage contract is initialized with address of RewardEscrowV2Frozen',
+	});
 
-	// // RewardEscrow on RewardsDistribution should be set to new RewardEscrowV2
-	// await runStep({
-	// 	contract: 'RewardsDistribution',
-	// 	target: RewardsDistribution,
-	// 	read: 'rewardEscrow',
-	// 	expected: input => input === addressOf(RewardEscrowV2),
-	// 	write: 'setRewardEscrow',
-	// 	writeArg: addressOf(RewardEscrowV2),
-	// 	comment: 'Ensure the RewardsDistribution can read the RewardEscrowV2 address',
-	// });
+	// RewardEscrow on RewardsDistribution should be set to new RewardEscrowV2
+	await runStep({
+		contract: 'RewardsDistribution',
+		target: RewardsDistribution,
+		read: 'rewardEscrow',
+		expected: input => input === addressOf(RewardEscrowV2),
+		write: 'setRewardEscrow',
+		writeArg: addressOf(RewardEscrowV2),
+		comment: 'Ensure the RewardsDistribution can read the RewardEscrowV2 address',
+	});
 };
