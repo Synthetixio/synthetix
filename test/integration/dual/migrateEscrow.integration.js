5775edc66dae211c0348b0cc76862ad2a5687379
meb <4982406+barrasso@users.noreply.github.com>
2024-02-16 14:35:26 -0500

    fix int dual tests

--- test/integration/dual/migrateEscrow.integration.js
+++ test/integration/dual/migrateEscrow.integration.js
@@ -42,6 +42,7 @@ describe('migrateEscrow() integration tests (L1, L2)', () => {
 
 		before('create and append escrow entries', async () => {
 			user = ctx.l1.users.owner;
+			await RewardEscrowV2.connect(user).setPermittedEscrowCreator(user.address, true);
 
 			escrowEntriesData = await appendEscrows({
 				ctx: ctx.l1,
