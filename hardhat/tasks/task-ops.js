60366f0553c4d9a3295b366b79ef048d503612d4
meb <4982406+barrasso@users.noreply.github.com>
2024-02-29 20:43:47 -0500

    Use commandSync

--- hardhat/tasks/task-ops.js
+++ hardhat/tasks/task-ops.js
@@ -153,9 +153,9 @@ function _build({ opsPath, opsCommit, opsBranch }) {
 
 	// needed options for execa.sync https://github.com/sindresorhus/execa/issues/473
 	const pnpmOpts = { stdout: 'inherit', stderr: 'inherit', shell: true, cwd: opsPath };
-	execa.sync('', ['', `pnpm install `], pnpmOpts);
+	execa.commandSync('pnpm install', pnpmOpts);
 	console.log(gray('  build'));
-	execa.sync('', ['', `pnpm build `], pnpmOpts);
+	execa.commandSync('pnpm build', pnpmOpts);
 }
 
 function _buildOps({ opsPath }) {
