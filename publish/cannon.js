a40e2c65eef85e00d8e7f644897d4aa4cfdffaa4
dbeal <git@danb.email>
2024-03-19 00:08:11 +0900

    support for latest version of cannon (#2222)
    * support for latest version of cannon
    
    * fix lint
    
    ---------
    
    Co-authored-by: meb <4982406+barrasso@users.noreply.github.com>

--- publish/cannon.js
+++ publish/cannon.js
@@ -64,15 +64,18 @@ async function deploy(runtime, networkVariant) {
 	const buildPath = path.join(__dirname, '..', synthetix.constants.BUILD_FOLDER);
 
 	// get the signer that we want to have for the deployer
-	let signer = await runtime.getDefaultSigner({});
+	let cannonSigner = await runtime.getDefaultSigner({});
 	try {
 		// if cannon can give us the signer for the owner address, we should use that
 		const ownerAddress = synthetix.getUsers({ network, useOvm, user: 'owner' }).address;
-		signer = await runtime.getSigner(ownerAddress);
+		cannonSigner = await runtime.getSigner(ownerAddress);
 	} catch (err) {
 		// otherwise we want to use the cannon default signer, which is set above
 		console.log(err);
 	}
+
+	const provider = new hre.ethers.providers.Web3Provider(runtime.provider);
+	const signer = provider.getSigner(cannonSigner.address);
 
 	await deployInstance({
 		addNewSynths: true,
@@ -80,7 +83,7 @@ async function deploy(runtime, networkVariant) {
 		useOvm,
 		network,
 		freshDeploy: networkVariant.startsWith('local'),
-		provider: runtime.provider,
+		provider,
 		signer,
 	});
 
