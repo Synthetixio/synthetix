5bcb639ac2cc4fdfd89ebb81855258ef68dad4f3
meb <4982406+barrasso@users.noreply.github.com>
2024-03-28 13:53:40 -0400

    update Issuer and DynamicSynthRedeemer

--- contracts/DynamicSynthRedeemer.sol
+++ contracts/DynamicSynthRedeemer.sol
@@ -9,7 +9,6 @@ import "./interfaces/IDynamicSynthRedeemer.sol";
 import "./SafeDecimalMath.sol";
 
 // Internal references
-import "./interfaces/IERC20.sol";
 import "./interfaces/IIssuer.sol";
 import "./interfaces/IExchangeRates.sol";
 
@@ -18,30 +17,25 @@ contract DynamicSynthRedeemer is Owned, IDynamicSynthRedeemer, MixinResolver {
 
     bytes32 public constant CONTRACT_NAME = "DynamicSynthRedeemer";
 
-    bytes32 private constant CONTRACT_ISSUER = "Issuer";
-    bytes32 private constant CONTRACT_SYNTHSUSD = "SynthsUSD";
-    bytes32 private constant CONTRACT_EXRATES = "ExchangeRates";
-
-    // Rate applied to chainlink price for redemptions
     uint public discountRate;
+
+    bytes32 internal constant sUSD = "sUSD";
+
+    bytes32 private constant CONTRACT_ISSUER = "Issuer";
+    bytes32 private constant CONTRACT_EXRATES = "ExchangeRates";
 
     constructor(address _owner, address _resolver) public Owned(_owner) MixinResolver(_resolver) {
         discountRate = SafeDecimalMath.unit();
     }
 
     function resolverAddressesRequired() public view returns (bytes32[] memory addresses) {
-        addresses = new bytes32[](3);
+        addresses = new bytes32[](2);
         addresses[0] = CONTRACT_ISSUER;
-        addresses[1] = CONTRACT_SYNTHSUSD;
-        addresses[2] = CONTRACT_EXRATES;
+        addresses[1] = CONTRACT_EXRATES;
     }
 
     function issuer() internal view returns (IIssuer) {
         return IIssuer(requireAndGetAddress(CONTRACT_ISSUER));
-    }
-
-    function sUSD() internal view returns (IERC20) {
-        return IERC20(requireAndGetAddress(CONTRACT_SYNTHSUSD));
     }
 
     function exchangeRates() internal view returns (IExchangeRates) {
@@ -52,10 +46,10 @@ contract DynamicSynthRedeemer is Owned, IDynamicSynthRedeemer, MixinResolver {
         return discountRate;
     }
 
-    function setDiscountRate(uint newRate) external onlyOwner {
-        require(newRate >= 0, "Invalid rate");
-        discountRate = newRate;
-        emit DiscountRateUpdated(discountRate);
+    function setDiscountRate(uint _newRate) external onlyOwner {
+        require(_newRate >= 0, "Invalid rate");
+        discountRate = _newRate;
+        emit DiscountRateUpdated(_newRate);
     }
 
     function redeemAll(IERC20[] calldata synthProxies, bytes32[] calldata currencyKeys) external {
@@ -84,12 +78,13 @@ contract DynamicSynthRedeemer is Owned, IDynamicSynthRedeemer, MixinResolver {
         uint amountOfSynth,
         bytes32 currencyKey
     ) internal {
+        // Discount rate applied to chainlink price for dynamic redemptions
         uint rateToRedeem = exchangeRates().rateForCurrency(currencyKey).multiplyDecimalRound(discountRate);
         require(rateToRedeem > 0, "Synth not redeemable");
         require(amountOfSynth > 0, "No balance of synth to redeem");
         issuer().burnForRedemption(address(synthProxy), msg.sender, amountOfSynth);
         uint amountInsUSD = amountOfSynth.multiplyDecimal(rateToRedeem);
-        sUSD().transfer(msg.sender, amountInsUSD);
+        issuer().issueSynthsWithoutDebt(sUSD, msg.sender, amountInsUSD);
         emit SynthRedeemed(address(synthProxy), msg.sender, amountOfSynth, amountInsUSD);
     }
 
