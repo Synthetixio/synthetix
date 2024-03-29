a9108093cf312f680314734ab77484303c9bdc19
meb <4982406+barrasso@users.noreply.github.com>
2024-03-29 13:43:03 -0400

    update snapshot script for ovm forks

--- publish/src/commands/deploy/take-debt-snapshot-when-required.js
+++ publish/src/commands/deploy/take-debt-snapshot-when-required.js
@@ -17,93 +17,106 @@ module.exports = async ({
 }) => {
 	const { DebtCache } = deployer.deployedContracts;
 
-	console.log(gray(`\n------ CHECKING DEBT CACHE ------\n`));
+	if (!useOvm && !useFork) {
+		console.log(gray(`\n------ CHECKING DEBT CACHE ------\n`));
 
-	const refreshSnapshotIfPossible = async (wasInvalid, isInvalid, force = false) => {
-		const validityChanged = wasInvalid !== isInvalid;
+		const refreshSnapshotIfPossible = async (wasInvalid, isInvalid, force = false) => {
+			const validityChanged = wasInvalid !== isInvalid;
 
-		if (force || validityChanged) {
-			console.log(yellow(`Refreshing debt snapshot...`));
-			await runStep({
-				gasLimit: 12.0e6,
-				contract: 'DebtCache',
-				target: DebtCache,
-				write: 'takeDebtSnapshot',
-				writeArg: [],
-				publiclyCallable: true, // does not require owner
-				skipSolidity: true, // should not be in an upgrade contract - it'll exceed the block gas limit
-			});
-		} else if (!validityChanged) {
-			console.log(
-				red('⚠⚠⚠ WARNING: Deployer attempted to refresh the debt cache, but it cannot be.')
-			);
-		}
-	};
+			if (force || validityChanged) {
+				console.log(yellow(`Refreshing debt snapshot...`));
+				await runStep({
+					gasLimit: 12.0e6,
+					contract: 'DebtCache',
+					target: DebtCache,
+					write: 'takeDebtSnapshot',
+					writeArg: [],
+					publiclyCallable: true, // does not require owner
+					skipSolidity: true, // should not be in an upgrade contract - it'll exceed the block gas limit
+				});
+			} else if (!validityChanged) {
+				console.log(
+					red('⚠⚠⚠ WARNING: Deployer attempted to refresh the debt cache, but it cannot be.')
+				);
+			}
+		};
 
-	const checkSnapshot = async ({ useFork }) => {
-		const cacheInfo = await DebtCache.cacheInfo();
-		const currentDebt = await DebtCache.currentDebt();
+		const checkSnapshot = async ({ useFork }) => {
+			const cacheInfo = await DebtCache.cacheInfo();
+			const currentDebt = await DebtCache.currentDebt();
 
-		// Check if the snapshot is stale and can be fixed.
-		if (cacheInfo.isStale && !currentDebt.anyRateIsInvalid) {
-			console.log(yellow('Debt snapshot is stale, and can be refreshed.'));
-			await refreshSnapshotIfPossible(
-				cacheInfo.isInvalid,
-				currentDebt.anyRateIsInvalid,
-				cacheInfo.isStale
-			);
-			return true;
-		}
-
-		// Otherwise, if the rates are currently valid,
-		// we might still need to take a snapshot due to invalidity or deviation.
-		if (!currentDebt.anyRateIsInvalid) {
-			if (cacheInfo.isInvalid) {
-				console.log(yellow('Debt snapshot is invalid, and can be refreshed.'));
+			// Check if the snapshot is stale and can be fixed.
+			if (cacheInfo.isStale && !currentDebt.anyRateIsInvalid) {
+				console.log(yellow('Debt snapshot is stale, and can be refreshed.'));
 				await refreshSnapshotIfPossible(
 					cacheInfo.isInvalid,
 					currentDebt.anyRateIsInvalid,
 					cacheInfo.isStale
 				);
 				return true;
-			} else {
-				const cachedDebtEther = formatUnits(cacheInfo.debt);
-				const currentDebtEther = formatUnits(currentDebt.debt);
-				const deviation =
-					(Number(currentDebtEther) - Number(cachedDebtEther)) / Number(cachedDebtEther);
-				const maxDeviation = debtSnapshotMaxDeviation;
+			}
 
-				if (maxDeviation <= Math.abs(deviation)) {
-					console.log(
-						yellow(
-							`Debt cache deviation is ${deviation * 100}% >= ${maxDeviation *
-								100}%; refreshing it...`
-						)
+			// Otherwise, if the rates are currently valid,
+			// we might still need to take a snapshot due to invalidity or deviation.
+			if (!currentDebt.anyRateIsInvalid) {
+				if (cacheInfo.isInvalid) {
+					console.log(yellow('Debt snapshot is invalid, and can be refreshed.'));
+					await refreshSnapshotIfPossible(
+						cacheInfo.isInvalid,
+						currentDebt.anyRateIsInvalid,
+						cacheInfo.isStale
 					);
-					await refreshSnapshotIfPossible(cacheInfo.isInvalid, currentDebt.anyRateIsInvalid, true);
 					return true;
+				} else {
+					const cachedDebtEther = formatUnits(cacheInfo.debt);
+					const currentDebtEther = formatUnits(currentDebt.debt);
+					const deviation =
+						(Number(currentDebtEther) - Number(cachedDebtEther)) / Number(cachedDebtEther);
+					const maxDeviation = debtSnapshotMaxDeviation;
+
+					if (maxDeviation <= Math.abs(deviation)) {
+						console.log(
+							yellow(
+								`Debt cache deviation is ${deviation * 100}% >= ${maxDeviation *
+									100}%; refreshing it...`
+							)
+						);
+						await refreshSnapshotIfPossible(
+							cacheInfo.isInvalid,
+							currentDebt.anyRateIsInvalid,
+							true
+						);
+						return true;
+					}
 				}
 			}
-		}
 
-		// Finally, if the debt cache is currently valid, but needs to be invalidated, we will also perform a snapshot.
-		if (!cacheInfo.isInvalid && currentDebt.anyRateIsInvalid) {
-			console.log(yellow('Debt snapshot needs to be invalidated.'));
-			await refreshSnapshotIfPossible(cacheInfo.isInvalid, currentDebt.anyRateIsInvalid, false);
-			return true;
-		}
-		return false;
-	};
+			// Finally, if the debt cache is currently valid, but needs to be invalidated, we will also perform a snapshot.
+			if (!cacheInfo.isInvalid && currentDebt.anyRateIsInvalid) {
+				console.log(yellow('Debt snapshot needs to be invalidated.'));
+				await refreshSnapshotIfPossible(cacheInfo.isInvalid, currentDebt.anyRateIsInvalid, false);
+				return true;
+			}
+			return false;
+		};
 
-	try {
-		const performedSnapshot = await checkSnapshot({ useFork });
+		try {
+			const performedSnapshot = await checkSnapshot({ useFork });
 
-		if (performedSnapshot) {
-			console.log(gray('Snapshot complete.'));
-		} else {
-			console.log(gray('No snapshot required.'));
+			if (performedSnapshot) {
+				console.log(gray('Snapshot complete.'));
+			} else {
+				console.log(gray('No snapshot required.'));
+			}
+		} catch (err) {
+			catchMissingResolverWhenGeneratingSolidity({
+				contract: 'DebtSnapshot',
+				err,
+				generateSolidity,
+			});
 		}
-	} catch (err) {
-		catchMissingResolverWhenGeneratingSolidity({ contract: 'DebtSnapshot', err, generateSolidity });
+	} else {
+		// DebtCache.currentDebt() call fails on ovm fork on CI since it takes too long to execute.
+		console.log(gray(`\n------ Skipping debt cache check on OVM fork... ------\n`));
 	}
 };
